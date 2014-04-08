3c49cdbec6728cf625f75fe99458d69c8a6a4db3
Robin Winslow <robin@robinwinslow.co.uk>
2014-04-08 09:35:12 +0100

    Add fenchurch module and create rule in urls.py to point to it
    Former-commit-id: 69351db83dc44ae7843276aa731a764ce159f213

--- ubuntu/urls.py
+++ ubuntu/urls.py
@@ -1,12 +1,6 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, url
+from fenchurch import TemplateFinder
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ubuntu.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<template>.*)$', TemplateFinder.as_view()),
 )
