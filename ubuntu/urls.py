bb4a06145184d94f19b20bc815562328aa9ab32f
Tristram Oaten <tristram@oaten.name>
2014-08-29 11:14:35 +0100

    KILLED TAB WITH FIRE
    Former-commit-id: 9bb76415d2ca49eaee4dd7fc2589c08e44da4b23

--- ubuntu/urls.py
+++ ubuntu/urls.py
@@ -4,6 +4,6 @@ from fenchurch import TemplateFinder
 from ubuntu.views import DownloadView
 
 urlpatterns = patterns('',
-	url(r'^download/desktop/thank-you$', DownloadView.as_view()),
+    url(r'^download/desktop/thank-you$', DownloadView.as_view()),
     url(r'^(?P<template>.*)$', TemplateFinder.as_view()),
 )
