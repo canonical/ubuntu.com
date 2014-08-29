5cee188e223ceb42cd097b099fc7364e243e67a7
Robin Winslow <robin@robinwinslow.co.uk>
2014-08-29 15:34:39 +0100

    Tidy DownloadView.get_context_data

--- ubuntu/views.py
+++ ubuntu/views.py
@@ -8,15 +8,24 @@ from lib.gsa import GSAParser
 
 
 class DownloadView(TemplateFinder):
+
     def get_context_data(self, **kwargs):
+        """
+        Load mirrors data from RSS file
+        and return the list as context
+        """
+
         context = super(DownloadView, self).get_context_data(**kwargs)
+
         with open('/tmp/mirrors.rss') as rss:
             mirrors = parse(rss).entries
+
         mirror_list = [
             {'link': mirror['link'], 'bandwidth': mirror['mirror_bandwidth']}
             for mirror in mirrors
-            if mirror['mirror_countrycode'] == "GB"
-            #self.request.GET.get('country', "NO_COUNTRY_CODE")
+            if mirror['mirror_countrycode'] == self.request.GET.get(
+                'country', "NO_COUNTRY_CODE"
+            )
         ]
         context['mirror_list'] = json.dumps(mirror_list)
         return context
