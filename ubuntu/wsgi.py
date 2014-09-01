071dff041bf926448fcb2e5c50357a18e12be6d2
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-01 13:50:18 +0100

    Add Cling and dj-static
    Former-commit-id: 6b7f773532f78b1c56767edd6bc8cbebb2c1fce8

--- ubuntu/wsgi.py
+++ ubuntu/wsgi.py
@@ -8,7 +8,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubuntu.settings")
+import sys
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canonical.settings")
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+
