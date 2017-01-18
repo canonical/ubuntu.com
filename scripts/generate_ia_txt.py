0ad8c60ed0a3b1e87e66029e1cfd37cf0830b140
Robin Winslow <robin@robinwinslow.co.uk>
2017-01-19 00:09:20 +1100

    Update Information Architecture documents (#1258)
    * Put scripts in scripts directory
    
    * Generate updated IA files

--- scripts/generate_ia_txt.py
+++ scripts/generate_ia_txt.py
@@ -2,6 +2,9 @@ import os
 import urllib2
 import string
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+project_dir = os.path.dirname(script_dir)
+template_dir = os.path.join(project_dir, 'templates')
 
 def clean_path(p):
     cleaned = p[1:-1].split(".html")[0].split("index")[0]
@@ -27,10 +30,10 @@ def get_title_from_html(list_of_html_strings):
             title = string.rstrip(title, "</title>;\r\n")
     return title
 
+os.chdir(template_dir)
 html_files = os.popen('find . -name "*.html"').readlines()
 cleaned = map(clean_path, html_files)
 
-print '"Page title", "URL"'
 for path in cleaned:
     if (path.find("/_") == -1) and \
             (path.find("/base_") == -1) and \
@@ -39,8 +42,5 @@ for path in cleaned:
             (path.find("patterns-assets") == -1):
         full_path = 'https://www.ubuntu.com%s' % path
         page, path = grab_ubuntu_page(path)
-        print '"{title}", "{path}"'.format(
-            title=get_title_from_html(page).replace('"', '""'),
-            path=full_path
-        )
+        print "%s\t%s" % (get_title_from_html(page), full_path)
 
