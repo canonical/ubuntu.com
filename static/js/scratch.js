77dc5c6edc2c3c8c3c4ad5dc39a390cf509f0fbf
Graham Bancroft <gbancroft@gmail.com>
2017-04-03 09:08:12 +0100

    Fix cookie policy

--- static/js/scratch.js
+++ static/js/scratch.js
@@ -118,7 +118,7 @@ core.cookiePolicy = function() {
                 var range = document.createRange();
                 var cookieNode = range.createContextualFragment('<div class="cookie-policy"><div class="wrapper"><a href="?cp=close" class="link-cta">Close</a><p>We use cookies to improve your experience. By your continued use of this site you accept such use. To change your settings please <a href="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy#cookies">see our policy</a>.</p></div></div>');
                 document.body.insertBefore(cookieNode, document.body.lastChild);
-                document.querySelector('footer.global .legal').classList.add('has-cookie');
+                document.querySelector('footer.p-footer').classList.add('has-cookie');
                 document.querySelector('.cookie-policy .link-cta').addEventListener('click', function(e) {
                     e.preventDefault();
                     state('close');
