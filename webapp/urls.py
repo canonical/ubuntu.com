c62f12aedf151a19cb20cd7229c27fc50544cd77
Anthony Dillon <me@anthonydillon.com>
2017-11-23 17:26:57 +0000

    Add topic filtering to the resources list

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,7 @@ from canonicalwebteam import yaml_deleted_paths
 from ubuntudesign.gsa.views import SearchView
 
 # Local code
-from .views import UbuntuTemplateFinder, DownloadView
+from .views import UbuntuTemplateFinder, DownloadView, ResourcesView
 
 
 urlpatterns = load_redirects()
@@ -19,6 +19,7 @@ urlpatterns += [
         r'^(?P<template>download/desktop/contribute)$',
         DownloadView.as_view()
     ),
+    url(r'^resources', ResourcesView.as_view()),
     url(r'^search$', SearchView.as_view(template_name='search.html')),
     url(r'^(?P<template>.*)[^\/]$', UbuntuTemplateFinder.as_view()),
     url(r'$^', UbuntuTemplateFinder.as_view()),
--- webapp/views.py
+++ webapp/views.py
@@ -3,7 +3,11 @@ import os
 import re
 
 from feedparser import parse
+from django.views.generic.base import TemplateView
 from django_template_finder_view import TemplateFinder
+from canonicalwebteam.get_feeds import (
+    get_json_feed_content
+)
 
 
 class UbuntuTemplateFinder(TemplateFinder):
@@ -23,6 +27,59 @@ class UbuntuTemplateFinder(TemplateFinder):
         for index, path, in enumerate(clean_path.split('/')):
             context["level_" + str(index + 1)] = path
 
+        return context
+
+
+class ResourcesView(TemplateView):
+    template_name = 'resources/index.html'
+
+    INSIGHTS_URL = 'https://insights.ubuntu.com'
+    API_URL = INSIGHTS_URL + '/wp-json/wp/v2'
+    RESOURCE_FILTER = 'categories=1172,1509,1187'
+
+    def _get_categories_by_slug(self, slugs=[]):
+        if slugs:
+            if isinstance(slugs, list):
+                slugs = ','.join(slugs)
+        api_url = '{api_url}/group?slug={slug}'.format(
+            api_url=self.API_URL,
+            slug=slugs,
+        )
+        response = get_json_feed_content(api_url)
+        return response
+
+    def get_resources(self):
+
+        topic = self.request.GET.get('topic')
+        api_url = '{api_url}/posts?{resource_filter}'.format(
+            api_url=self.API_URL,
+            resource_filter=self.RESOURCE_FILTER,
+        )
+
+        if topic:
+            categories = self._get_categories_by_slug(topic)
+            category_id = categories[0]['id']
+
+            api_url = '{api_url}/posts?group={category_id}&{resource_filter}'
+            api_url = api_url.format(
+                api_url=self.API_URL,
+                category_id=str(category_id),
+                resource_filter=self.RESOURCE_FILTER,
+            )
+
+        resources = get_json_feed_content(api_url)
+        return resources
+
+    def get_context_data(self, **kwargs):
+        """
+        Get the context from insights for the resources page
+        """
+
+        # Get any existing context
+        context = super(ResourcesView, self).get_context_data(**kwargs)
+
+        context['items'] = self.get_resources()
+        context['slug'] = self.request.GET.get('topic')
         return context
 
 
