66976476f18d3e599c78aa25ce8913e0c5c47c7e
David Callé <david.calle@canonical.com>
2018-07-03 14:06:20 +0200

    Optimize and avoid errors on pages without a target element

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -156,6 +156,8 @@ function closeMainMenu() {
   }
 }
 
+var origin = window.location.pathname;
+
 addGAEvents('#canonical-products', 'www.ubuntu.com-nav-0-products');
 addGAEvents('#canonical-login', 'www.ubuntu.com-nav-0-login');
 addGAEvents('#enterprise-content', 'www.ubuntu.com-nav-1-enterprise');
@@ -167,15 +169,19 @@ addGAEvents('.p-footer__nav', 'www.ubuntu.com-nav-footer');
 addGAEvents('.p-contextual-footer', 'www.ubuntu.com-nav-contextual-footer');
 
 function addGAEvents(target, category){
-  document.querySelector(target).querySelectorAll('a').forEach(function(a) {
-    a.addEventListener('click', function(event){
-      dataLayer.push({
-        'event' : 'GAEvent',
-        'eventCategory' : category,
-        'eventAction' : 'from:'+window.location.pathname+' to:'+a.href.replace('https://www.ubuntu.com', '').split("?")[0],
-        'eventLabel' : a.text,
-        'eventValue' : undefined
+  var t = document.querySelector(target);
+  if (t) {
+    t.querySelectorAll('a').forEach(function(a) {
+      var destination = a.href.replace('https://www.ubuntu.com', '').split("?")[0];
+      a.addEventListener('click', function(event){
+        dataLayer.push({
+          'event' : 'GAEvent',
+          'eventCategory' : category,
+          'eventAction' : `from:${origin} to:${destination}`,
+          'eventLabel' : a.text,
+          'eventValue' : undefined
+        });
       });
     });
-  });
+  }
 }
