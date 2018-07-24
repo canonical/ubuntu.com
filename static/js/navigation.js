3c512e656a18caa39589bcbf8599240f773440d9
Caleb Ellis <caleb.ellis@canonical.com>
2018-07-24 12:38:22 +0100

    Make primary navigation stick to top of viewport

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,8 +1,25 @@
+/* Constants */
+var AUTO_CLOSE_BUFFER = 200;
+var GLOBAL_NAV_HEIGHT = 32;
+var NAV_BREAKPOINT = 900;
+var PRIMARY_NAV_HEIGHT = 48;
+var SCROLL_DELTA = 5;
+var SCROLL_LISTENER_INTERVAL = 250;
+var SLIDE_ANIM_TIME = 333;
+
 /* DOM elements */
-var navDropdowns = document.querySelectorAll('.p-navigation__dropdown-link');
+var body = document.body;
+var closeMenuLink = document.querySelector('.p-navigation__toggle--close');
 var dropdownWindow = document.querySelector('.dropdown-window');
 var dropdownWindowOverlay = document.querySelector('.dropdown-window-overlay');
-var closeMenuLink = document.querySelector('.p-navigation__toggle--close');
+var html = document.documentElement;
+var nav = document.querySelector('.p-navigation');
+var navDropdowns = document.querySelectorAll('.p-navigation__dropdown-link');
+
+/* Global variables */
+var didScroll;
+var lastScrollTop = 0;
+var slideAnimTimeout;
 
 var toggleDropdown = function(event) {
   event.preventDefault();
@@ -19,6 +36,14 @@ var toggleDropdown = function(event) {
       if (dropdown.classList.contains('is-selected')) {
         closeDropdown(dropdown);
       } else {
+        if (window.innerWidth > NAV_BREAKPOINT) {
+          clearTimeout(slideAnimTimeout);
+          var scrollPos = window.scrollY + PRIMARY_NAV_HEIGHT;
+          if (window.scrollY < GLOBAL_NAV_HEIGHT) {
+            scrollPos += GLOBAL_NAV_HEIGHT;
+          }
+          dropdownWindow.style.top = scrollPos + 'px';
+        }
         dropdown.classList.add('is-selected');
         dropdownContent.classList.remove('u-hide');
 
@@ -39,23 +64,74 @@ var closeNavigation = function() {
       closeDropdown(dropdown);
     }
   });
+  
 }
 
 function closeDropdown(dropdown) {
   dropdown.classList.remove('is-selected');
   dropdownWindow.classList.add('slide-animation');
   dropdownWindowOverlay.classList.add('fade-animation');
+  if (window.innerWidth > NAV_BREAKPOINT) {
+    dropdownWindow.style.top = window.scrollY - window.clientHeight;
+    slideAnimTimeout = setTimeout(function() {
+      dropdownWindow.style.top = null;
+    }, SLIDE_ANIM_TIME);
+  }
   if (window.history.pushState) {
     window.history.pushState(null, null, window.location.href.split('#')[0]);
   }
 }
 
+function controlNavPosition() {
+  var scrollPos = window.scrollY;
+  var dropdownHeight = dropdownWindow.clientHeight;
+  var dropdownPos = parseInt(dropdownWindow.style.top, 10);
+  var docHeight = Math.max(
+    body.offsetHeight,
+    body.scrollHeight,
+    html.clientHeight,
+    html.offsetHeight,
+    html.scrollHeight
+  );
+
+  if (Math.abs(lastScrollTop - scrollPos) <= SCROLL_DELTA) return;
+
+  if (scrollPos > lastScrollTop || scrollPos <= GLOBAL_NAV_HEIGHT) {
+    nav.classList.remove('is-down');
+    nav.classList.add('is-up');
+
+    if (scrollPos > dropdownPos + dropdownHeight) {
+      closeNavigation();
+    }
+  } else if (scrollPos + window.innerHeight < docHeight) {
+    nav.classList.remove('is-up');
+    nav.classList.add('is-down');
+
+    if (scrollPos + AUTO_CLOSE_BUFFER < dropdownPos) {
+      closeNavigation();
+    }
+  }
+
+  lastScrollTop = scrollPos;
+}
+
+/* Set interval on how often to run scroll function */
+setInterval(function() {
+  if (didScroll) {
+    controlNavPosition();
+    didScroll = false;
+  }
+}, SCROLL_LISTENER_INTERVAL);
+
 /* Apply event listeners to DOM elements */
+closeMenuLink.addEventListener('click', closeNavigation);
+dropdownWindowOverlay.addEventListener('click', closeNavigation);
 navDropdowns.forEach(function(dropdown) {
   dropdown.addEventListener('click', toggleDropdown);
 });
-closeMenuLink.addEventListener('click', closeNavigation);
-dropdownWindowOverlay.addEventListener('click', closeNavigation);
+window.addEventListener('scroll', function() {
+  didScroll = true;
+});
 
 /* Open primary nav on relevant dropdown when browser back button pressed */
 if (window.location.hash) {
