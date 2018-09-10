5c96a6d8cc5c4111af274afc7ff06e4b751f3d50
Anthony Dillon <me@anthonydillon.com>
2018-09-10 15:56:26 +0100

    Format OpenStack in chart keys

--- static/js/chart.js
+++ static/js/chart.js
@@ -178,6 +178,7 @@ function formatKeyLabel(key) {
   var keyLowerCase = key.toLowerCase().replace(/_/g, ' ');
   var formattedKey = keyLowerCase.charAt(0).toUpperCase() + keyLowerCase.substr(1);
   formattedKey = formattedKey.replace(' lts ', ' LTS ');
+  formattedKey = formattedKey.replace(' openstack ', 'OpenStack ');
 
   return formattedKey;
 }
