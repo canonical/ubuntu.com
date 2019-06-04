ffc8087c49f002e890931e971323d13912dc3c84
Peter Mahnke <peter@transitionelement.com>
2019-06-04 20:26:59 +0100

    Update template tag

--- webapp/templatetags/utils.py
+++ webapp/templatetags/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import template
 import dateutil.parser
+import re
 
 register = template.Library()
 
@@ -39,3 +40,39 @@ def keyvalue(dictionary, key_name):
     """
 
     return dictionary.get(key_name)
+
+
+# Variables
+class SetVarNode(template.Node):
+    def __init__(self, new_val, var_name):
+        self.new_val = new_val
+        self.var_name = var_name
+
+    def render(self, context):
+        context[self.var_name] = self.new_val
+        return ""
+
+
+@register.tag
+def setvar(parser, token):
+    # This version uses a regular expression to parse tag contents.
+
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires arguments" % token.contents.split()[0]
+        )
+    m = re.search(r"(.*?) as (\w+)", arg)
+    if not m:
+        raise template.TemplateSyntaxError(
+            "%r tag had invalid arguments" % tag_name
+        )
+    new_val, var_name = m.groups()
+    if not (new_val[0] == new_val[-1] and new_val[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+            "%r tag's argument should be in quotes" % tag_name
+        )
+
+    return SetVarNode(new_val[1:-1], var_name)
