6b92c94a02b4e0af013022d3ea9e711d4582c667
Robin Winslow <robin@robinwinslow.co.uk>
2019-08-21 15:02:08 +0100

    Remove unused template filter

--- webapp/templatetags/utils.py
+++ webapp/templatetags/utils.py
@@ -8,17 +8,6 @@ register = template.Library()
 
 
 @register.filter
-def truncate_chars(value, max_length):
-    length = len(value)
-    if length > max_length:
-        truncated = value[:max_length]
-        if not length == (max_length + 1) and value[max_length + 1] != " ":
-            truncated = truncated[: truncated.rfind(" ")]
-        return truncated + "&hellip;"
-    return value
-
-
-@register.filter
 def replace(value, args):
     params = args.split(",")
     return value.replace(params[0], params[1])
