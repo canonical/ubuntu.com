9014589fb2e9ca86b14691ff25103461499f4d00
Robin Winslow <robin@robinwinslow.co.uk>
2020-02-21 16:22:58 +0000

    Trigger the build when submitting /core/build
    I've bastardised the markup in `templates/core/build.html` just to
    illustrate how to POST the form to my view function. So
    @anthonydillon please fix it up.
    
    This makes use of the brand spanking new
    [canonicalwebteam.launchpad](https://github.com/canonical-web-and-design/canonicalwebteam.launchpad)
    module, where I mostly copied the code from snapcraft.io.

--- webapp/app.py
+++ webapp/app.py
@@ -37,6 +37,7 @@ from webapp.views import (
     blog_custom_topic,
     blog_press_centre,
     download_thank_you,
+    post_build,
     releasenotes_redirect,
     search_snaps,
     build_tutorials_index,
@@ -156,6 +157,7 @@ app.add_url_rule("/logout", view_func=logout)
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
 app.add_url_rule("/snaps", view_func=search_snaps)
+app.add_url_rule("/core/build", view_func=post_build, methods=["POST"])
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
 
 url_prefix = "/server/docs"
