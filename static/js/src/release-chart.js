8039cf3c1ce8bcf054d659f0613310043bfddf8c
Steve Rydz <steve.rydz@canonical.com>
2020-02-26 16:37:18 +0000

    Fix release chart js linting errors

--- static/js/src/release-chart.js
+++ static/js/src/release-chart.js
@@ -1,4 +1,4 @@
-import {createChart} from './chart'
+import { createChart } from './chart';
 import {
   smallReleases,
   serverAndDesktopReleases,
@@ -35,16 +35,16 @@ import {
 
 
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
 }
 
 function buildCharts() {
