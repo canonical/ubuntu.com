62bfa5c61bb92efd5f6077ffcfe714b644e543a3
Steve Rydz <steve.rydz@canonical.com>
2020-02-27 10:09:12 +0000

    Fix remaining lint errors in desktop statistics js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
       }],
       "linebreak-style": ["error", "unix"],
       "semi": ["error", "always"],
-      "object-curly-spacing": ["error", "always"]
+      "object-curly-spacing": ["error", "always"],
+      "no-prototype-builtins": "off"
     }
 };
