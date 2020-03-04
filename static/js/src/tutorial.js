75d9826054a9cfa64afcc3d79ce19dd869dcede9
Scott Mason Nash <sowasred2012@gmail.com>
2020-03-04 16:54:40 +0000

    prevent firefox showing a blank tutorials page when using back button

--- static/js/src/tutorial.js
+++ static/js/src/tutorial.js
@@ -1,54 +1,59 @@
 (function() {
   function toggleTutorialNavigation() {
-    const toggleButton = document.querySelector('.l-tutorial__nav-toggle');
-    const menu = document.querySelector('.l-tutorial__nav');
-    toggleButton.classList.toggle('p-icon--menu');
-    toggleButton.classList.toggle('p-icon--close');
-    menu.classList.toggle('u-hide--small');
+    const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
+    const menu = document.querySelector(".l-tutorial__nav");
+    toggleButton.classList.toggle("p-icon--menu");
+    toggleButton.classList.toggle("p-icon--close");
+    menu.classList.toggle("u-hide--small");
   }
 
   function setActiveLink(navigationItems) {
     navigationItems.forEach(item => {
-      const link = item.querySelector('.l-tutorial__nav-link');
-      if (link.getAttribute('href') === window.location.hash) {
-        item.classList.add('is-active');
+      const link = item.querySelector(".l-tutorial__nav-link");
+      if (link.getAttribute("href") === window.location.hash) {
+        item.classList.add("is-active");
       } else {
-        item.classList.remove('is-active');
+        item.classList.remove("is-active");
       }
     });
   }
 
-  const navigationItems = document.querySelectorAll('.l-tutorial__nav-item');
-  const toggleButton = document.querySelector('.l-tutorial__nav-toggle');
+  const navigationItems = document.querySelectorAll(".l-tutorial__nav-item");
+  const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
 
   if (toggleButton) {
-    toggleButton.addEventListener('click', toggleTutorialNavigation);
+    toggleButton.addEventListener("click", toggleTutorialNavigation);
   }
 
   setActiveLink(navigationItems);
 
   navigationItems.forEach(item => {
-    const link = item.querySelector('.l-tutorial__nav-link');
-    link.addEventListener('click', toggleTutorialNavigation);
+    const link = item.querySelector(".l-tutorial__nav-link");
+    link.addEventListener("click", toggleTutorialNavigation);
   });
 
-  window.addEventListener('hashchange', e => {
+  window.addEventListener("hashchange", e => {
     e.preventDefault();
     setActiveLink(navigationItems);
   });
 
   const sectionIds = [];
 
-  const tutorialSections = document.querySelectorAll('.l-tutorial__content section');
+  const tutorialSections = document.querySelectorAll(
+    ".l-tutorial__content section"
+  );
   tutorialSections.forEach(section => {
     sectionIds.push(section.id);
   });
 
   // Navigate to first tutorial step on load if no URL hash
   if (!window.location.hash) {
-    const firstSectionLink = document.querySelector('.l-tutorial__nav-link');
+    const firstSectionLink = document.querySelector(".l-tutorial__nav-link");
     if (firstSectionLink) {
-      window.location.hash = firstSectionLink.getAttribute('href');
+      const hash = firstSectionLink.getAttribute("href");
+      const url = window.location + hash;
+
+      window.location.replace(url);
     }
   } else {
     // Redirect #0, #1 etc. to the correct section
@@ -57,51 +62,57 @@
     if (match) {
       const index = parseInt(match[1]);
       const sectionId = sectionIds[index];
-      window.location.hash = '#' + sectionId;
+      window.location.hash = "#" + sectionId;
       window.location.reload();
     }
   }
 
-  const tutorialFeedbackOptions = document.querySelector('.l-tutorial__feedback-options');
-  const tutorialFeedbackIcons = document.querySelectorAll('.js-feedback-icon');
-  const tutorialFeedbackResult = document.querySelector('.l-tutorial__feedback-result');
+  const tutorialFeedbackOptions = document.querySelector(
+    ".l-tutorial__feedback-options"
+  );
+  const tutorialFeedbackIcons = document.querySelectorAll(".js-feedback-icon");
+  const tutorialFeedbackResult = document.querySelector(
+    ".l-tutorial__feedback-result"
+  );
 
   tutorialFeedbackIcons.forEach(icon => {
-    icon.addEventListener('click', function(e) {
-      const feedbackValue = e.target.getAttribute('data-feedback-value');
+    icon.addEventListener("click", function(e) {
+      const feedbackValue = e.target.getAttribute("data-feedback-value");
       dataLayer.push({
-        'event' : 'GAEvent',
-        'eventCategory' : 'feedback',
-        'eventAction' : feedbackValue,
-        'eventLabel' : feedbackValue,
-        'eventValue' : undefined
+        event: "GAEvent",
+        eventCategory: "feedback",
+        eventAction: feedbackValue,
+        eventLabel: feedbackValue,
+        eventValue: undefined
       });
 
-      tutorialFeedbackOptions.classList.add('u-hide');
-      tutorialFeedbackResult.classList.remove('u-hide');
+      tutorialFeedbackOptions.classList.add("u-hide");
+      tutorialFeedbackResult.classList.remove("u-hide");
     });
   });
 })();
 
 (function() {
-  const polls = document.querySelectorAll('.poll');
+  const polls = document.querySelectorAll(".poll");
 
   polls.forEach(poll => {
     const answers = poll.querySelectorAll('[type="radio"]');
-    const pollId = poll.getAttribute('data-poll-name');
+    const pollId = poll.getAttribute("data-poll-name");
 
     answers.forEach(answer => {
-      answer.addEventListener('change', e => {
-        const answerLabel = document.querySelector('label[for="' + e.target.id + '"]');
+      answer.addEventListener("change", e => {
+        const answerLabel = document.querySelector(
+          'label[for="' + e.target.id + '"]'
+        );
         const eventLabel = answerLabel.innerText;
         const eventAction = document.getElementById(pollId).innerText;
 
         dataLayer.push({
-          'event' : 'GAEvent',
-          'eventCategory' : 'survey',
-          'eventAction' : eventAction,
-          'eventLabel' : eventLabel,
-          'eventValue' : undefined
+          event: "GAEvent",
+          eventCategory: "survey",
+          eventAction: eventAction,
+          eventLabel: eventLabel,
+          eventValue: undefined
         });
       });
     });
