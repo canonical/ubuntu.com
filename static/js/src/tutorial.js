fd44512c02d8c3869d323184f94201c692cd6d0e
Steve Rydz <steve.rydz@canonical.com>
2020-03-05 16:19:30 +0000

    Add tests for tutorial page JS

--- static/js/src/tutorial.js
+++ static/js/src/tutorial.js
@@ -1,120 +1,118 @@
-(function() {
-  function toggleTutorialNavigation() {
-    const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
-    const menu = document.querySelector(".l-tutorial__nav");
-    toggleButton.classList.toggle("p-icon--menu");
-    toggleButton.classList.toggle("p-icon--close");
-    menu.classList.toggle("u-hide--small");
-  }
-
-  function setActiveLink(navigationItems) {
-    navigationItems.forEach(item => {
-      const link = item.querySelector(".l-tutorial__nav-link");
-      if (link.getAttribute("href") === window.location.hash) {
-        item.classList.add("is-active");
-      } else {
-        item.classList.remove("is-active");
-      }
-    });
-  }
-
-  const navigationItems = document.querySelectorAll(".l-tutorial__nav-item");
+function toggleTutorialNavigation() {
   const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
+  const menu = document.querySelector(".l-tutorial__nav");
+  toggleButton.classList.toggle("p-icon--menu");
+  toggleButton.classList.toggle("p-icon--close");
+  menu.classList.toggle("u-hide--small");
+}
 
-  if (toggleButton) {
-    toggleButton.addEventListener("click", toggleTutorialNavigation);
-  }
-
-  setActiveLink(navigationItems);
-
+function setActiveLink(navigationItems, hash) {
   navigationItems.forEach(item => {
     const link = item.querySelector(".l-tutorial__nav-link");
-    link.addEventListener("click", toggleTutorialNavigation);
-  });
-
-  window.addEventListener("hashchange", e => {
-    e.preventDefault();
-    setActiveLink(navigationItems);
-  });
-
-  const sectionIds = [];
-
-  const tutorialSections = document.querySelectorAll(
-    ".l-tutorial__content section"
-  );
-  tutorialSections.forEach(section => {
-    sectionIds.push(section.id);
-  });
-
-  // Navigate to first tutorial step on load if no URL hash
-  if (!window.location.hash) {
-    const firstSectionLink = document.querySelector(".l-tutorial__nav-link");
-    if (firstSectionLink) {
-      const hash = firstSectionLink.getAttribute("href");
-      const url = window.location + hash;
-
-      window.location.replace(url);
+    if (link.getAttribute("href") === hash) {
+      item.classList.add("is-active");
+    } else {
+      item.classList.remove("is-active");
     }
-  } else {
-    // Redirect #0, #1 etc. to the correct section
-    const match = window.location.hash.match(/^#(\d+)$/);
+  });
+}
 
-    if (match) {
-      const index = parseInt(match[1]);
-      const sectionId = sectionIds[index];
-      window.location.hash = "#" + sectionId;
-      window.location.reload();
-    }
+const navigationItems = document.querySelectorAll(".l-tutorial__nav-item");
+const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
+
+if (toggleButton) {
+  toggleButton.addEventListener("click", toggleTutorialNavigation);
+}
+
+setActiveLink(navigationItems, window.location.hash);
+
+navigationItems.forEach(item => {
+  const link = item.querySelector(".l-tutorial__nav-link");
+  link.addEventListener("click", toggleTutorialNavigation);
+});
+
+window.addEventListener("hashchange", e => {
+  e.preventDefault();
+  setActiveLink(navigationItems, window.location.hash);
+});
+
+const sectionIds = [];
+
+const tutorialSections = document.querySelectorAll(
+  ".l-tutorial__content section"
+);
+tutorialSections.forEach(section => {
+  sectionIds.push(section.id);
+});
+
+// Navigate to first tutorial step on load if no URL hash
+if (!window.location.hash) {
+  const firstSectionLink = document.querySelector(".l-tutorial__nav-link");
+  if (firstSectionLink) {
+    const hash = firstSectionLink.getAttribute("href");
+    const url = window.location + hash;
+
+    window.location.replace(url);
   }
+} else {
+  // Redirect #0, #1 etc. to the correct section
+  const match = window.location.hash.match(/^#(\d+)$/);
 
-  const tutorialFeedbackOptions = document.querySelector(
-    ".l-tutorial__feedback-options"
-  );
-  const tutorialFeedbackIcons = document.querySelectorAll(".js-feedback-icon");
-  const tutorialFeedbackResult = document.querySelector(
-    ".l-tutorial__feedback-result"
-  );
+  if (match) {
+    const index = parseInt(match[1]);
+    const sectionId = sectionIds[index];
+    window.location.hash = "#" + sectionId;
+    window.location.reload();
+  }
+}
 
-  tutorialFeedbackIcons.forEach(icon => {
-    icon.addEventListener("click", function(e) {
-      const feedbackValue = e.target.getAttribute("data-feedback-value");
+const tutorialFeedbackOptions = document.querySelector(
+  ".l-tutorial__feedback-options"
+);
+const tutorialFeedbackIcons = document.querySelectorAll(".js-feedback-icon");
+const tutorialFeedbackResult = document.querySelector(
+  ".l-tutorial__feedback-result"
+);
+
+tutorialFeedbackIcons.forEach(icon => {
+  icon.addEventListener("click", function(e) {
+    const feedbackValue = e.target.getAttribute("data-feedback-value");
+    dataLayer.push({
+      event: "GAEvent",
+      eventCategory: "feedback",
+      eventAction: feedbackValue,
+      eventLabel: feedbackValue,
+      eventValue: undefined
+    });
+
+    tutorialFeedbackOptions.classList.add("u-hide");
+    tutorialFeedbackResult.classList.remove("u-hide");
+  });
+});
+
+const polls = document.querySelectorAll(".poll");
+
+polls.forEach(poll => {
+  const answers = poll.querySelectorAll('[type="radio"]');
+  const pollId = poll.getAttribute("data-poll-name");
+
+  answers.forEach(answer => {
+    answer.addEventListener("change", e => {
+      const answerLabel = document.querySelector(
+        'label[for="' + e.target.id + '"]'
+      );
+      const eventLabel = answerLabel.innerText;
+      const eventAction = document.getElementById(pollId).innerText;
+
       dataLayer.push({
         event: "GAEvent",
-        eventCategory: "feedback",
-        eventAction: feedbackValue,
-        eventLabel: feedbackValue,
+        eventCategory: "survey",
+        eventAction: eventAction,
+        eventLabel: eventLabel,
         eventValue: undefined
       });
-
-      tutorialFeedbackOptions.classList.add("u-hide");
-      tutorialFeedbackResult.classList.remove("u-hide");
     });
   });
-})();
+});
 
-(function() {
-  const polls = document.querySelectorAll(".poll");
-
-  polls.forEach(poll => {
-    const answers = poll.querySelectorAll('[type="radio"]');
-    const pollId = poll.getAttribute("data-poll-name");
-
-    answers.forEach(answer => {
-      answer.addEventListener("change", e => {
-        const answerLabel = document.querySelector(
-          'label[for="' + e.target.id + '"]'
-        );
-        const eventLabel = answerLabel.innerText;
-        const eventAction = document.getElementById(pollId).innerText;
-
-        dataLayer.push({
-          event: "GAEvent",
-          eventCategory: "survey",
-          eventAction: eventAction,
-          eventLabel: eventLabel,
-          eventValue: undefined
-        });
-      });
-    });
-  });
-})();
+export { toggleTutorialNavigation, setActiveLink };
---
+++ static/js/src/tutorial.test.js
@@ -0,0 +1,75 @@
+import { toggleTutorialNavigation, setActiveLink } from "./tutorial.js";
+
+describe("toggleTutorialNavigation", () => {
+  it("toggles menu classes`", () => {
+    // Set up DOM elements
+    document.body.innerHTML = `
+      <div class="l-tutorial__nav-toggle p-icon--menu">Toggle</div>
+      <div class="l-tutorial__nav u-hide--small">Nav</div>
+    `;
+
+    toggleTutorialNavigation();
+
+    expect(
+      document
+        .querySelector(".l-tutorial__nav-toggle")
+        .classList.contains("p-icon--menu")
+    ).toBe(false);
+    expect(
+      document
+        .querySelector(".l-tutorial__nav-toggle")
+        .classList.contains("p-icon--close")
+    ).toBe(true);
+    expect(
+      document
+        .querySelector(".l-tutorial__nav")
+        .classList.contains("u-hide--small")
+    ).toBe(false);
+
+    toggleTutorialNavigation();
+
+    expect(
+      document
+        .querySelector(".l-tutorial__nav-toggle")
+        .classList.contains("p-icon--menu")
+    ).toBe(true);
+    expect(
+      document
+        .querySelector(".l-tutorial__nav-toggle")
+        .classList.contains("p-icon--close")
+    ).toBe(false);
+    expect(
+      document
+        .querySelector(".l-tutorial__nav")
+        .classList.contains("u-hide--small")
+    ).toBe(true);
+  });
+});
+
+describe("setActiveLink", () => {
+  it("sets correct link as active", () => {
+    // Set up DOM elements
+    document.body.innerHTML = `
+      <ul>
+        <li class="l-tutorial__nav-item">
+          <a class="l-tutorial__nav-link" href="#1">Link 1</a>
+        </li>
+        <li class="l-tutorial__nav-item">
+          <a class="l-tutorial__nav-link" href="#2">Link 2</a>
+        </li>
+      </ul>
+    `;
+
+    const navigationItems = document.querySelectorAll(".l-tutorial__nav-item");
+
+    setActiveLink(navigationItems, "#1");
+
+    expect(navigationItems[0].classList.contains("is-active")).toBe(true);
+    expect(navigationItems[1].classList.contains("is-active")).toBe(false);
+
+    setActiveLink(navigationItems, "#2");
+
+    expect(navigationItems[0].classList.contains("is-active")).toBe(false);
+    expect(navigationItems[1].classList.contains("is-active")).toBe(true);
+  });
+});
