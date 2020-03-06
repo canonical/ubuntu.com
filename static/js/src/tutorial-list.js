18355c4c76f8962ee181ff45689616a878808e86
Steve Rydz <steve.rydz@canonical.com>
2020-03-06 11:46:01 +0000

    Add tutorial list JS tests

--- static/js/src/tutorial-list.js
+++ static/js/src/tutorial-list.js
@@ -1,38 +1,38 @@
-(function() {
-  const topicsFilter = document.getElementById("tutorials-topic");
-  const sortFilter = document.getElementById("tutorials-sort");
-  const urlObj = new URL(window.location);
+const topicsFilter = document.getElementById("tutorials-topic");
+const sortFilter = document.getElementById("tutorials-sort");
+const urlObj = new URL(window.location);
 
-  function handleFilter(key, el, url) {
-    if (!el) {
-      return;
+function handleFilter(key, el, url) {
+  if (!el) {
+    return;
+  }
+
+  if (url.searchParams.has(key)) {
+    el.value = url.searchParams.get(key);
+  }
+
+  el.addEventListener("change", e => {
+    const value = e.target.value;
+
+    if (url.searchParams.has("page")) {
+      url.searchParams.delete("page");
     }
 
-    if (url.searchParams.has(key)) {
-      el.value = url.searchParams.get(key);
-    }
-
-    el.addEventListener("change", e => {
-      const value = e.target.value;
-
-      if (url.searchParams.has("page")) {
-        url.searchParams.delete("page");
-      }
-
-      if (value) {
-        if (url.searchParams.has(key)) {
-          url.searchParams.delete(key);
-        }
-
-        url.searchParams.append(key, value);
-      } else {
+    if (value) {
+      if (url.searchParams.has(key)) {
         url.searchParams.delete(key);
       }
 
-      window.location = url;
-    });
-  }
+      url.searchParams.append(key, value);
+    } else {
+      url.searchParams.delete(key);
+    }
 
-  handleFilter("topic", topicsFilter, urlObj);
-  handleFilter("sort", sortFilter, urlObj);
-})();
+    window.location = url;
+  });
+}
+
+handleFilter("topic", topicsFilter, urlObj);
+handleFilter("sort", sortFilter, urlObj);
+
+export { handleFilter };
