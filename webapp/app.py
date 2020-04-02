8818abca7f04756f3720c2d87bc31bf788ff9760
Carlos Wu <carlos.fei@canonical.com>
2020-04-02 15:18:04 +0100

    All data

--- webapp/app.py
+++ webapp/app.py
@@ -175,7 +175,7 @@ app.add_url_rule("/security/notices/<notice_id>", view_func=notice)
 # cve section
 app.add_url_rule("/security/cve", view_func=cve_index)
 app.add_url_rule(
-    "/security/<regex('cve-\\d{4}-\\d{4,7}'):cve_id>", view_func=cve
+    "/security/<regex('(cve-|CVE-)\\d{4}-\\d{4,7}'):cve_id>", view_func=cve
 )
 
 # Login
