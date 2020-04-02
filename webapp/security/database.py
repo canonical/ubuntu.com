4cfcd9af42fc39e89e69e23882c984e9b0f2cb8b
Carlos Wu <carlos.fei@canonical.com>
2020-04-02 09:25:17 +0100

    added relationship data

--- webapp/security/database.py
+++ webapp/security/database.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-db_engine = create_engine(os.environ["DATABASE_URL"])
+db_engine = create_engine("postgres://postgres:pw@localhost:5432/postgres")
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 )
