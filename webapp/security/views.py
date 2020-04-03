d34a45095c7645a2989b8094d11171b2283187f7
Carlos Wu <carlos.fei@canonical.com>
2020-04-03 20:55:29 +0100

    Add script to load cve data

--- webapp/security/views.py
+++ webapp/security/views.py
@@ -5,7 +5,6 @@ from math import ceil
 
 # Packages
 import flask
-import os
 from feedgen.entry import FeedEntry
 from feedgen.feed import FeedGenerator
 from marshmallow import EXCLUDE
@@ -25,11 +24,14 @@ from webapp.security.models import (
 )
 from webapp.security.schemas import NoticeSchema
 <<<<<<< HEAD
+<<<<<<< HEAD
 from webapp.security.auth import authorization_required
 
 =======
 from webapp.security.helper import create_data
 >>>>>>> All data
+=======
+>>>>>>> Add script to load cve data
 
 markdown_parser = Markdown(
     hard_wrap=True, parse_block_html=True, parse_inline_html=True
@@ -274,21 +276,11 @@ def create_notice():
 # ===
 def cve_index():
 
-    # Temporary fix for data to be displayed
-    cves_query = db_session.query(CVE).first()
-    if not cves_query and os.environ["FLASK_DEBUG"] == "true":
-        create_data()
     list_cve = db_session.query(CVE).limit(10)
-
     return flask.render_template("security/cve/index.html", list_cve=list_cve)
 
 
 def cve(cve_id):
-
-    # Temporary fix for data to be displayed
-    cves_query = db_session.query(CVE).first()
-    if not cves_query and os.environ["FLASK_DEBUG"] == "true":
-        create_data()
 
     cve = db_session.query(CVE).get(cve_id.upper())
     if not cve:
