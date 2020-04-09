1f4a0c43dfa8e3248b3281e3e4c06c8b94486d19
Carlos Wu <carlos.fei@canonical.com>
2020-04-09 10:31:39 +0100

    data changes

--- webapp/security/fixtures/load_sample_cve.py
+++ webapp/security/fixtures/load_sample_cve.py
@@ -161,7 +161,7 @@ def load_sample_cve():
                             name="Ubuntu 14.04 ESM (Precise Pangolin)",
                             status="DNE",
                         ),
-                        PackageReleaseStatus(
+                        Release(
                             name="Ubuntu 16.04 LTS (Xenial Xerus)",
                             status="needs-triage",
                         ),
@@ -986,6 +986,7 @@ def load_sample_cve():
             status="not-for-us",
             notes="Ubuntu-security Does not apply to software "
             + "found in Ubuntu. TestLinkOpenSourceTRMS",
+<<<<<<< HEAD
             packages=[
                 Package(
                     name="gitlab",
@@ -1284,6 +1285,19 @@ def load_sample_cve():
                     ],
                 ),
             ],
+=======
+<<<<<<< HEAD
+=======
+        ),
+        CVEReference(
+            uri="https://cve.mitre.org/cgi-bin/cvename.cgi?"
+            + "name=CVE-2020-9365"
+        ),
+        CVEReference(
+            uri="https://github.com/jedisct1/pure-ftpd/commit/"
+            + "36c6d268cb190282a2c17106acfd31863121b"
+>>>>>>> data changes
+>>>>>>> data changes
         ),
         Release(
             name="Ubuntu 12.04",
