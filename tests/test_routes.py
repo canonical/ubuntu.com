4aafba885c26fd099aae5066f032bcaa917aaad9
Scott Mason Nash <sowasred2012@gmail.com>
2020-05-03 13:42:19 +0100

    add route tests to check /advantage returns 200 response when logged in or out

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,6 +18,24 @@ class TestRoutes(unittest.TestCase):
 
         self.assertEqual(self.client.get("/").status_code, 200)
 
+    def test_advantage(self):
+        """
+        When given the advantage URL,
+        we should return a 200 status code
+        """
+        self.assertEqual(self.client.get("/advantage").status_code, 200)
+
+    def test_advantage_logged_in(self):
+        """
+        When given the advantage URL,
+        and the user is logged in,
+        we should return a 200 status code
+        """
+        with self.client.session_transaction() as s:
+            s["openid"] = "openid"
+
+        self.assertEqual(self.client.get("/advantage").status_code, 200)
+
     def test_not_found(self):
         """
         When given a non-existent URL,
