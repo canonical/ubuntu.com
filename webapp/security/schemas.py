cd3fc3d4ffd29b2d5280f4d8a11f82173535caa9
Robin Winslow <robin@robinwinslow.co.uk>
2020-05-20 00:19:43 +0100

    Updates for notices
    - Use references as a simple JSON list
    - Remove isummary

--- webapp/security/schemas.py
+++ webapp/security/schemas.py
@@ -5,7 +5,6 @@ class NoticeSchema(Schema):
     notice_id = fields.Str(data_key="id", required=True)
     title = fields.Str(required=True)
     summary = fields.Str(required=True)
-    isummary = fields.Str()
     description = fields.Str(required=True)
     action = fields.Str()
     releases = fields.Dict(required=True)
