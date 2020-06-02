971f950149d8d6d2bff40b7816fe1b573c6947ac
Steve Rydz <steve.rydz@canonical.com>
2020-06-02 11:29:22 +0100

    Update webpack config for consistency across projects

---
+++ webpack/entry.js
@@ -0,0 +1,24 @@
+module.exports = {
+  contributions: "./static/js/src/contributions.js",
+  "desktop-statistics": "./static/js/src/desktop-statistics.js",
+  tutorials: [
+    "./static/js/src/tutorial-list.js",
+    "./static/js/src/tutorial.js",
+  ],
+  forms: "./static/js/src/forms.js",
+  "image-download": "./static/js/src/image-download.js",
+  main: [
+    "./static/js/src/polyfills.js",
+    "./static/js/src/contextual-menu.js",
+    "./static/js/src/dynamic-contact-form.js",
+    "./static/js/src/core.js",
+    "./static/js/src/navigation.js",
+    "./static/js/src/form-validation.js",
+    "./static/js/src/scratch.js",
+  ],
+  "release-chart": "./static/js/src/release-chart.js",
+  tabotronic: "./static/js/src/tabotronic.js",
+  "tco-calculator": "./static/js/src/tco-calculator.js",
+  "sticky-nav": "./static/js/src/sticky-nav.js",
+  imageBuilder: "./static/js/src/imageBuilder.js",
+};
