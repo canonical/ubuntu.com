c47ab46dd261faf4a60f4328d9461853ba419c7e
David Callé <david.calle@canonical.com>
2020-06-17 14:53:12 +0200

    Add section path matching to 2nd nav generation

--- webapp/context.py
+++ webapp/context.py
@@ -73,7 +73,7 @@ def get_navigation(path):
                 # always show "Topics" as active on child topic pages
                 child["active"] = True
                 break
-            elif child["path"] == path or (
+            elif (child["path"] == path and path.startswith(nav_section["path"])) or (
                 child.get("persist") and path.startswith(child["path"])
             ):
                 # If child path matches current path or has persist set to true
