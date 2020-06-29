31378e739ec1c5302a36835e4c6e82d1818da370
Scott Mason Nash <sowasred2012@gmail.com>
2020-06-29 14:31:39 +0100

    handle multiple products on payment modal

---
+++ static/js/src/advantage/tests/fixtures/order-data.js
@@ -0,0 +1,12 @@
+export const appsInfraServer = [
+  {
+    name: "UA Infra Advanced Server",
+    quantity: 10,
+    unitPrice: 5000,
+  },
+  {
+    name: "UA Apps Server",
+    quantity: 10,
+    unitPrice: 5000,
+  },
+];
