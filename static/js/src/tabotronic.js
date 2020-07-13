0759cd4ae0633ac66edee1682d3d6b4fb788c75f
Scott Mason Nash <sowasred2012@gmail.com>
2020-07-13 15:28:29 +0100

    update tabotronic to prevent page jumping if necessary

--- static/js/src/tabotronic.js
+++ static/js/src/tabotronic.js
@@ -1,4 +1,4 @@
-(function () {
+function initTabs() {
   const tabLinks = document.querySelectorAll(".p-tabs__link");
   const tabContent = document.querySelectorAll(".p-tabs__content");
 
@@ -6,45 +6,63 @@
     const link = document.querySelector(
       `#${tab.getAttribute("aria-labelledby")}`
     );
+
     if (link && link.getAttribute("aria-selected") !== "true") {
       tab.classList.add("u-hide");
     }
   });
 
   tabLinks.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      const panelToOpen = e.target.getAttribute("aria-controls");
-      if (panelToOpen) {
-        tabClickHandler(e.target, panelToOpen);
+    link.addEventListener("click", function (event) {
+      if (link.dataset.noscroll) {
+        // prevent the page jumping around when switching tabs,
+        // whilst still using :target
+        // https://gist.github.com/pimterry/260841c2104f27cadc954a29b9873b96#file-disable-link-jump-with-workaround-js
+        event.preventDefault();
+        history.pushState({}, "", link.href);
+
+        // Update the URL again with the same hash, then go back
+        history.pushState({}, "", link.href);
+        history.back();
+        setActiveTab(false);
+      } else {
+        setActiveTab(true);
       }
     });
   });
 
-  function tabClickHandler(tabElement, panelToOpen) {
-    const container = tabElement.closest(".js-tab-container");
-    const tabLinks = container.querySelectorAll(".p-tabs__link");
-    const panels = container.querySelectorAll(".p-tabs__content");
-    const panelElement = container.querySelector(`#${panelToOpen}`);
+  document.addEventListener("DOMContentLoaded", setActiveTab());
+
+  window.addEventListener(
+    "hashchange",
+    function () {
+      setActiveTab();
+    },
+    false
+  );
+
+  function setActiveTab(scroll) {
+    var urlHash = window.location.hash;
+
     tabLinks.forEach((link) => {
-      link.setAttribute("aria-selected", false);
-    });
-    tabElement.setAttribute("aria-selected", true);
-    panels.forEach((panel) => {
-      panel.classList.add("u-hide");
-    });
-    panelElement.classList.remove("u-hide");
-    panelElement.scrollIntoView();
-  }
+      if (urlHash) {
+        const id = link.getAttribute("aria-controls");
+        const tabContent = document.getElementById(id);
 
-  function hashChange() {
-    const id = window.location.hash.replace("#", "");
-    const tab = document.querySelector(`[aria-controls="${id}"]`);
-    if (tab) {
-      tabClickHandler(tab, id);
-    }
-  }
+        if (`#${id}` === urlHash) {
+          link.setAttribute("aria-selected", true);
+          tabContent.classList.remove("u-hide");
 
-  window.addEventListener("hashchange", function () {
-    hashChange();
-  });
-})();
+          if (scroll) {
+            tabContent.scrollIntoView();
+          }
+        } else {
+          link.setAttribute("aria-selected", false);
+          tabContent.classList.add("u-hide");
+        }
+      }
+    });
+  }
+}
+
+initTabs();
