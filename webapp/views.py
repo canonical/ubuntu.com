aa99f9e91dabef770dc7e2306eaabde8b2288952
Carlos Wu <carlos.fei@canonical.com>
2020-08-13 20:16:16 +0200

    Build templates for engage pages from new discourse module - Individual engage pages template `/engage/<page>` - Index page `/engage` - Thank you template `/engage/thank-you` - Multilanguage thank you templates

--- webapp/views.py
+++ webapp/views.py
@@ -736,30 +736,12 @@ def build_engage_index(engage_docs):
         sort = flask.request.args.get("sort", default=None, type=str)
         posts_per_page = 15
         engage_docs.parser.parse()
-        if not topic:
-            metadata = engage_docs.parser.metadata
-        else:
-            metadata = [
-                doc
-                for doc in engage_docs.parser.metadata
-                if topic in doc["categories"]
-            ]
-
-        if sort == "difficulty-desc":
-            metadata = sorted(
-                metadata, key=lambda k: k["difficulty"], reverse=True
-            )
-
-        if sort == "difficulty-asc" or not sort:
-            metadata = sorted(
-                metadata, key=lambda k: k["difficulty"], reverse=False
-            )
+        metadata = engage_docs.parser.metadata
 
         total_pages = math.ceil(len(metadata) / posts_per_page)
 
         return flask.render_template(
             "engage/index.html",
-            navigation=engage_docs.parser.navigation,
             forum_url=engage_docs.parser.api.base_url,
             metadata=metadata,
             page=page,
@@ -770,6 +752,55 @@ def build_engage_index(engage_docs):
         )
 
     return engage_index
+
+
+def engage_thank_you(engage_pages):
+    """
+    Renders an engage pages thank-you page
+    i.e. whitepapers, pdfs
+
+    If there is no current topic it can't render the page
+    e.g. accessing directly
+
+    @parameters: language (optional) and page path name
+    e.g. /cloud-init-whitepaper
+    @returns: a function that renders a template
+    """
+
+    def render_template(language, page):
+        engage_pages.parser.parse()
+        page_url = f"/engage/{page}"
+        if language:
+            page_url = f"/engage/{language}/{page}"
+        index_topic_data = next(
+            (x for x in engage_pages.parser.metadata if x["path"] == page_url),
+            None,
+        )
+
+        if index_topic_data:
+            topic_id = engage_pages.parser.url_map[page_url]
+            engage_page_data = engage_pages.parser.get_topic(topic_id)
+            request_url = flask.request.referrer
+            resource_name = index_topic_data["type"]
+            resource_url = engage_page_data["metadata"]["resource_url"]
+            related = engage_page_data["related"]
+            language = index_topic_data["language"]
+            template_language = "engage/thank-you.html"
+            if language != "en":
+                template_language = f"engage/shared/_{language}_thank-you.html"
+
+            return flask.render_template(
+                template_language,
+                request_url=request_url,
+                resource_name=resource_name,
+                resource_url=resource_url,
+                related=related,
+            )
+        else:
+            return flask.abort(404)
+
+    return render_template
+
 
 # Blog
 # ===
