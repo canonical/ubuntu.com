f59f0c6f392701e835aca9febf44534752266cb0
Scott Mason Nash <sowasred2012@gmail.com>
2020-08-26 12:29:08 +0100

    redirect user to /advantage after successful payment

--- webapp/views.py
+++ webapp/views.py
@@ -828,9 +828,7 @@ def post_stripe_invoice_id(tx_type, tx_id, invoice_id):
             api_url=flask.current_app.config["CONTRACTS_API_URL"],
         )
 
-        return advantage.post_stripe_invoice_id(
-            tx_type, tx_id, invoice_id
-        )
+        return advantage.post_stripe_invoice_id(tx_type, tx_id, invoice_id)
     else:
         return flask.jsonify({"error": "authentication required"}), 401
 
