b27df188d3a7fbb9382c3f05d81e37ea77d549c8
Anthony Dillon <me@anthonydillon.com>
2020-09-22 06:44:15 +0100

    Write test for contribution thank you

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -90,6 +90,19 @@ class TestRoutes(VCRTestCase):
 
         self.assertIn(b"ubuntu-20.04-desktop-amd64.iso", desktop_response.data)
 
+    def test_contribution(self):
+        """
+        Check contribution thank-you pages render
+        """
+
+        thank_you_response = self.client.get("/download/desktop/thank-you")
+
+        self.assertEqual(thank_you_response.status_code, 200)
+
+        self.assertIn(
+            b"Thank you for your contribution", thank_you_response.data
+        )
+
     def test_advantage(self):
         """
         When given the advantage URL,
