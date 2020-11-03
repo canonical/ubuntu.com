d3cfde34474d2a0ff9079834d3897a3ac5982778
David Callé <david.calle@canonical.com>
2020-11-03 18:50:58 +0100

    Typos

--- static/js/src/dynamic-contact-form.js
+++ static/js/src/dynamic-contact-form.js
@@ -124,11 +124,11 @@
 
     function setGclid() {
       if (localStorage.getItem("gclid")) {
-        var gclid_field = document.getElementById("gclid");
+        var gclidField = document.getElementById("gclid");
         var gclid = JSON.parse(localStorage.getItem("gclid"));
         var isGclidValid = new Date().getTime() < gclid.expiryDate;
-        if (gclid && isGclidValid && gclid_field) {
-          gclid_field.value = gclid.value;
+        if (gclid && isGclidValid && gclidField) {
+          gclidField.value = gclid.value;
         }
       }
     }
