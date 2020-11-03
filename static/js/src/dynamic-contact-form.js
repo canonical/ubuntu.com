b2d87180314e5128adba3eb794e0e40e8bf0bf1a
David Callé <david.calle@canonical.com>
2020-11-03 18:50:04 +0100

    Quotes

--- static/js/src/dynamic-contact-form.js
+++ static/js/src/dynamic-contact-form.js
@@ -123,9 +123,9 @@
     }
 
     function setGclid() {
-      if (localStorage.getItem('gclid')) {
+      if (localStorage.getItem("gclid")) {
         var gclid_field = document.getElementById("gclid");
-        var gclid = JSON.parse(localStorage.getItem('gclid'));
+        var gclid = JSON.parse(localStorage.getItem("gclid"));
         var isGclidValid = new Date().getTime() < gclid.expiryDate;
         if (gclid && isGclidValid && gclid_field) {
           gclid_field.value = gclid.value;
