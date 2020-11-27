1245d62155142a71e2fec27cee5364a3bb69aaa0
Beth Collins <bethcollinsliv92@gmail.com>
2020-11-27 12:34:47 +0000

    Build draft CUBE landing page

--- webapp/views.py
+++ webapp/views.py
@@ -1229,3 +1229,9 @@ def cube_microcerts():
     }
 
     return flask.render_template("cube/microcerts.html", **data)
+
+
+def cube_home():
+    data = {}
+
+    return flask.render_template("cube/index.html", **data)
