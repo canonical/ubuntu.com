187766fa7be4d427e9c966f5106c8a38cfc2596e
ClementChaumel <clement.chaumel@gmail.com>
2020-12-07 10:45:02 +0100

    Switch back to live values

--- webapp/views.py
+++ webapp/views.py
@@ -400,7 +400,7 @@ def advantage_view():
     new_subscription_id = None
     open_subscription = flask.request.args.get("subscription", None)
     stripe_publishable_key = os.getenv(
-        "STRIPE_PUBLISHABLE_KEY", "pk_test_yndN9H0GcJffPe0W58Nm64cM00riYG4N46"
+        "STRIPE_PUBLISHABLE_KEY", "pk_live_68aXqowUeX574aGsVck8eiIE"
     )
 
     if user_info(flask.session):
@@ -700,7 +700,7 @@ def post_renewal_preview(renewal_id):
 def advantage_shop_view():
     account = previous_purchase_id = None
     stripe_publishable_key = os.getenv(
-        "STRIPE_PUBLISHABLE_KEY", "pk_test_yndN9H0GcJffPe0W58Nm64cM00riYG4N46"
+        "STRIPE_PUBLISHABLE_KEY", "pk_live_68aXqowUeX574aGsVck8eiIE"
     )
     api_url = flask.current_app.config["CONTRACTS_API_URL"]
 
