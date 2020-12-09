b5e9e661fb9aec36049508ca0335236470819229
João Martins <joao.figueiredo.martins@canonical.com>
2020-12-09 14:24:51 +0000

    Move CUBE\ to it's own module

--- webapp/app.py
+++ webapp/app.py
@@ -46,8 +46,6 @@ from webapp.views import (
     BlogSitemapPage,
     build,
     build_tutorials_index,
-    cube_home,
-    cube_microcerts,
     download_server_steps,
     download_thank_you,
     appliance_install,
@@ -586,6 +584,7 @@ core_als_autils_docs = Docs(
     blueprint_name="alsa-utils",
 )
 core_als_autils_docs.init_app(app)
+
 # Cube docs
 app.add_url_rule("/cube", view_func=cube_home)
 app.add_url_rule("/cube/microcerts", view_func=cube_microcerts)
