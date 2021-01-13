d227e2f15ce258f313b0da3789e3c3ba9eb6166d
Carlos Wu <carlos.fei@canonical.com>
2021-01-13 17:48:11 +0000

    Js homepage takeover

--- webapp/app.py
+++ webapp/app.py
@@ -370,42 +370,41 @@ engage_pages = EngagePages(
 app.add_url_rule(engage_path, view_func=build_engage_index(engage_pages))
 
 
-def build_takeovers(engage_pages):
-    def index_page():
-        engage_pages.parser.parse()
-
-        # Show only active
-        active_takeovers = [
-            takeover
-            for takeover in engage_pages.parser.takeovers
-            if takeover["active"] == "true"
-        ]
-        return flask.render_template("index.html", takeovers=active_takeovers)
-
-    return index_page
-
-
-def build_takeovers_index(engage_pages):
-    def takeover_index():
-        engage_pages.parser.parse()
-        sorted_takeovers = sorted(
-            engage_pages.parser.takeovers,
-            key=lambda takeover: takeover["publish_date"],
-            reverse=True,
-        )
-        active_takeovers = [
-            takeover
-            for takeover in engage_pages.parser.takeovers
-            if takeover["active"] == "true"
-        ]
-        active_count = len(active_takeovers)
-        hidden_count = len(sorted_takeovers) - active_count
+def get_takeovers(engage_pages, active=False):
+    engage_pages.parser.parse()
+    sorted_takeovers = sorted(
+        engage_pages.parser.takeovers,
+        key=lambda takeover: takeover["publish_date"],
+        reverse=True,
+    )
+    active_takeovers = [
+        takeover
+        for takeover in engage_pages.parser.takeovers
+        if takeover["active"] == "true"
+    ]
+    active_count = len(active_takeovers)
+    hidden_count = len(sorted_takeovers) - active_count
+    if active:
+        return flask.jsonify(active_takeovers)
+    else:
         return flask.render_template(
             "takeovers/index.html",
             active_count=active_count,
             hidden_count=hidden_count,
             takeovers=sorted_takeovers,
         )
+
+
+def build_takeovers(engage_pages):
+    def index_page():
+        return get_takeovers(engage_pages, True)
+
+    return index_page
+
+
+def build_takeovers_index(engage_pages):
+    def takeover_index():
+        return get_takeovers(engage_pages)
 
     return takeover_index
 
