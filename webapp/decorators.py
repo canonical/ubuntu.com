2833b72968beb9b8f26fa44692111a408c3b8a5c
João Martins <joao.figueiredo.martins@canonical.com>
2021-02-01 09:51:06 -0500

    Change Badgr and EDX API URIs

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -7,6 +7,7 @@ from distutils.util import strtobool
 import flask
 from webapp.login import user_info
 
+
 def login_required(func):
     """
     Decorator that checks if a user is logged in, and redirects
