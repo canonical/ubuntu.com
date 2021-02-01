2211c0972104f1e5c03c60f3d4d5d9593c7fd53e
João Martins <joao.figueiredo.martins@canonical.com>
2021-02-01 09:51:06 -0500

    Change Badgr and EDX API URIs

--- webapp/cube/api.py
+++ webapp/cube/api.py
@@ -102,11 +102,7 @@ class EdxAPI:
         uri = (
             "/api/edx_proctoring/v1/proctored_exam/attempt/course_id/"
             f"{course_id}/search/{username}"
-<<<<<<< HEAD
             "?page_size=100"
-=======
-            "&page_size=100"
->>>>>>> Get attempts for each course from EDX to work out the course status
         )
         return self.make_request(
             "GET",
