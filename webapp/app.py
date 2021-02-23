546e595e23fc4f0109885cb0d072b898a4694883
Robin Winslow <robin@robinwinslow.co.uk>
2021-02-23 11:37:07 +0000

    Upgrade to canonicalwebteam.search v1 to use default session

--- webapp/app.py
+++ webapp/app.py
@@ -240,7 +240,9 @@ app.add_url_rule(
 
 app.add_url_rule("/getubuntu/releasenotes", view_func=releasenotes_redirect)
 app.add_url_rule(
-    "/search", "search", build_search_view(template_path="search.html")
+    "/search",
+    "search",
+    build_search_view(session=session, template_path="search.html"),
 )
 app.add_url_rule(
     (
