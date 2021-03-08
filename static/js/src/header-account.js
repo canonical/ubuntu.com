12b65005eca6a5f5f009488c7a43ac38386c4aee
Anthony Dillon <me@anthonydillon.com>
2021-03-08 16:48:17 +0000

    Add prettier config and format js

--- static/js/src/header-account.js
+++ static/js/src/header-account.js
@@ -1,10 +1,11 @@
 var accountContainer = document.querySelector('.js-account');
 if (accountContainer) {
   fetch('/account.json')
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       if (data.account === null) {
-        accountContainer.innerHTML = '<a href="/login" class="p-navigation__link-anchor">Sign in</a>';
+        accountContainer.innerHTML =
+          '<a href="/login" class="p-navigation__link-anchor">Sign in</a>';
       } else {
         accountContainer.innerHTML = `<div class="p-navigation__link-anchor">${data.account.fullname} (<a href="/logout" class="p-navigation__logout">logout</a>)</div>`;
       }
