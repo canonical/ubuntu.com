1fd33309bd75c3d065c68448f899ff731b359b32
Carlos Wu <carlos.fei@canonical.com>
2021-03-24 11:39:42 +0000

    Setup api endpoints for certifications

--- webapp/certification/views.py
+++ webapp/certification/views.py
@@ -1,4 +1,14 @@
 import flask
+import talisker.requests
+import talisker.sentry
+from requests import Session
+from webapp.certification.api import CertificationAPI
+
+session = Session()
+talisker.requests.configure(session)
+api = CertificationAPI(
+    base_url="https://certification.canonical.com/api/v1", session=session
+)
 
 
 def certification_home():
