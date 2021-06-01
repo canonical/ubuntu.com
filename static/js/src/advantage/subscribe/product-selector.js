0c303fa5db5ea9292932b67c951325f7766005da
ClementChaumel <clement.chaumel@gmail.com>
2021-06-01 13:51:23 +0200

    Clean up

--- static/js/src/advantage/subscribe/product-selector.js
+++ static/js/src/advantage/subscribe/product-selector.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { debounce } from "../../utils/debounce";
 import { saveState } from "../../utils/persitState";
-import { getCustomerInfo } from "../contracts-api";
 import initFormInputs from "./listeners/form-event-listeners";
 import initUIControls from "./listeners/ui-event-listeners";
 
 import formReducer from "./reducers/form-reducer";
 import UIReducer from "./reducers/ui-reducer";
-import UserInfoReducer, { initUserInfo } from "./reducers/user-info-reducer";
 
 import render from "./renderers/main-renderer";
 
@@ -19,15 +17,8 @@ const store = configureStore({
   reducer: {
     form: formReducer,
     ui: UIReducer,
-    userInfo: UserInfoReducer,
   },
 });
-
-getCustomerInfo(window.accountId)
-  .then((res) => {
-    store.dispatch(initUserInfo(res.data));
-  })
-  .catch((e) => console.error({ e }));
 
 initFormInputs(store);
 initUIControls(store);
--- static/js/src/advantage/subscribe/renderers/main-renderer.js
+++ static/js/src/advantage/subscribe/renderers/main-renderer.js
@@ -1,9 +1,7 @@
 import renderForm from "./form-renderer";
 import renderUI from "./UI-renderer";
-import renderPurchaseModal from "./purchase-modal-renderer";
 
 export default function render(state) {
   renderForm(state.form);
   renderUI(state.ui);
-  renderPurchaseModal(state);
 }
