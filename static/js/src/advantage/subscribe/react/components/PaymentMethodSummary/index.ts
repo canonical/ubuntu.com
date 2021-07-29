8f9cd5c16930030aafe43f9482cc3b91e9127d25
ClementChaumel <clement.chaumel@gmail.com>
2021-07-29 12:17:09 +0200

    Prettier-ify

--- static/js/src/advantage/subscribe/react/components/PaymentMethodSummary/index.ts
+++ static/js/src/advantage/subscribe/react/components/PaymentMethodSummary/index.ts
@@ -1,1 +1,1 @@
-export { default } from './PaymentMethodSummary';
+export { default } from "./PaymentMethodSummary";
--- static/js/src/advantage/subscribe/react/components/Summary/index.ts
+++ static/js/src/advantage/subscribe/react/components/Summary/index.ts
@@ -1,1 +1,1 @@
-export { default } from './Summary';
+export { default } from "./Summary";
