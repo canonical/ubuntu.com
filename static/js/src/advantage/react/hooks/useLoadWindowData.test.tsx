f77416bd9ad36c354004fa18462206d4f9b8b0df
Huw Wilkins <huw.wilkins@canonical.com>
2021-07-29 14:50:21 +1000

    Add subscription data to the window and insert into the react query store.

---
+++ static/js/src/advantage/react/hooks/useLoadWindowData.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import type { ReactNode } from "react";
+import { shallow } from "enzyme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { renderHook } from "@testing-library/react-hooks";
+
+import { useLoadWindowData } from "./useLoadWindowData";
+
+describe("useLoadWindowData", () => {
+  beforeEach(() => {
+    window.enterpriseContracts = { enterprise: "contracts" };
+    window.pendingPurchaseId = "12345";
+    window.personalAccount = { personal: "account" };
+  });
+
+  afterEach(() => {
+    delete window.enterpriseContracts;
+    delete window.pendingPurchaseId;
+    delete window.personalAccount;
+  });
+
+  it("fetches data from the window and inserts into react-query", async () => {
+    const queryClient = new QueryClient();
+    const wrapper = ({ children }: { children: ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
+    renderHook(() => useLoadWindowData(queryClient), {
+      wrapper,
+    });
+    expect(queryClient.getQueryData("enterpriseContracts")).toStrictEqual({
+      enterprise: "contracts",
+    });
+    expect(queryClient.getQueryData("pendingPurchaseId")).toBe("12345");
+    expect(queryClient.getQueryData("personalAccount")).toStrictEqual({
+      personal: "account",
+    });
+  });
+});
