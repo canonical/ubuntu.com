3e70437669d33d1787ef46aecdab963fd70d9f95
Huw Wilkins <huw.wilkins@canonical.com>
2021-07-29 12:23:56 +1000

    Add basic subscriptions and subscriptions content components.

---
+++ static/js/src/advantage/react/components/Subscriptions/Subscriptions.tsx
@@ -0,0 +1,12 @@
+import { Strip } from "@canonical/react-components";
+import React from "react";
+
+import Content from "./Content";
+
+const Subscriptions = () => (
+  <Strip>
+    <Content />
+  </Strip>
+);
+
+export default Subscriptions;
