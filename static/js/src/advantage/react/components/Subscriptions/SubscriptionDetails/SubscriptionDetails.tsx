133f56fe36a6397004cf79b563b40bc62b9a125f
Huw Wilkins <huw.wilkins@canonical.com>
2021-07-30 11:26:13 +1000

    Add basic scroll subscription list  and subscription details components.

---
+++ static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/SubscriptionDetails.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SubscriptionDetails = () => (
+  <div className="p-subscribe__details">
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+    <p>Details</p>
+  </div>
+);
+
+export default SubscriptionDetails;
--- static/js/src/advantage/react/hooks/useLoadWindowData.test.tsx
+++ static/js/src/advantage/react/hooks/useLoadWindowData.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { ReactNode } from "react";
-import { shallow } from "enzyme";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { renderHook } from "@testing-library/react-hooks";
 
