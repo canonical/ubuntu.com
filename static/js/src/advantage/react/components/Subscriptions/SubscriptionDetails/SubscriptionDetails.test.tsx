2c49619c5c4ce15aca7dd0006cf1d84da9106a07
Huw Wilkins <huw.wilkins@canonical.com>
2021-08-09 15:55:39 +1000

    Add a subscription edit form.

--- static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/SubscriptionDetails.test.tsx
+++ static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/SubscriptionDetails.test.tsx
@@ -4,8 +4,33 @@ import { shallow } from "enzyme";
 import SubscriptionDetails from "./SubscriptionDetails";
 
 describe("SubscriptionDetails", () => {
-  it("renders", () => {
+  it("initially shows the content", () => {
     const wrapper = shallow(<SubscriptionDetails />);
-    expect(wrapper.find(".p-subscriptions__details").exists()).toBe(true);
+    expect(wrapper.find("DetailsContent").exists()).toBe(true);
+    expect(wrapper.find("SubscriptionEdit").exists()).toBe(false);
+  });
+
+  it("can show the edit form", () => {
+    const wrapper = shallow(<SubscriptionDetails />);
+    wrapper.find("[data-test='edit-button']").simulate("click");
+    expect(wrapper.find("SubscriptionEdit").exists()).toBe(true);
+    expect(wrapper.find("DetailsContent").exists()).toBe(false);
+  });
+
+  it("disables the buttons when showing the edit form", () => {
+    const wrapper = shallow(<SubscriptionDetails />);
+    expect(wrapper.find("[data-test='edit-button']").prop("disabled")).toBe(
+      false
+    );
+    expect(wrapper.find("[data-test='cancel-button']").prop("disabled")).toBe(
+      false
+    );
+    wrapper.find("[data-test='edit-button']").simulate("click");
+    expect(wrapper.find("[data-test='edit-button']").prop("disabled")).toBe(
+      true
+    );
+    expect(wrapper.find("[data-test='cancel-button']").prop("disabled")).toBe(
+      true
+    );
   });
 });
--- static/js/src/advantage/react/components/Subscriptions/SubscriptionEdit/SubscriptionEdit.tsx
+++ static/js/src/advantage/react/components/Subscriptions/SubscriptionEdit/SubscriptionEdit.tsx
@@ -1,21 +1,76 @@
-import { Button } from "@canonical/react-components";
+import { ActionButton, Button } from "@canonical/react-components";
 import React from "react";
 import usePortal from "react-useportal";
+import { Formik } from "formik";
 
 import SubscriptionCancel from "../SubscriptionCancel";
+import FormikField from "../../FormikField";
 
-const SubscriptionEdit = () => {
+type Props = {
+  onClose: () => void;
+};
+
+const SubscriptionEdit = ({ onClose }: Props) => {
   const { openPortal, closePortal, isOpen, Portal } = usePortal();
   return (
     <>
-      <Button appearance="link" data-test="cancel-button" onClick={openPortal}>
-        You can cancel this subscription online or contact us.
-      </Button>
-      {isOpen && (
-        <Portal>
-          <SubscriptionCancel onClose={closePortal} />
-        </Portal>
-      )}
+      <Formik
+        initialValues={{
+          // TODO: use the initial value from the edited subscription.
+          size: 0,
+        }}
+        onSubmit={() => {
+          // TODO: Implement updating the subscription:
+          // https://github.com/canonical-web-and-design/commercial-squad/issues/113
+        }}
+      >
+        {({ handleSubmit }) => (
+          <>
+            <div className="u-sv3">
+              <hr />
+            </div>
+            <form className="u-sv2" onSubmit={handleSubmit}>
+              <h5>Resize subscription</h5>
+              <div className="u-sv3">
+                <FormikField
+                  className="p-subscription__resize"
+                  help={
+                    <>
+                      You can resize your subscriptions to as many machines as
+                      needed.
+                      <br />
+                      Your next billing period will reflect the changes
+                      accordingly.
+                    </>
+                  }
+                  label="Number of machines"
+                  name="size"
+                  type="number"
+                />
+              </div>
+              <div className="u-align--right">
+                <Button appearance="base" onClick={onClose}>
+                  Cancel
+                </Button>
+                <ActionButton appearance="positive">Resize</ActionButton>
+              </div>
+            </form>
+            <hr />
+            <Button
+              appearance="link"
+              data-test="cancel-button"
+              onClick={openPortal}
+            >
+              You can cancel this subscription online or contact us.
+            </Button>
+            {isOpen && (
+              <Portal>
+                <SubscriptionCancel onClose={closePortal} />
+              </Portal>
+            )}
+          </>
+        )}
+      </Formik>
     </>
   );
 };
