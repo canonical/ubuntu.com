e4b3325178ef27ab845fdad4ec0bcb70562c7bc1
ClementChaumel <clement.chaumel@gmail.com>
2021-08-09 17:17:01 +0200

    Add dependency

--- static/js/src/advantage/subscribe/react/app.jsx
+++ static/js/src/advantage/subscribe/react/app.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PurchaseModal from "./PurchaseModal";
 
-const stripePromise = loadStripe(window.stripePublishableKey);
+Sentry.init({
+  dsn: "https://0293bb7fc3104e56bafd2422e155790c@sentry.is.canonical.com//13",
+  integrations: [new Integrations.BrowserTracing()],
 
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const stripePromise = loadStripe(window.stripePublishableKey);
 const oneHour = 1000 * 60 * 60;
 const queryClient = new QueryClient({
   defaultOptions: {
