978dcae54a40735576b01a9fdeb88c71c7163cdc
Huw Wilkins <huw.wilkins@canonical.com>
2021-08-13 09:11:09 +1000

    Adapt the content component for small screens.

--- static/js/src/advantage/react/components/Subscriptions/Content/Content.test.tsx
+++ static/js/src/advantage/react/components/Subscriptions/Content/Content.test.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+import { Context as ResponsiveContext } from "react-responsive";
 
 import Content from "./Content";
 
 describe("Content", () => {
-  it("renders", () => {
-    const wrapper = shallow(<Content />);
+  it("displays the list and details for large screens", () => {
+    const wrapper = mount(
+      <ResponsiveContext.Provider value={{ width: 1000 }}>
+        <Content />
+      </ResponsiveContext.Provider>
+    );
     expect(wrapper.find("SubscriptionList").exists()).toBe(true);
     expect(wrapper.find("SubscriptionDetails").exists()).toBe(true);
+  });
+
+  it("hides the details for small screens", () => {
+    const wrapper = mount(
+      <ResponsiveContext.Provider value={{ width: 200 }}>
+        <Content />
+      </ResponsiveContext.Provider>
+    );
+    expect(wrapper.find("SubscriptionList").exists()).toBe(true);
+    expect(wrapper.find("SubscriptionDetails").exists()).toBe(false);
   });
 });
--- static/js/src/advantage/react/components/Subscriptions/Content/Content.tsx
+++ static/js/src/advantage/react/components/Subscriptions/Content/Content.tsx
@@ -1,14 +1,23 @@
 import { Card } from "@canonical/react-components";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 import SubscriptionDetails from "../SubscriptionDetails";
 import SubscriptionList from "../SubscriptionList";
 
-const Content = () => (
-  <Card className="u-no-margin--bottom u-no-padding p-subscriptions__card">
-    <SubscriptionList />
-    <SubscriptionDetails />
-  </Card>
-);
+enum Breakpoint {
+  SMALL = "620px",
+}
 
+const Content = () => {
+  const isSmallScreen = useMediaQuery({
+    query: `(max-width: ${Breakpoint.SMALL})`,
+  });
+  return (
+    <Card className="u-no-margin--bottom u-no-padding p-subscriptions__card">
+      <SubscriptionList />
+      {isSmallScreen ? null : <SubscriptionDetails />}
+    </Card>
+  );
+};
 export default Content;
