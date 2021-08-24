ba30331767194fb7ee27ac9f93671ea355124ad4
Albert Kolozsvari <albertartk@gmail.com>
2021-08-24 17:14:13 +0100

    Add helper and builder functions

--- webapp/advantage/ua_contracts/builders.py
+++ webapp/advantage/ua_contracts/builders.py
@@ -1,9 +1,111 @@
 from typing import List, Dict
 
+from webapp.advantage.ua_contracts.helpers import (
+    group_items_by_listing,
+    get_items_aggregated_values,
+    get_machine_type,
+    is_trialling_user_subscription,
+    get_user_subscription_statuses,
+)
+from webapp.advantage.ua_contracts.primitives import (
+    Contract,
+    Account,
+    Subscription,
+    ContractItem,
+)
 from webapp.advantage.models import Listing, UserSubscription
 
 
 def build_user_subscriptions(
     user_summary: List, listings: Dict[str, Listing]
 ) -> List[UserSubscription]:
-    pass
+    grouped_items = build_initial_user_subscriptions(
+        user_summary=user_summary,
+        listings=listings,
+    )
+
+    return build_final_user_subscriptions(grouped_items)
+
+
+def build_initial_user_subscriptions(
+    user_summary: List, listings: Dict[str, Listing]
+) -> List:
+    free_groups = []
+    shop_groups = []
+    legacy_groups = []
+    for user_details in user_summary:
+        contracts: List[Contract] = user_details.get("contracts")
+        account: Account = user_details.get("account")
+        subscriptions: List[Subscription] = user_details.get("subscriptions")
+
+        for contract in contracts:
+            # Free user subscriptions
+            if contract.product_id == "free":
+                free_groups.append(
+                    {
+                        "account": account,
+                        "contract": contract,
+                        "listing": None,
+                        "subscriptions": subscriptions,
+                        "items": contract.items,
+                        "type": "free",
+                    }
+                )
+
+                continue
+
+            # Shop user subscriptions
+            raw_shop_groups = group_items_by_listing(items=contract.items)
+            for listing_id in raw_shop_groups:
+                listing: Listing = listings[listing_id]
+                items: List[ContractItem] = raw_shop_groups[listing_id]
+                is_trialled = is_trialling_user_subscription(items=items)
+
+                shop_groups.append(
+                    {
+                        "account": account,
+                        "contract": contract,
+                        "listing": listing,
+                        "subscriptions": subscriptions,
+                        "items": items,
+                        "type": listing.period if not is_trialled else "trial",
+                    }
+                )
+
+    return free_groups + shop_groups + legacy_groups
+
+
+def build_final_user_subscriptions(
+    grouped_items: List,
+) -> List[UserSubscription]:
+    user_subscriptions = []
+    for group in grouped_items:
+        account: Account = group.get("account")
+        listing: Listing = group.get("listing")
+        contract: Contract = group.get("contract")
+        subscriptions: List[Subscription] = group.get("subscriptions")
+        items: List[ContractItem] = group.get("items")
+        aggregated_values = get_items_aggregated_values(items)
+
+        user_subscription = UserSubscription(
+            type=group.get("type"),
+            account_id=account.id,
+            entitlements=contract.entitlements,
+            start_date=aggregated_values.get("start_date"),
+            end_date=aggregated_values.get("end_date"),
+            number_of_machines=aggregated_values.get("number_of_machines"),
+            product_name=listing.product_name if listing else None,
+            marketplace=listing.marketplace if listing else None,
+            price_per_unit=listing.price if listing else None,
+            machine_type=get_machine_type(contract.product_id),
+            statuses=get_user_subscription_statuses(
+                type=group.get("type"),
+                end_date=aggregated_values.get("end_date"),
+                subscriptions=subscriptions or [],
+                listing=listing or None,
+            ),
+        )
+
+        user_subscriptions.append(user_subscription)
+
+    return user_subscriptions
--- webapp/advantage/ua_contracts/helpers.py
+++ webapp/advantage/ua_contracts/helpers.py
@@ -1,3 +1,177 @@
+from datetime import datetime
+from typing import List, Optional, Dict
+
+import pytz
+from dateutil.parser import parse
+
+from webapp.advantage.models import Listing
+from webapp.advantage.ua_contracts.primitives import Subscription, ContractItem
+
+
+def group_items_by_listing(
+    items: List[ContractItem],
+) -> Dict[str, List[ContractItem]]:
+    item_groups = {}
+    for item in items:
+        listing_id = item.product_listing_id
+
+        # skip legacy contract items
+        if not listing_id:
+            continue
+
+        item_groups[listing_id] = item_groups.get(listing_id, [])
+        item_groups[listing_id].append(item)
+
+    return item_groups
+
+
+def get_items_aggregated_values(
+    items: List[ContractItem],
+) -> Dict:
+    start_date = None
+    end_date = None
+    number_of_machines = 0
+    for item in items:
+        number_of_machines = number_of_machines + item.value
+
+        if not start_date:
+            start_date = item.start_date
+
+        if not end_date:
+            end_date = item.end_date
+
+        if start_date and start_date > item.start_date:
+            start_date = item.start_date
+
+        if end_date and end_date < item.end_date:
+            end_date = item.end_date
+
+    parsed_end_date = parse(end_date)
+    # free user subscription end date is None
+    if parsed_end_date.year == 9999:
+        end_date = None
+
+    return {
+        "start_date": start_date,
+        "end_date": end_date,
+        "number_of_machines": number_of_machines,
+    }
+
+
+def get_machine_type(product_id: str) -> Optional[str]:
+    if "virtual" in product_id:
+        return "virtual"
+    if "physical" in product_id:
+        return "physical"
+    if "desktop" in product_id:
+        return "desktop"
+
+    return None
+
+
+def is_trialling_user_subscription(items: List[ContractItem]) -> bool:
+    return len(items) == 1 and items[0].reason == "trial_started"
+
+
+def get_user_subscription_statuses(
+    type: str,
+    end_date: str = None,
+    subscriptions: List[Subscription] = None,
+    listing: Listing = None,
+) -> dict:
+    statuses = {
+        "is_upsizeable": False,
+        "is_downsizeable": False,
+        "is_cancellable": False,
+        "is_cancelled": False,
+        "is_expiring": False,
+        "is_in_grace_period": False,
+        "is_expired": False,
+        "is_trialled": False,
+        "is_renewable": False,
+    }
+
+    if type == "free":
+        return statuses
+
+    date_statuses = get_date_statuses(end_date)
+    statuses["is_expiring"] = date_statuses["is_expiring"]
+    statuses["is_in_grace_period"] = date_statuses["is_in_grace_period"]
+    statuses["is_expired"] = date_statuses["is_expired"]
+
+    subscriptions = subscriptions or []
+    if has_pending_purchases(subscriptions) or statuses["is_expired"]:
+        return statuses
+
+    if type == "trial":
+        statuses["is_trialled"] = True
+
+    if type == "yearly":
+        statuses["is_upsizeable"] = True
+
+    if type == "monthly":
+        is_cancelled = is_user_subscription_cancelled(listing, subscriptions)
+        statuses["is_cancelled"] = is_cancelled
+
+        if not is_cancelled:
+            statuses["is_upsizeable"] = True
+            statuses["is_downsizeable"] = True
+            statuses["is_cancellable"] = True
+
+    return statuses
+
+
+def get_date_statuses(end_date: str) -> dict:
+    parsed_end_date = parse(end_date)
+    time_now = datetime.utcnow().replace(tzinfo=pytz.utc)
+    delta_till_expiry = time_now - parsed_end_date
+    days_till_expiry = delta_till_expiry.days
+
+    is_expiring_start = 30
+    is_expiring_end = 0
+    grace_period_end = -14
+
+    is_expiring = is_expiring_start > days_till_expiry >= is_expiring_end
+    is_in_grace_period = is_expiring_end > days_till_expiry >= grace_period_end
+    is_expired = grace_period_end < days_till_expiry
+
+    return {
+        "is_expiring": is_expiring,
+        "is_in_grace_period": is_in_grace_period,
+        "is_expired": is_expired,
+    }
+
+
+def get_pending_purchases(subscriptions: List[Subscription]) -> List:
+    pending_purchases = []
+    for subscription in subscriptions:
+        purchases_ids = subscription.pending_purchases
+        if purchases_ids and len(purchases_ids) > 0:
+            pending_purchases.extend(purchases_ids)
+
+    return pending_purchases
+
+
+def has_pending_purchases(subscriptions: List[Subscription]) -> bool:
+    return len(get_pending_purchases(subscriptions=subscriptions)) > 0
+
+
+def is_user_subscription_cancelled(
+    listing: Listing, subscriptions: List[Subscription]
+) -> bool:
+    listing_found = False
+    for subscription in subscriptions:
+        for item in subscription.items:
+            if item.product_listing_id == listing.id:
+                listing_found = True
+
+            break
+
+    is_cancelled = True if not listing_found else False
+
+    return is_cancelled
+
+
 def to_dict(structure, class_key=None):
     """Converts structure to dictionary
 
