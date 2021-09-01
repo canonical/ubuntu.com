d556eb2b25dccc0725834d2e72c98b5cc0217a8e
Peter Makowski <peter@makowski.uk>
2021-09-01 15:57:45 +0200

    add advantage/users route - add advantage_account_users_view - create empty React app mounted at #advantage-account-users-app - add Account users child item to navigation

--- build.js
+++ build.js
@@ -18,6 +18,7 @@ let entries = {
   chassisAnimation: "./static/js/src/chassis-animation.js",
   cve: "./static/js/src/cve/cve.js",
   productSelector: "./static/js/src/advantage/subscribe/product-selector.js",
+  advantageAccountUsers: "./static/js/src/advantage/users/app.jsx",
   openstackChart: "./static/js/src/openstack-chart.js",
   uaSubscribe: "./static/js/src/advantage/subscribe/react/app.jsx",
   "cloud-price-slider": "./static/js/src/cloud-price-slider.js",
