16a6a6ea6f5725a9ec371a7d74345538ec36fc66
petesfrench <peterfrench94@gmail.com>
2021-09-01 17:27:07 +0200

    Add table

--- static/js/src/openstack-deployment-chart.js
+++ static/js/src/openstack-deployment-chart.js
@@ -3,73 +3,76 @@ fetch("/static/js/data/openstack-deployment-stats.json")
   .then((data) => {
     data.stats.sort((a, b) => b.percentage - a.percentage);
     drawChart(data.stats);
-    drawTable(data);
+    drawTable(data.stats);
   });
 
 const drawChart = (data) => {
-  console.log(data);
-  // Get positions for each data object
-  const piedata = d3.pie().value(d => d.percentage)(data);
-  // Define arcs for graphing
-  const arc = d3.arc().innerRadius(110).outerRadius(200);
+  const piedata = d3.pie().value((d) => d.percentage)(data);
+
+  const arc = d3.arc().innerRadius(90).outerRadius(180);
 
   const colors = d3.scaleOrdinal(["#E95520", "#77216F", "#AEA79F"]);
 
-  // Define the size and position of svg
-  const svg = d3.select("#openstack-pie-chart")
-          .append('svg')
-          .attr('width', 600)
-          .attr('height', 600)
-          // .style('background-color','yellow')
-          .append('g')
-            .attr('transform','translate(300,300)');
+  const svg = d3
+    .select("#openstack-pie-chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+    .append("g")
+    .attr("transform", "translate(230,230)");
 
-  // Add tooltip
-  const tooldiv = d3.select('#openstack-pie-chart')
-            .append('div')
-            .style('visibility','hidden')
-            .style('position','absolute')
-            .style('background-color', '#111')
-            .style('color', '#fff')
-            .style('border-radius', '0.125rem')
-            .style('border', '0')
-            .style('padding', '0.5rem 1rem');
-  // Draw pie
-  svg.append('g')
-    .selectAll('path')
+  const tooltip = d3
+    .select("#openstack-pie-chart")
+    .append("div")
+    .style("visibility", "hidden")
+    .style("position", "absolute")
+    .style("background-color", "#111")
+    .style("color", "#fff")
+    .style("border-radius", "0.125rem")
+    .style("border", "0")
+    .style("padding", "0.5rem 1rem");
+
+  svg
+    .append("g")
+    .selectAll("path")
     .data(piedata)
-    .join('path')
-      .attr('d', arc)
-      .attr('fill',(d,i)=>colors(i))
-      .attr('stroke', 'white')
-      .on('mouseover', (e,d)=>{
-        console.log(e);
-        console.log(d);
-
-        tooldiv.style('visibility','visible')
-            .text(`${d.data.name}:` + ' ' + `${d.data.percentage}%`);
-      })
-      .on('mousemove', (e,d)=>{
-        tooldiv.style('top', (e.pageY-50) + 'px')
-            .style('left', (e.pageX-50) + 'px');
-      })
-      .on('mouseout',()=>{
-        tooldiv.style('visibility','hidden');
-      });
+    .join("path")
+    .attr("d", arc)
+    .attr("fill", (d, i) => colors(i))
+    .attr("stroke", "white")
+    .on("mouseover", (e, d) => {
+      tooltip
+        .style("visibility", "visible")
+        .text(`${d.data.name}:` + " " + `${d.data.percentage}%`);
+    })
+    .on("mousemove", (e, d) => {
+      tooltip
+        .style("top", e.pageY - 50 + "px")
+        .style("left", e.pageX - 50 + "px");
+    })
+    .on("mouseout", () => {
+      tooltip.style("visibility", "hidden");
+    });
 };
 
-    // .range([
-    //   "#E95420",
-    //   "#FDEEE9",
-    //   "#FBDDD2",
-    //   "#F8CCBC",
-    //   "#F6BBA6",
-    //   "#F4AA90",
-    //   "#F29879",
-    //   "#F08763",
-    //   "#ED764D",
-    //   "#EB6536",
-    // ]);
-    //.range(["#E95520", "#77216F", "#AEA79F"]);
+const drawTable = (data) => {
+  const tableHeadings = `
+  <thead>
+    <tr>
+      <th>Company</th>
+      <th class="u-align--right">OpenStack coverage</th>
+      <th colspan="2"></th>
+    </tr>
+  </thead>`;
+  let tableContent = ``;
+  data.forEach((d) => {
+    tableContent += `<tr>
+        <td>${d.name}</td>
+        <td class="u-align--right">${d.percentage}</td>
+        <td colspan="2"></td>
+      </tr>`;
+  });
 
-const drawTable = (data) => {};
+  const tableContainer = document.getElementById("openstack-table");
+  tableContainer.innerHTML = `<TABLE> ${tableHeadings} <tbody> ${tableContent} </tbody> </TABLE>`;
+};
