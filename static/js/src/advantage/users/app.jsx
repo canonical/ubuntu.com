4055bb865d2726ed8a2b7b78a5aa04d5f90de506
Peter Makowski <peter@makowski.uk>
2021-09-02 13:48:31 +0200

    add organisation title

--- static/js/src/advantage/users/app.jsx
+++ static/js/src/advantage/users/app.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Organisation from "./components/Organisation";
 
 function App() {
   return (
     <div>
-      <section className="p-strip">
+      <div className="p-strip">
         <div className="row">
           <div className="col-12">
             <h1>Account users</h1>
           </div>
+        </div>
+      </div>
+      <section>
+        <div className="row">
+          <Organisation name="myOrganisation" />
         </div>
       </section>
     </div>
---
+++ static/js/src/advantage/users/components/Organisation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Organisation = ({ name }) => (
+  <>
+    <p className="p-text--x-small-capitalised">organisation</p>
+    <p className="p-heading--3 u-no-padding--top">{name}</p>
+  </>
+);
+
+export default Organisation;
