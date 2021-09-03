bbc222dfeb9fa5ffe4f6c3f0ebbb77106d2ea65b
Peter Makowski <peter@makowski.uk>
2021-09-03 11:00:05 +0200

    add mock data

--- static/js/src/advantage/users/app.jsx
+++ static/js/src/advantage/users/app.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AccountUsers from "./AccountUsers";
 
+// TODO: replace with real data once API integration is complete
+const mockData = {
+  organisationName: "myOrganisation",
+};
+
 function App() {
-  return <AccountUsers />;
+  return <AccountUsers organisationName={mockData.organisationName} />;
 }
 
 ReactDOM.render(
