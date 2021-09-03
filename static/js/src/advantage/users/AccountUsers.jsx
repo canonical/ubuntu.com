4df664aaca7bb60a66575e232b2631aa70817094
Peter Makowski <peter@makowski.uk>
2021-09-03 10:34:01 +0200

    organise components

---
+++ static/js/src/advantage/users/AccountUsers.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Organisation from "./components/Organisation";
+
+const AccountUsers = ({ organisationName }) => {
+  return (
+    <div>
+      <div className="p-strip">
+        <div className="row">
+          <div className="col-12">
+            <h1>Account users</h1>
+          </div>
+        </div>
+      </div>
+      <section className="p-strip u-no-padding--top">
+        <div className="row">
+          <div className="col-6">
+            <Organisation name={organisationName} />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AccountUsers;
--- static/js/src/advantage/users/app.jsx
+++ static/js/src/advantage/users/app.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Organisation from "./components/Organisation";
+import AccountUsers from "./AccountUsers";
 
 function App() {
-  return (
-    <div>
-      <div className="p-strip">
-        <div className="row">
-          <div className="col-12">
-            <h1>Account users</h1>
-          </div>
-        </div>
-      </div>
-      <section>
-        <div className="row">
-          <Organisation name="myOrganisation" />
-        </div>
-      </section>
-    </div>
-  );
+  return <AccountUsers />;
 }
 
 ReactDOM.render(
