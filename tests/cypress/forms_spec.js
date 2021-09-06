cb13f68add6ee520b06b82898c4cd16d741b626c
Beth Collins <bethcollinsliv92@gmail.com>
2021-09-06 15:32:40 +0100

    Fix failing engage page test and add token

--- tests/cypress/forms_spec.js
+++ tests/cypress/forms_spec.js
@@ -12,18 +12,26 @@ const getIframeBody = () => {
     .then(cy.wrap);
 };
 
-context("Marketo form", () => {
+context("Marketo forms", () => {
   beforeEach(() => {
-    cy.server();
-    cy.route("POST", "/marketo/submit").as("captureLead");
+    cy.intercept(
+      { method: "POST", url: "/marketo/submit",},
+      { 
+          headers: {
+          "Client Id": Cypress.env("Client Id"),
+          "Client Secret": Cypress.env("Client Secret"),
+          "Authorized User": Cypress.env("Authorized User"),
+          "Token": Cypress.env("Token"),
+        },
+      }
+    ).as("captureLead");
     cy.setCookie("_cookies_accepted", "all");
   });
 
   afterEach(() => {
-    cy.wait("@captureLead").should((xhr) => {
-      expect(xhr.method).to.equal("POST");
-      //This isn't returning a 200 as we don't have the API key available
-      expect(xhr.status).to.equal(400);
+    cy.wait("@captureLead").then(({ request, response }) => {
+      expect(request.method).to.equal("POST");
+      expect(response.statusCode).to.equal(200);
     });
   });
 
@@ -48,35 +56,39 @@ context("Marketo form", () => {
     cy.get("#mktoForm_1266")
       .submit()
       .should((page) => {
-        expect(page[0].action).to.include("/marketo/submit");
+        expect(page[0].action).to.include("marketo/submit");
       });
+    cy.findByText("Thank you").should("be.visible");
   });
 
-  //Commented out this test for now as there is an error on initial submit of the engage page which is causing this test to fail.
+  it("/engage/anbox-cloud-gaming-whitepaper", () => {
+    //This exception can be removed when this issue is resolved: https://github.com/canonical-web-and-design/web-squad/issues/4345
+    cy.on("uncaught:exception", () => {
+      return false;
+    });
+    cy.visit("/engage/anbox-cloud-gaming-whitepaper");
 
-  // it("/engage/anbox-cloud-gaming-whitepaper", () => {
-  //   cy.visit("/engage/anbox-cloud-gaming-whitepaper");
+    cy.get('input[name="firstName"]').type("Test");
+    cy.get('input[name="lastName"]').type("Test");
+    cy.get('input[name="company"]').type("Test");
+    cy.get('input[name="title"]').type("Test");
+    cy.get('input[name="email"]').type("test@test.com");
+    cy.get('input[name="phone"]').type("000000000");
 
-  //   cy.get('input[name="firstName"]').type("Test");
-  //   cy.get('input[name="lastName"]').type("Test");
-  //   cy.get('input[name="company"]').type("Test");
-  //   cy.get('input[name="title"]').type("Test");
-  //   cy.get('input[name="email"]').type("test@test.com");
-  //   cy.get('input[name="phone"]').type("000000000");
+    getIframeBody().find(".rc-anchor-content").click();
 
-  //   getIframeBody().find(".rc-anchor-content").click();
+    cy.wait(3000); // eslint-disable-line
+    cy.get("#mktoForm_3494")
+      .submit()
+      .should((page) => {
+        expect(page[0].action).to.include("/marketo/submit");
+      });
 
-  //   cy.wait(3000); // eslint-disable-line
-  //   cy.get("#mktoForm_3494")
-  //     .submit()
-  //     .should((page) => {
-  //       expect(page[0].action).to.include("/marketo/submit");
-  //     });
-  // });
+    cy.findByText("Thank you").should("be.visible");
+  });
 
   it("should open pop up model and successfully complete contact form then submit to Marketo", () => {
-    cy.server();
-    cy.route("POST", "/marketo/submit").as("captureLead");
+    cy.intercept("POST", "/marketo/submit").as("captureLead");
     cy.visit("/openstack#get-in-touch");
     cy.get(
       ".p-modal__dialog .js-pagination--1 .pagination__link--next"
@@ -101,7 +113,7 @@ context("Marketo form", () => {
     cy.get(".p-modal__close").click();
   });
 
-  it("/download/server/thank-you", () => {
+  it("should successfully complete download server", () => {
     cy.visit("/download/server/s390x");
 
     cy.get('input[name="firstName"]').type("Test");
