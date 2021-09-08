b5474ad2c230ed62dfb10377ca6d9e3db341a522
Peter Makowski <peter@makowski.uk>
2021-09-08 15:36:19 +0200

    add FocusTrap

---
+++ static/js/src/advantage/users/components/FocusTrap.tsx
@@ -0,0 +1,21 @@
+import React, { useRef, useEffect } from "react";
+import { createFocusTrap, FocusTrap } from "focus-trap";
+
+const FocusTrap: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const trapRef = useRef(null);
+  const focusTrap = useRef<FocusTrap | null>(null);
+
+  useEffect(() => {
+    if (trapRef.current) {
+      focusTrap.current = createFocusTrap(trapRef.current);
+      focusTrap.current.activate();
+    }
+    return () => {
+      focusTrap.current?.deactivate();
+    };
+  }, [trapRef]);
+
+  return <div ref={trapRef}>{children}</div>;
+};
+
+export default FocusTrap;
