3d94cd83da3de8b19320b84d2efe9f6897f83619
Peter Makowski <peter@makowski.uk>
2021-09-13 16:29:19 +0200

    add getTextContentMatcher test util

---
+++ static/js/src/advantage/tests/utils.ts
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+
+const getTextContentMatcher = (textMatch) => (content, node) => {
+  const hasText = (node) =>
+    node.textContent === textMatch || node.textContent.match(textMatch);
+  const nodeHasText = hasText(node);
+  const childrenDontHaveText = Array.from(node.children).every(
+    (child) => !hasText(child)
+  );
+
+  return nodeHasText && childrenDontHaveText;
+};
+
+// use only if a regular "getByText" query is not working, this is much slower
+// https://github.com/testing-library/dom-testing-library/issues/410#issuecomment-797486513
+export const getByTextContent = (text) =>
+  screen.getByText(getTextContentMatcher(text));
