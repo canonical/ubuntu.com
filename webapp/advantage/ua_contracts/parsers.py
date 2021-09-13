154512c7132950c405b945160db532c5ac1bbd22
Albert Kolozsvari <albertartk@gmail.com>
2021-09-13 08:33:35 +0100

    Add user json parsers with tests

--- webapp/advantage/ua_contracts/parsers.py
+++ webapp/advantage/ua_contracts/parsers.py
@@ -8,6 +8,7 @@ from webapp.advantage.ua_contracts.primitives import (
     SubscriptionItem,
     Account,
     Renewal,
+    User,
 )
 from webapp.advantage.models import Listing, Product
 
@@ -200,3 +201,25 @@ def parse_subscriptions(raw_subscriptions: Dict) -> List[Subscription]:
         parse_subscription(raw_subscription)
         for raw_subscription in raw_subscriptions
     ]
+
+
+def parse_user(raw_user: Dict) -> User:
+    user = User(
+        display_name=raw_user.get("displayName"),
+        name=raw_user.get("name"),
+        email=raw_user.get("email"),
+        id=raw_user.get("id"),
+        last_login=raw_user.get("lastLogin"),
+        first_login=raw_user.get("firstLogin"),
+        verified=raw_user.get("verified"),
+    )
+
+    user_role_on_account = raw_user.get("userRoleOnAccount")
+    if user_role_on_account:
+        user.set_user_role_on_account(user_role_on_account)
+
+    return user
+
+
+def parse_users(raw_users: List) -> List[User]:
+    return [parse_user(raw_user) for raw_user in raw_users]
