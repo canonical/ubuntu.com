83039b92883bc044935a58825532ed8152dcfc94
Huw Wilkins <huw.wilkins@canonical.com>
2021-09-17 10:51:56 +1000

    Display a message if the subscriptions can't be loaded.

--- static/js/src/advantage/react/components/Subscriptions/Content/Content.test.tsx
+++ static/js/src/advantage/react/components/Subscriptions/Content/Content.test.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { mount } from "enzyme";
 
+import * as contracts from "advantage/api/contracts";
 import Content from "./Content";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { UserSubscription } from "advantage/api/types";
 import { userSubscriptionFactory } from "advantage/tests/factories/api";
+import { act } from "react-dom/test-utils";
 
 describe("Content", () => {
   let queryClient: QueryClient;
   let contract: UserSubscription;
+  let getUserSubscriptionsSpy: jest.SpyInstance;
 
   beforeEach(async () => {
-    queryClient = new QueryClient();
+    getUserSubscriptionsSpy = jest.spyOn(contracts, "getUserSubscriptions");
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
     contract = userSubscriptionFactory.build();
     queryClient.setQueryData("userSubscriptions", [contract]);
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 
   it("renders", () => {
@@ -24,5 +37,25 @@ describe("Content", () => {
     );
     expect(wrapper.find("SubscriptionList").exists()).toBe(true);
     expect(wrapper.find("SubscriptionDetails").exists()).toBe(true);
+  });
+
+  it("displays a message if the contracts can't be loaded", async () => {
+    // Mock the hook to simulate a failed request.
+    getUserSubscriptionsSpy.mockImplementation(() => Promise.reject("Uh oh"));
+    // Remove the current queries so that the hook attempts to refetch the subs.
+    queryClient.removeQueries("userSubscriptions");
+    const wrapper = mount(
+      <QueryClientProvider client={queryClient}>
+        <Content />
+      </QueryClientProvider>
+    );
+    // Use act and wrapper.update to force waiting  for the component to finish rendering.
+    await act(async () => {});
+    wrapper.update();
+    expect(
+      wrapper.find("Notification[data-test='loading-error']").exists()
+    ).toBe(true);
+    expect(wrapper.find("SubscriptionList").exists()).toBe(false);
+    expect(wrapper.find("SubscriptionDetails").exists()).toBe(false);
   });
 });
--- static/js/src/advantage/react/components/Subscriptions/Content/Content.tsx
+++ static/js/src/advantage/react/components/Subscriptions/Content/Content.tsx
@@ -1,4 +1,8 @@
-import { Card } from "@canonical/react-components";
+import {
+  Card,
+  Notification,
+  NotificationSeverity,
+} from "@canonical/react-components";
 import { useUserSubscriptions } from "advantage/react/hooks";
 import { useScrollIntoView } from "advantage/react/hooks/useScrollIntoView";
 import React, { useCallback, useEffect, useState } from "react";
@@ -13,7 +17,7 @@ const Content = () => {
   const [scrollTargetRef, scrollIntoView] = useScrollIntoView<HTMLDivElement>(
     20
   );
-  const { data: allSubscriptions, isLoading } = useUserSubscriptions();
+  const { data: allSubscriptions, isError, isLoading } = useUserSubscriptions();
   const onSetActive = useCallback(
     (token: SelectedId) => {
       // Only set the token if it has changed to another token. The selected
@@ -37,6 +41,19 @@ const Content = () => {
       setSelectedId(allSubscriptions[0].contract_id);
     }
   }, [selectedId, setSelectedId, allSubscriptions, isLoading]);
+
+  if (isError) {
+    return (
+      <Notification
+        data-test="loading-error"
+        severity={NotificationSeverity.NEGATIVE}
+        title="Loading failed:"
+      >
+        Your subscriptions could not be loaded. Please refresh the page or try
+        again later.
+      </Notification>
+    );
+  }
 
   return (
     <Card className="u-no-margin--bottom u-no-padding p-subscriptions__card">
