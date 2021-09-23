a8ca5b46bedb258e26b2659af9a5b032b9e9b96c
Peter Makowski <peter@makowski.uk>
2021-09-23 10:36:29 +0200

    add sentry to advantage/users - add allowUrls, tracingOrigins to advantage/subscribe

--- static/js/src/advantage/users/app.tsx
+++ static/js/src/advantage/users/app.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { Integrations } from "@sentry/tracing";
+
 import AccountUsers from "./AccountUsers";
 import { requestAccountUsers } from "./api";
 
@@ -17,11 +20,22 @@ const queryClient = new QueryClient({
   },
 });
 
+Sentry.init({
+  dsn: "https://0293bb7fc3104e56bafd2422e155790c@sentry.is.canonical.com//13",
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["ubuntu.com"],
+    }),
+  ],
+  allowUrls: ["ubuntu.com"],
+});
+
 const AccountUsersWithQuery = () => {
   const { status, data } = useQuery("accountUsers", async () => {
     const res = await requestAccountUsers();
     return res;
   });
+
   return (
     <div>
       <div className="p-strip">
@@ -34,6 +48,20 @@ const AccountUsersWithQuery = () => {
       <section className="p-strip u-no-padding--top">
         <div className="row">
           <div className="col-12">
+            {status === "error" ? (
+              <div className="row">
+                <div className="col-6">
+                  <div className={`p-notification--negative`}>
+                    <div className="p-notification__content">
+                      <h5 className="p-notification__title">Error</h5>
+                      <p className="p-notification__message" role="alert">
+                        An unknown error has occurred. Please try again.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : null}
             {status === "loading" ? (
               <div className="u-no-margin--bottom p-card">
                 <div className="p-card__content">
@@ -60,9 +88,11 @@ const AccountUsersWithQuery = () => {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AccountUsersWithQuery />
-    </QueryClientProvider>
+    <Sentry.ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <AccountUsersWithQuery />
+      </QueryClientProvider>
+    </Sentry.ErrorBoundary>
   );
 }
 
