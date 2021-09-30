035815ebc6b48d4eed24b8be37bbb58a5f93466a
Morgan Robertson <mrgnr@pm.me>
2021-09-30 12:25:53 +0200

    Reactify microcerts table; use dummy data.

--- static/js/src/cube/components/MicrocertificationsTable/MicrocertificationsTable.tsx
+++ static/js/src/cube/components/MicrocertificationsTable/MicrocertificationsTable.tsx
@@ -1,109 +1,204 @@
 import React from "react";
-import { Button, MainTable, Icon, ICONS } from "@canonical/react-components";
+import { Button, ContextualMenu, MainTable } from "@canonical/react-components";
+
 const TableView = () => {
+  const enum Status {
+    Enrolled = "Enrolled",
+    NotEnrolled = "Not Enrolled",
+    Passed = "Passed",
+    Failed = "Failed",
+    InProgress = "In Progress",
+  }
+
+  // TODO: replace dummy data with data from endpoint
+  const modules = [
+    {
+      badgeURL: "https://assets.ubuntu.com/v1/9ef4a092-Architecture.svg",
+      name: "Ubuntu System Architecture",
+      topics: [
+        "Review and record relevant system information",
+        "Manage kernels and devices",
+        "Make administrative changes to devices and hardware",
+      ],
+      status: Status.Enrolled,
+    },
+    {
+      badgeURL: "https://assets.ubuntu.com/v1/c19e9931-Packages.svg",
+      name: "Managing Packages in Ubuntu",
+      topics: [
+        "Assess package information",
+        "Use and configure snaps",
+        "Manage Debian packages",
+        "Manage executables with alternatives",
+      ],
+      status: Status.NotEnrolled,
+    },
+    {
+      badgeURL: "https://assets.ubuntu.com/v1/43f7d2a3-Commands.svg",
+      name: "Using Command Line Tools",
+      topics: [
+        "Work with file contents",
+        "Process data using the command line",
+        "Use essential utilities",
+      ],
+      status: Status.Passed,
+    },
+    {
+      badgeURL: "https://assets.ubuntu.com/v1/093b57b6-Devices+%26+files.svg",
+      name: "Linux Devices and Filesystems",
+      topics: [
+        "Configure volumes and subvolumes",
+        "Create and configure partitions",
+        "Encrypt disks",
+        "Manage and configure block devices, Device Mapper, and filesystems",
+      ],
+      status: Status.Failed,
+    },
+    {
+      badgeURL: "https://assets.ubuntu.com/v1/093b57b6-Devices+%26+files.svg",
+      name: "Bash Shell Scripting",
+      topics: [
+        "Create, check, and test files",
+        "Work with logs",
+        "Monitor and gather information from other systems",
+        "Automate tasks with loops and conditionals",
+      ],
+      status: Status.InProgress,
+    },
+  ];
+
+  const copyBadgeUrl = (badgeUrl: string) => {
+    const el = document.createElement("textarea");
+    el.value = badgeUrl;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
+
+  const renderModuleName = (name: string, topics: Array<string>) => (
+    <div>
+      <h3 className="p-heading--5 u-no-margin u-no-padding">{name}</h3>
+      <p className="u-text--muted u-hide--small">
+        <small>{topics.length} topics</small>
+      </p>
+    </div>
+  );
+
+  const renderTopics = (topics: Array<string>) => (
+    <ul className="p-list u-no-margin" style={{ whiteSpace: "normal" }}>
+      {topics.map((topic) => (
+        <li key={topic} className="p-list__item">
+          {topic}
+        </li>
+      ))}
+    </ul>
+  );
+
+  const renderStatus = (status: Status) => (
+    <p className="u-no-padding--top">
+      <>
+        {status === Status.Passed ? (
+          <i className="p-icon--success" />
+        ) : status === Status.Failed ? (
+          <i className="p-icon--error" />
+        ) : null}
+        {status}
+      </>
+    </p>
+  );
+
+  const renderActions = (
+    status: Status,
+    badgeURL: string,
+    moduleName: string
+  ) => (
+    <div className="p-table--cube__contents u-no-padding--top u-align--right">
+      {status === Status.Enrolled ? (
+        <>
+          <Button className="u-no-margin--right">Prepare</Button>
+          <Button appearance={"positive"}>Take</Button>
+        </>
+      ) : status === Status.InProgress ? (
+        <Button appearance={"positive"}>Resume</Button>
+      ) : status === Status.Passed ? (
+        <ContextualMenu
+          hasToggleIcon
+          toggleLabel="Share"
+          links={[
+            {
+              children: "Share via LinkedIn",
+              element: "a",
+              href: `https://www.linkedin.com/shareArticle?mini=true&amp;url=${badgeURL}`,
+            },
+            {
+              children: "Share via Twitter",
+              element: "a",
+              href:
+                `https://twitter.com/share?text=I earned ${moduleName} ` +
+                `from @Canonical. Now I’m one step closer to becoming a ` +
+                `Certified @Ubuntu Engineer. Learn more about ` +
+                `Canonical’s available microcerts and CUBE 2020 at ` +
+                `https://www.ubuntu.com/cube.&amp;url=${badgeURL}&amp;` +
+                `hashtags=CUBE2020`,
+            },
+            {
+              children: "Copy to clipboard",
+              onClick: () => copyBadgeUrl(badgeURL),
+            },
+          ]}
+        />
+      ) : (
+        <Button appearance={"positive"}>Purchase</Button>
+      )}
+    </div>
+  );
+
   return (
     <MainTable
       responsive
       className="p-table--cube--grid"
       headers={[
-        {
-          content: "#",
-        },
-        {
-          content: "",
-        },
-        {
-          content: "Module",
-        },
-        {
-          content: "Topics",
-        },
-        {
-          content: "Status",
-          className: "p-table__cell--icon-placeholder",
-        },
-        {
-          content: "Action",
-          className: "u-align--right",
-        },
+        { content: "#" },
+        { content: "" },
+        { content: "Module" },
+        { content: "Topics" },
+        { content: "Status", className: "p-table__cell--icon-placeholder" },
+        { content: "Action", className: "u-align--right" },
       ]}
-      rows={[
-        {
-          key: 1,
+      rows={modules.map(({ badgeURL, name, topics, status }, index) => {
+        return {
+          key: index + 1,
           columns: [
             {
-              content: "1",
-              className: "u-text--muted",
+              content: <span className="u-text--muted">{index + 1}</span>,
+              "aria-label": "Module number",
             },
             {
-              content: (
-                <img
-                  src="https://assets.ubuntu.com/v1/9ef4a092-Architecture.svg"
-                  alt=""
-                />
-              ),
+              content: <img src={badgeURL} alt="" />,
               className: "p-table--cube--grid__module-logo",
+              "aria-label": "Badge",
             },
             {
-              content: "Ubuntu System Architecture",
+              content: renderModuleName(name, topics),
               "aria-label": "Module",
             },
             {
-              content: "Review and record relevant system information",
+              content: renderTopics(topics),
               "aria-label": "Topics",
             },
             {
-              content: (
-                <>
-                  <Icon name={ICONS.success} />
-                  Passed
-                </>
-              ),
+              content: renderStatus(status),
               className: "p-table__cell--icon-placeholder",
-              "aria-label": "status",
+              "aria-label": "Status",
             },
-            {
-              content: <Button appearance={"positive"}>Purchase</Button>,
-              className: "u-align--right",
-            },
+            { content: renderActions(status, badgeURL, name) },
           ],
-        },
-        {
-          key: 2,
-          columns: [
-            {
-              content: "2",
-              className: "u-text--muted",
-            },
-            {
-              content: (
-                <img
-                  src="https://assets.ubuntu.com/v1/c19e9931-Packages.svg"
-                  alt=""
-                />
-              ),
-              className: "p-table--cube--grid__module-logo",
-            },
-            {
-              content: "Ubuntu System Architecture",
-              "aria-label": "Module",
-            },
-            {
-              content: "Review and record relevant system information",
-              "aria-label": "Topics",
-            },
-            {
-              content: "Not Enrolled",
-              className: "p-table__cell--icon-placeholder",
-              "aria-label": "status",
-            },
-            {
-              content: <Button appearance={"positive"}>Purchase</Button>,
-              className: "u-align--right",
-            },
-          ],
-        },
-      ]}
+        };
+      })}
     />
   );
 };
