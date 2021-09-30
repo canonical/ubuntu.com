357a090c05a08157fbf069a5e693fa1858b1fc01
Peter Makowski <peter@makowski.uk>
2021-09-30 09:34:10 +0200

    update DeleteConfirmationModal test

--- static/js/src/advantage/users/components/DeleteConfirmationModal/DeleteConfirmationModal.test.tsx
+++ static/js/src/advantage/users/components/DeleteConfirmationModal/DeleteConfirmationModal.test.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { getByTextContent } from "../../../tests/utils";
 
 import DeleteConfirmationModal from "./DeleteConfirmationModal";
-import { mockUser } from "../../mockData";
+import { mockUser, mockAccountId } from "../../mockData";
+import { requestDeleteUser } from "../../api";
+
+jest.mock("../../api");
+
+const renderWithQueryClient = (Component: React.ReactNode) => {
+  const queryClient = new QueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{Component}</QueryClientProvider>
+  );
+};
 
 it("displays confirmation message correctly", () => {
-  const mockHandleConfirmDelete = jest.fn();
+  const onAfterDeleteSuccess = jest.fn();
   const mockHandleClose = jest.fn();
 
   render(
     <DeleteConfirmationModal
+      accountId={mockAccountId}
       user={mockUser}
-      handleConfirmDelete={mockHandleConfirmDelete}
+      onAfterDeleteSuccess={onAfterDeleteSuccess}
       handleClose={mockHandleClose}
     />
   );
@@ -26,17 +38,19 @@ it("displays confirmation message correctly", () => {
   ).toBeVisible();
 });
 
-it("makes a correct call to handleConfirmDelete", () => {
-  const mockHandleConfirmDelete = jest.fn(Promise.resolve);
+it("makes a correct call to requestDeleteUser", () => {
+  const onAfterDeleteSuccess = jest.fn();
   const mockHandleClose = jest.fn();
 
-  render(
+  const { container } = renderWithQueryClient(
     <DeleteConfirmationModal
+      accountId={mockAccountId}
       user={mockUser}
-      handleConfirmDelete={mockHandleConfirmDelete}
+      onAfterDeleteSuccess={onAfterDeleteSuccess}
       handleClose={mockHandleClose}
     />
   );
   userEvent.click(screen.getByText("Yes, remove user"));
-  expect(mockHandleConfirmDelete).toHaveBeenCalledWith(mockUser.email);
+  expect(container.firstChild).toMatchInlineSnapshot();
+  expect(requestDeleteUser).toHaveBeenCalledTimes(1);
 });
