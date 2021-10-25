e9d16b2e1c8394e36724b786669e2c310fbe22d3
Beth Collins <bethcollinsliv92@gmail.com>
2021-10-25 14:22:03 +0100

    Add condition for vendor section

--- static/js/src/certified-search-results.js
+++ static/js/src/certified-search-results.js
@@ -112,17 +112,19 @@ function updateResultsPerPage() {
 
 function toggleVendorsList() {
   const vendorSection = document.querySelector("#vendor-section");
-  const vendorPanel = vendorSection.querySelector(".p-accordion__panel");
-  const toggleVendorButtons = document.querySelectorAll(".p-reveal-vendors");
-  toggleVendorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      window.scrollTo(0, 360);
-      vendorPanel.classList.toggle("is-collapsed");
-      toggleVendorButtons.forEach((button) => {
-        button.classList.toggle("u-hide");
+  if (vendorSection) {
+    const vendorPanel = vendorSection.querySelector(".p-accordion__panel");
+    const toggleVendorButtons = document.querySelectorAll(".p-reveal-vendors");
+    toggleVendorButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        window.scrollTo(0, 360);
+        vendorPanel.classList.toggle("is-collapsed");
+        toggleVendorButtons.forEach((button) => {
+          button.classList.toggle("u-hide");
+        });
       });
     });
-  });
+  }
 }
 
 function toggleVersionsList() {
