58d90e043112b192a103e269618d961cc433c6b4
Scott Mason Nash <sowasred2012@gmail.com>
2021-12-01 13:21:49 +0000

    remove global nav

--- static/js/src/core.js
+++ static/js/src/core.js
@@ -1,11 +1,4 @@
-import { createNav } from "@canonical/global-nav";
 import { cookiePolicy } from "@canonical/cookie-policy";
-
-// Initalise the global navigation.
-createNav({
-  showLogins: false,
-  hiring: "https://canonical.com/careers/start",
-});
 
 // Initalise the cookie policy notification.
 cookiePolicy();
