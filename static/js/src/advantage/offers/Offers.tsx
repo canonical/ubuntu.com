d888d8a476d9c67b3f9923afae06ee812d055aaf
ClementChaumel <clement.chaumel@gmail.com>
2021-12-15 10:31:46 +0100

    [Frontend] Sharable cart referral link - create new page Fixes canonical-web-and-design/commercial-squad#434

---
+++ static/js/src/advantage/offers/Offers.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col } from "@canonical/react-components";
+import OffersList from "./components/OffersList";
+
+const Offers = () => {
+  return (
+    <div className="p-strip--suru-topped">
+      <Row>
+        <Col size={12}>
+          <h1>Your Ubuntu Advantage Offers</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col size={12}>
+          <OffersList />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Offers;
---
+++ static/js/src/advantage/offers/components/OffersList.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const OffersList = () => {
+  const [offersList] = useState([]);
+
+  if (offersList.length < 1) {
+    return <p>You have no offers available.</p>;
+  }
+
+  return (
+    <>
+      {offersList.map((offer) => {
+        <p>{offer.name}</p>;
+      })}
+    </>
+  );
+};
+
+export default OffersList;
