dc7dece36e2c99323a276616d36c79c160d3c30d
minkyngkm <min.kim@canonical.com>
2022-01-04 11:56:46 +0000

    Update marketo endpoint

--- static/js/src/advantage/subscribe/react/components/BuyButton/BuyButton.tsx
+++ static/js/src/advantage/subscribe/react/components/BuyButton/BuyButton.tsx
@@ -201,10 +201,8 @@ const BuyButton = ({
 
       const request = new XMLHttpRequest();
       const formData = new FormData();
-      formData.append("munchkinId", "066-EOV-335");
       formData.append("formid", "3756");
-      formData.append("formVid", "3756");
-      formData.append("Email", userInfo?.customerInfo?.email);
+      formData.append("email", userInfo?.customerInfo?.email ?? "");
       formData.append("Consent_to_Processing__c", "yes");
       formData.append("GCLID__c", sessionData?.gclid || "");
       formData.append("utm_campaign", sessionData?.utm_campaign || "");
@@ -216,10 +214,7 @@ const BuyButton = ({
         isMarketingOptInChecked ? "yes" : "no"
       );
 
-      request.open(
-        "POST",
-        "https://app-sjg.marketo.com/index.php/leadCapture/save2"
-      );
+      request.open("POST", "/marketo/submit");
       request.send(formData);
 
       request.onreadystatechange = () => {
