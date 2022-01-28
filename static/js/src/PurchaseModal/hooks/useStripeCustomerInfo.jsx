f5a1dcbee52c0af0d1728f910cb380130d807e95
ClementChaumel <clement.chaumel@gmail.com>
2022-01-28 10:12:48 +0100

    Fetch email address through account.json

--- static/js/src/PurchaseModal/hooks/useStripeCustomerInfo.jsx
+++ static/js/src/PurchaseModal/hooks/useStripeCustomerInfo.jsx
@@ -6,18 +6,24 @@ const useStripeCustomerInfo = () => {
     "paymentModalUserInfo",
     async () => {
       if (!window.accountId) {
-        throw new Error("MISSING ACCOUNT_ID");
+        const response = await fetch(`/account.json${window.location.search}`, {
+          cache: "no-store",
+        });
+        const res = await response.json();
+        return {
+          customerInfo: {
+            email: res.account.email,
+            name: res.account.fullname,
+          },
+        };
       }
       const res = await getCustomerInfo(window.accountId);
-
       if (res.data.code) {
         throw new Error(res.data.message);
       }
       return res.data;
     },
-    {
-      enabled: !!window.accountId,
-    }
+    {}
   );
 
   return {
