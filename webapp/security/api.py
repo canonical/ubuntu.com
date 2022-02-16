885f4fe2207707ec6a663a146b494837cd3fc1d5
mtruj013 <mtruj013@fiu.edu>
2022-02-16 13:44:22 +0100

    fix indentation and whitespaces

--- webapp/security/api.py
+++ webapp/security/api.py
@@ -1,55 +1,42 @@
 from requests import Session
 
+
 class SecurityAPI:
-  def __init__(
-    self, session: Session, base_url: str, 
-  ):
-    self.session = session
-    self.base_url = base_url
-  
-  """
-  Defines get request set up, returns data if succesful, 
-  raises HTTP errors if not
-  """
-  def _get(
-    self, 
-    path: str,
-    params = {}
-  ):
-    uri=f"{self.base_url}{path}"
+    def __init__(
+        self,
+        session: Session,
+        base_url: str,
+    ):
+        self.session = session
+        self.base_url = base_url
 
+    def _get(self, path: str, params={}):
+        """
+        Defines get request set up, returns data if succesful,
+        raises HTTP errors if not
+        """
+        uri = f"{self.base_url}{path}"
 
-    response = self.session.get (
-      uri, params=params
-    )
+        response = self.session.get(uri, params=params)
 
-    response.raise_for_status()
+        response.raise_for_status()
 
-    return response
+        return response
 
-  """
-  Makes request for specific cve_id, 
-  returns json object if found  
-  """
-  def get_cve(
-    self,
-    id: str,
-  ):
-    return self._get(
-      f"cves/{id.upper()}.json"
-    ).json()
+    def get_cve(
+        self,
+        id: str,
+    ):
+        """
+        Makes request for specific cve_id,
+        returns json object if found
+        """
+        return self._get(f"cves/{id.upper()}.json").json()
 
-  """
-  Makes request for all releases with ongoing support, 
-  returns json object if found
-  """
-  def get_releases(
-    self,
-  ):
-    return self._get(
-      "releases.json"
-    ).json()
+    def get_releases(self):
+        """
+        Makes request for all releases with ongoing support,
+        returns json object if found
+        """
 
-    
-
-  
+        return self._get("releases.json").json()
