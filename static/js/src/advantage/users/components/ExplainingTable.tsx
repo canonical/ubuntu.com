60da8cc1b36228b6141396bafc652e1a0b561761
ClementChaumel <clement.chaumel@gmail.com>
2022-04-12 16:31:46 +0200

    Add explaining table

---
+++ static/js/src/advantage/users/components/ExplainingTable.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { MainTable, Icon, ICONS } from "@canonical/react-components";
+
+const ExplainingTable = () => (
+  <>
+    <h2 className="p-heading--3">Roles descriptions</h2>
+    <MainTable
+      headers={[
+        {
+          content: null,
+        },
+        {
+          content: "Manage users",
+          className: "u-align--center",
+        },
+        {
+          content: "Access tokens",
+          className: "u-align--center",
+        },
+        {
+          content: "Buy subscriptions",
+          className: "u-align--center",
+        },
+        {
+          content: "Payment & Invoices",
+          className: "u-align--center",
+        },
+        {
+          content: "Support Portal seat",
+          className: "u-align--center",
+        },
+      ]}
+      rows={[
+        {
+          columns: [
+            {
+              content: "Admin",
+              role: "rowheader",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              content: "Technical",
+              role: "rowheader",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              content: "Billing",
+              role: "rowheader",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.success} />,
+              className: "u-align--center",
+            },
+            {
+              content: <Icon name={ICONS.error} />,
+              className: "u-align--center",
+            },
+          ],
+        },
+      ]}
+      responsive
+    />
+  </>
+);
+
+export default ExplainingTable;
