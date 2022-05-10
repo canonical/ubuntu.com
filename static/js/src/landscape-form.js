9160940a2b92b896ae62ca1126bc8551f57c4fa5
mtruj013 <mtruj013@fiu.edu>
2022-05-10 17:50:04 +0200

    fix code format

--- static/js/src/landscape-form.js
+++ static/js/src/landscape-form.js
@@ -1,17 +1,13 @@
-// Stringifies non-standard form field value and adds it to "comments_from_lead_c" textarea on submission. 
+// Stringifies non-standard form field value and adds it to "comments_from_lead_c" textarea on submission.
 function contactPrefrence() {
-
-  const telephoneOption = document.getElementById('telephonePreferred');
-  const emailOption = document.getElementById('emailPreferred');
+  const telephoneOption = document.getElementById("telephonePreferred");
+  const emailOption = document.getElementById("emailPreferred");
   const textarea = document.getElementById("Comments_from_lead__c");
-  
-  if (telephoneOption.checked) {
 
+  if (telephoneOption.checked) {
     const text = document.getElementById("preferredContactTelephone").outerText;
     textarea.value += text;
-
-  } else if (emailOption.checked){
-
+  } else if (emailOption.checked) {
     const text = document.getElementById("preferredContactEmail").outerText;
     textarea.value += text;
   }
