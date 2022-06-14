ce2797a281a328cecdebcc5d3ae03d37cc6107bf
Anthony Dillon <me@anthonydillon.com>
2022-06-14 17:57:37 +0100

    Revert "Disable site restricted search"

--- webapp/app.py
+++ webapp/app.py
@@ -757,6 +757,7 @@ app.add_url_rule(
         site="ubuntu.com/server/docs",
         template_path="/server/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 
@@ -810,6 +811,7 @@ app.add_url_rule(
         site="ubuntu.com/ceph/docs",
         template_path="ceph/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 
@@ -831,6 +833,7 @@ app.add_url_rule(
         site="ubuntu.com/core/docs",
         template_path="/core/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 core_docs.init_app(app)
@@ -956,6 +959,7 @@ app.add_url_rule(
         site="ubuntu.com/openstack/docs",
         template_path="openstack/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 
@@ -982,6 +986,7 @@ app.add_url_rule(
         site="ubuntu.com/security/livepatch/docs",
         template_path="/security/livepatch/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 
@@ -1008,6 +1013,7 @@ app.add_url_rule(
         site="ubuntu.com/security/certifications/docs",
         template_path="/security/certifications/docs/search-results.html",
         search_engine_id=search_engine_id,
+        site_restricted_search=True,
     ),
 )
 
