a893e08658f5df42cd0178b9a217bb191244b285
ClementChaumel <clement.chaumel@gmail.com>
2022-06-15 13:46:47 +0200

    Comment out blender

--- static/js/src/advantage/subscribe/blender/BlenderPurchase.tsx
+++ static/js/src/advantage/subscribe/blender/BlenderPurchase.tsx
@@ -1,50 +1,49 @@
-import React from "react";
-import PurchaseModal from "../../../PurchaseModal";
-import useProduct from "../../subscribe/react/hooks/useProduct";
-import usePreview from "../../subscribe/react/hooks/usePreview";
-import Summary from "./Summary";
-import BuyButton from "./BuyButton";
+// import React from "react";
+// import PurchaseModal from "../../../PurchaseModal";
+// import usePreview from "../../subscribe/react/hooks/usePreview";
+// import Summary from "./Summary";
+// import BuyButton from "./BuyButton";
 
-const BlenderPurchase = () => {
-  const { product, quantity } = useProduct();
-  const { data: preview } = usePreview();
+// const BlenderPurchase = () => {
+//   // const { product, quantity } = useProduct();
+//   const { data: preview } = usePreview();
 
-  const termsLabel = (
-    <>
-      I agree to the{" "}
-      <a
-        href="/legal/ubuntu-advantage-service-terms"
-        target="_blank"
-        rel="noopener norefferer"
-      >
-        Ubuntu Advantage terms
-      </a>
-      , which apply to the{" "}
-      <a href="/legal/solution-support">Solution Support</a> service.
-    </>
-  );
+//   const termsLabel = (
+//     <>
+//       I agree to the{" "}
+//       <a
+//         href="/legal/ubuntu-advantage-service-terms"
+//         target="_blank"
+//         rel="noopener norefferer"
+//       >
+//         Ubuntu Advantage terms
+//       </a>
+//       , which apply to the{" "}
+//       <a href="/legal/solution-support">Solution Support</a> service.
+//     </>
+//   );
 
-  const marketingLabel =
-    "I agree to receive information about Canonical's products and services";
+//   const marketingLabel =
+//     "I agree to receive information about Canonical's products and services";
 
-  const closeModal = () => {
-    const purchaseModal = document.querySelector("#purchase-modal");
-    purchaseModal?.classList.add("u-hide");
-  };
+//   const closeModal = () => {
+//     const purchaseModal = document.querySelector("#purchase-modal");
+//     purchaseModal?.classList.add("u-hide");
+//   };
 
-  return (
-    <PurchaseModal
-      termsLabel={termsLabel}
-      marketingLabel={marketingLabel}
-      product={product}
-      preview={preview}
-      quantity={quantity}
-      closeModal={closeModal}
-      Summary={Summary}
-      BuyButton={BuyButton}
-      marketplace="blender"
-    />
-  );
-};
+//   return (
+//     <PurchaseModal
+//       termsLabel={termsLabel}
+//       marketingLabel={marketingLabel}
+//       product={product}
+//       preview={preview}
+//       quantity={quantity}
+//       closeModal={closeModal}
+//       Summary={Summary}
+//       BuyButton={BuyButton}
+//       marketplace="blender"
+//     />
+//   );
+// };
 
-export default BlenderPurchase;
+// export default BlenderPurchase;
