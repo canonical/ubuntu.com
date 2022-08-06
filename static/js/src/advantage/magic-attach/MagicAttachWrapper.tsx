39e30e1c9598073487e50f6503fd87f5a725667c
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-08-07 04:04:03 +0530

    wip-magic attach dropdown proto

--- static/js/src/advantage/magic-attach/MagicAttachWrapper.tsx
+++ static/js/src/advantage/magic-attach/MagicAttachWrapper.tsx
@@ -1,12 +1,16 @@
 import { Row, Strip } from "@canonical/react-components";
-import React from "react";
+import React, { useState } from "react";
 import MagicAttachCode from "./components/MagicAttachCode";
+import MagicAttachDropdown from "./components/MagicAttachDropdown";
 
+const queryParams = new URLSearchParams(window.location.search);
+const selectedSubscription = queryParams.get("subscription");
 const MagicAttachWrapper = () => {
   return (
     <Strip className="p-strip--suru-topped">
       <Row className="u-vertically-center">
         <MagicAttachCode />
+        <MagicAttachDropdown selectedId={selectedSubscription} />
       </Row>
     </Strip>
   );
--- static/js/src/advantage/magic-attach/components/MagicAttachDropdown/MagicAttachDropdown.tsx
+++ static/js/src/advantage/magic-attach/components/MagicAttachDropdown/MagicAttachDropdown.tsx
@@ -1,5 +1,37 @@
 import React from "react";
-const MagicAttachDropdown = () => {
-  return <></>;
+import { useUserSubscriptions } from "advantage/react/hooks";
+import { selectUASubscriptions } from "advantage/react/hooks/useUserSubscriptions";
+import { Form, Select, Spinner } from "@canonical/react-components";
+
+type Props = {
+  selectedId: string | null;
+};
+const MagicAttachDropdown = ({ selectedId }: Props) => {
+  const {
+    data: uaSubscriptionsData = [],
+    isLoading: isLoadingUA,
+  } = useUserSubscriptions({
+    select: selectUASubscriptions,
+  });
+
+  const uaSubscriptionsOptions = uaSubscriptionsData.map((subscription) => ({
+    label: subscription.product_name,
+    value: subscription.subscription_id,
+  }));
+  if (isLoadingUA) {
+    return <Spinner />;
+  }
+
+  return (
+    <Form>
+      <Select
+        defaultValue=""
+        id="exampleSelect2"
+        label="Ubuntu releases"
+        name="exampleSelect"
+        options={uaSubscriptionsOptions}
+      />
+    </Form>
+  );
 };
 export default MagicAttachDropdown;
