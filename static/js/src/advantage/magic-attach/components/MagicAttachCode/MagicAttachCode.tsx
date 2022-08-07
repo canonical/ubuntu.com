31d4370f0189e3ba1f5da6b8af49beaf2f0edd78
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-08-08 02:14:04 +0530

    commit before encryption

--- static/js/src/advantage/magic-attach/components/MagicAttachCode/MagicAttachCode.tsx
+++ static/js/src/advantage/magic-attach/components/MagicAttachCode/MagicAttachCode.tsx
@@ -1,7 +1,8 @@
+import { Input } from "@canonical/react-components";
 import React from "react";
 
 const MagicAttachCode = () => {
-  return <></>;
+  return <Input type="text" id="exampleTextInput3" label="magicAttachCode" />;
 };
 
 export default MagicAttachCode;
