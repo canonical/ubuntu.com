1915b5b6447abb8abe75b7389a845acf266213f0
Morgan Robertson <mrgnr@pm.me>
2022-08-19 17:09:01 +0200

    wip -- TrueAbility API

--- webapp/shop/api/trueability/api.py
+++ webapp/shop/api/trueability/api.py
@@ -9,12 +9,12 @@ from requests import Session
 import http.client as http_client
 
 ## Uncomment the following to enable request logging
-# http_client.HTTPConnection.debuglevel = 1
-# logging.basicConfig()
-# logging.getLogger().setLevel(logging.DEBUG)
-# requests_log = logging.getLogger("requests.packages.urllib3")
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
+#  http_client.HTTPConnection.debuglevel = 1
+#  logging.basicConfig()
+#  logging.getLogger().setLevel(logging.DEBUG)
+#  requests_log = logging.getLogger("requests.packages.urllib3")
+#  requests_log.setLevel(logging.DEBUG)
+#  requests_log.propagate = True
 
 
 class TrueAbilityAPI:
@@ -58,44 +58,40 @@ class TrueAbilityAPI:
 
     def get_ability_screens(self):
         uri = "/api/v1/ability_screens"
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+        return self.make_request("GET", uri).json()
 
     def update_ability_screen(self):
         pass
 
-    def get_assessment_reservations(self):
-        uri = "/api/v1/assessment_reservations"
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+    def get_assessment_reservations(self, uuid: str = None):
+        uri = "/api/v1/assessment_reservations" + (f"/{uuid}" if uuid else "")
+        return self.make_request("GET", uri).json()
 
-    def get_assessments(self):
-        uri = "/api/v1/assessments"
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+    def get_assessments(self, uuid: str = None):
+        uri = "/api/v1/assessments" + (f"/{uuid}?uuid=true" if uuid else "")
+        return self.make_request("GET", uri).json()
+
+    def get_results(self, id: int = None):
+        uri = "/api/v1/results" + (f"/{id}" if id else "")
+        return self.make_request("GET", uri).json()
 
     def get_candidate_access_token_status(self, id: int):
         uri = f"/api/v1/candidate_access_tokens/{id}"
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+        return self.make_request("GET", uri).json()
 
-    def get_candidate_invitations(self, ability_screen_id: int):
+    def get_candidate_invitations(
+        self, ability_screen_id: int = None, code: str = None
+    ):
         uri = (
             "/api/v1/candidate_invitations"
-            f"?ability_screen_id={ability_screen_id}"
+            + (f"/{code}" if code else "")
+            + (
+                f"?ability_screen_id={ability_screen_id}"
+                if ability_screen_id
+                else ""
+            )
         )
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+        return self.make_request("GET", uri).json()
 
     def post_candidate_invitation(
         self,
@@ -106,7 +102,7 @@ class TrueAbilityAPI:
         expires_at: str,
     ):
         uri = "/api/v1/candidate_invitations"
-        #headers = {"Content-Type": "application/json"}
+        # headers = {"Content-Type": "application/json"}
         body = {
             "candidate_invitation": {
                 "ability_screen_id": ability_screen_id,
@@ -118,31 +114,17 @@ class TrueAbilityAPI:
         }
         return self.make_request("POST", uri, json=body).json()
 
-    def get_companies(self):
-        uri = "/api/v1/companies"
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+    def get_companies(self, hashid: str = None):
+        uri = "/api/v1/companies" + (f"/{hashid}" if hashid else "")
+        return self.make_request("GET", uri).json()
 
     def get_webhooks(self, ability_screen_id: int):
-        uri = (
-            "/api/v1/webhooks"
-            f"?ability_screen_id={ability_screen_id}"
-        )
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+        uri = "/api/v1/webhooks" f"?ability_screen_id={ability_screen_id}"
+        return self.make_request("GET", uri).json()
 
     def get_system_status(self):
-        uri = (
-            "/api/v1/system_status"
-        )
-        return self.make_request(
-            "GET",
-            uri,
-        ).json()
+        uri = "/api/v1/system_status"
+        return self.make_request("GET", uri).json()
 
 
 if __name__ == "__main__":
@@ -153,44 +135,61 @@ if __name__ == "__main__":
         session,
     )
 
-    print("# Ability screens")
-    print(json.dumps(api.get_ability_screens(), indent=4))
-    print()
+    ## Uncomment API calls below as needed for testing
 
-    print("# Assessment reservations")
-    print(json.dumps(api.get_assessment_reservations(), indent=4))
-    print()
+    #  print("# Ability screens")
+    #  print(json.dumps(api.get_ability_screens(), indent=4))
+    #  print()
 
-    print("# Assessments")
-    print(json.dumps(api.get_assessments(), indent=4))
-    print()
+    #  print("# Assessment reservations")
+    #  #  uuid = None
+    #  uuid = "228c490b-2742-41f4-b227-47de6e447018"
+    #  print(json.dumps(api.get_assessment_reservations(uuid), indent=4))
+    #  print()
 
-    print("# Candidate access token")
-    print(json.dumps(api.get_candidate_access_token_status(100), indent=4))
-    print()
+    #  print("# Assessments")
+    #  #  uuid = None
+    #  uuid = "ee0e7712-13ea-11ed-8035-0af7ea428473"
+    #  print(json.dumps(api.get_assessments(uuid), indent=4))
+    #  print()
 
-    print("# Candidate invitations")
-    print(json.dumps(api.get_candidate_invitations(4190), indent=4))
-    print()
+    #  print("# Results")
+    #  #  id = None
+    #  id = 112968
+    #  print(json.dumps(api.get_results(id), indent=4))
+    #  print()
 
-    print("# Invite candidate")
-    email = "morgan.robertson@canonical.com"
-    first_name, last_name = "Morgan", "Robertson"
-    expires_at = (datetime.utcnow() + timedelta(days=1)).isoformat()
-    print(
-        json.dumps(
-            api.post_candidate_invitation(
-                4190, email, first_name, last_name, expires_at
-            ),
-            indent=4,
-        )
-    )
-    print()
+    #  print("# Candidate access token")
+    #  print(json.dumps(api.get_candidate_access_token_status(4190), indent=4))
+    #  print()
 
-    print("# Webhooks")
-    print(json.dumps(api.get_webhooks(4190), indent=4))
-    print()
+    #  print("# Candidate invitations")
+    #  #  print(json.dumps(api.get_candidate_invitations(4190), indent=4))
+    #  print(json.dumps(api.get_candidate_invitations(code="ZDk0YmMxOTA"), indent=4))
+    #  print()
 
-    print("# System status")
-    print(json.dumps(api.get_system_status(), indent=4))
-    print()
+    #  print("# Invite candidate")
+    #  email = "morgan.robertson@canonical.com"
+    #  first_name, last_name = "Morgan", "Robertson"
+    #  expires_at = (datetime.utcnow() + timedelta(days=1)).isoformat()
+    #  print(
+    #      json.dumps(
+    #          api.post_candidate_invitation(
+    #              4190, email, first_name, last_name, expires_at
+    #          ),
+    #          indent=4,
+    #      )
+    #  )
+    #  print()
+
+    #  print("# Companies")
+    #  print(json.dumps(api.get_companies(), indent=4))
+    #  print()
+
+    #  print("# Webhooks")
+    #  print(json.dumps(api.get_webhooks(4190), indent=4))
+    #  print()
+
+    #  print("# System status")
+    #  print(json.dumps(api.get_system_status(), indent=4))
+    #  print()
