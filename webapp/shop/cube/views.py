227f0671d09650095993202e7b9b03c2989d45fa
Morgan Robertson <mrgnr@pm.me>
2022-08-31 16:10:59 +0200

    Add scheduling confirmation page.

--- webapp/shop/cube/views.py
+++ webapp/shop/cube/views.py
@@ -423,7 +423,14 @@ def cred_schedule(
         if "error" in response:
             error = response["message"]
         else:
-            return flask.redirect("/credentialing/scheduled")
+            exam = {
+                "name": "Linux Essentials",
+                "date": starts_at.strftime("%d %b %Y"),
+                "time": starts_at.strftime("%H:%M"),
+            }
+            return flask.render_template(
+                "/credentialing/schedule-confirm.html", exam=exam
+            )
 
     return flask.render_template("credentialing/schedule.html", error=error)
 
@@ -443,14 +450,18 @@ def cred_scheduled(
     tb = ""
     try:
         ability_screen_id = 4190
-        response = trueability_api.get_assessment_reservations(ability_screen_id)
+        response = trueability_api.get_assessment_reservations(
+            ability_screen_id
+        )
         user_email = user_info(flask.session)["email"]
         for r in response["assessment_reservations"]:
             if r["user"]["email"] != user_email:
                 continue
 
             name = r["ability_screen"]["display_name"]
-            starts_at = datetime.strptime(r["starts_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            starts_at = datetime.strptime(
+                r["starts_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            )
             timezone = r["user"]["time_zone"]
             exams.append(
                 {
@@ -468,7 +479,13 @@ def cred_scheduled(
     url = os.getenv("TRUEABILITY_URL", "")
     key_len = len(os.getenv("TRUEABILITY_API_KEY", ""))
 
-    return flask.render_template("credentialing/scheduled.html", exams=exams, url=url, key_len=key_len, tb=tb)
+    return flask.render_template(
+        "credentialing/scheduled.html",
+        exams=exams,
+        url=url,
+        key_len=key_len,
+        tb=tb,
+    )
 
 
 @shop_decorator(area="cube", permission="user", response="html")
@@ -492,16 +509,22 @@ def cred_assessments(
             continue
 
         name = r["ability_screen"]["name"]
-        started_at = datetime.strptime(r["started_at"], "%Y-%m-%dT%H:%M:%S%fZ") if r["started_at"] else None
+        started_at = (
+            datetime.strptime(r["started_at"], "%Y-%m-%dT%H:%M:%S%fZ")
+            if r["started_at"]
+            else None
+        )
         timezone = r["user"]["time_zone"]
         exams.append(
             {
                 "name": name,
-                "date": started_at.strftime("%d %b %Y") if started_at else "N/A",
+                "date": started_at.strftime("%d %b %Y")
+                if started_at
+                else "N/A",
                 "time": started_at.strftime("%H:%M") if started_at else "N/A",
                 "timezone": timezone,
                 "state": r["state"],
-                "id": r["id"]
+                "id": r["id"],
             }
         )
 
