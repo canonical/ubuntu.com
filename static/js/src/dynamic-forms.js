12ac951cdf5f3a45eb5ef8385154010c10f41164
petesfrench <peterfrench94@gmail.com>
2022-09-21 12:24:56 +0200

    Add intlTelInput utils.js to build

--- static/js/src/dynamic-forms.js
+++ static/js/src/dynamic-forms.js
@@ -510,9 +510,8 @@ import intlTelInput from "intl-tel-input";
 
       // Setup dial code dropdown options (intlTelInput)
       function setupIntlTelInput() {
-        const utilsScript =
-          "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.18/js/utils.js";
-
+        const utilsScript = "./static/js/dist/utils.js";
+        
         // remove name from original input so only the hidden input is submitted
         const inputName = phoneInput.name;
         phoneInput.removeAttribute("name");
