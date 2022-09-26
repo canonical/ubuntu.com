22009c0ad0f9a2fe3c16f7a190f17ff9f812c1c6
Morgan Robertson <mrgnr@pm.me>
2022-09-26 16:08:57 +0200

    Format with Black.

--- webapp/app.py
+++ webapp/app.py
@@ -899,7 +899,7 @@ core_als_autils_docs.init_app(app)
 
 # Cube docs
 app.add_url_rule("/credentialing", view_func=cube_home)
-app.add_url_rule("/credentialing/syllabus",view_func=cred_syllabus_data)
+app.add_url_rule("/credentialing/syllabus", view_func=cred_syllabus_data)
 app.add_url_rule("/cube/microcerts", view_func=cube_microcerts)
 app.add_url_rule("/cube/microcerts.json", view_func=get_microcerts)
 app.add_url_rule(
