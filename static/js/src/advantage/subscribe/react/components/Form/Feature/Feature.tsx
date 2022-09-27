157668045c186517e654ce71d5654f70a5e898c1
ClementChaumel <clement.chaumel@gmail.com>
2022-09-27 13:21:42 +0200

    Update feature section

--- static/js/src/advantage/subscribe/react/components/Form/Feature/Feature.tsx
+++ static/js/src/advantage/subscribe/react/components/Form/Feature/Feature.tsx
@@ -1,133 +1,173 @@
 import React, { useContext } from "react";
 import classNames from "classnames";
 import { Col, RadioInput, Row } from "@canonical/react-components";
-import { Features, isPublicCloud, ProductTypes } from "advantage/subscribe/react/utils/utils";
+import {
+  Features,
+  isPublicCloud,
+  ProductTypes,
+} from "advantage/subscribe/react/utils/utils";
 import { FormContext } from "advantage/subscribe/react/utils/FormContext";
 
-
 const Feature = () => {
-  const { productType, feature, setFeature} = useContext(FormContext);
+  const { productType, feature, setFeature } = useContext(FormContext);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFeature(event.target.value as Features);
   };
 
-  
-  const infraOnlyDisabled = (ProductTypes.desktop === productType);
+  const infraOnlyDisabled = ProductTypes.desktop === productType;
 
   return (
     <div
       className={classNames({
-        "row": true,
+        row: true,
         "p-divider": true,
         "u-disable": isPublicCloud(productType),
       })}
       data-testid="wrapper"
     >
-      <Col className="u-hide--small" size={12} small={4}>
-        <table className="p-table--selectable">
-          <thead>
-            <tr>
-              <th>CVE Security patching coverage until 2030 for:</th>
-              <th className={classNames({ selected: feature === Features.pro })}>
+      <Row style={{ gap: "0" }} className="u-hide--small">
+        <Col size={6} className="description-column">
+          <div className="heading">
+            <strong>CVE Security patching coverage until 2030 for:</strong>
+          </div>
+          <hr />
+          <div className="main">
+            <p>
+              Over 2,300 open source deb packages in Ubuntu Main repository
+              until 2030 (extended from 2025), including:
+            </p>
+            <div className="logos-wrapper">
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+            </div>
+          </div>
+          <hr />
+          <div className="universe">
+            <p>
+              Over 2,300 open source deb packages in Ubuntu Main repository
+              until 2030 (extended from 2025), including:
+            </p>
+            <div className="logos-wrapper">
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+              <img src="https://placekitten.com/91/40" alt="" />
+            </div>
+          </div>
+        </Col>
+        <Col size={3}>
+          <div
+            className={classNames({
+              "p-card--radio--column": true,
+              "is-selected": feature === Features.pro,
+            })}
+          >
+            <label className="p-radio u-align-text--center">
+              <input
+                className="p-radio__input"
+                autoComplete="off"
+                type="radio"
+                aria-labelledby={`pro-label`}
+                value={Features.pro}
+                checked={feature === Features.pro}
+                onChange={handleChange}
+              />
+              <span className="p-radio__label" id={`pro-label`}>
                 <RadioInput
-                  inline
-                  label="Ubuntu Pro"
-                  value={Features.pro}
+                  labelClassName="inner-label"
+                  label={"Ubuntu pro"}
                   checked={feature === Features.pro}
+                  value={Features.pro}
                   onChange={handleChange}
                 />
-              </th>
-              <th
-                className={classNames({ selected: feature === Features.infra })}
-              >
+                <hr />
+                <div className="included">
+                  <i className="p-icon--success"></i>Included
+                </div>
+                <hr />
+                <div className="included">
+                  <i className="p-icon--success"></i> Included
+                </div>
+              </span>
+            </label>
+          </div>
+        </Col>
+        <Col size={3}>
+          <div
+            className={classNames({
+              "p-card--radio--column": true,
+              "is-selected": feature === Features.infra,
+              "u-disable": infraOnlyDisabled,
+            })}
+          >
+            <label className="p-radio u-align-text--center">
+              <input
+                className="p-radio__input"
+                autoComplete="off"
+                type="radio"
+                aria-labelledby={`infra-label`}
+                value={Features.infra}
+                checked={feature === Features.infra}
+                onChange={handleChange}
+              />
+              <span className="p-radio__label" id={`infra-label`}>
                 <RadioInput
-                  inline
-                  label="Ubuntu Pro, Infra Only"
-                  value={Features.infra}
+                  labelClassName="inner-label"
+                  label={"Ubuntu pro, infra only"}
                   checked={feature === Features.infra}
+                  value={Features.infra}
                   onChange={handleChange}
-                  disabled={infraOnlyDisabled}
                 />
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <p>
-                  Over 2,300 open source deb packages in Ubuntu Main repository
-                  until 2030 (extended from 2025), including:
-                </p>
-                <img src="" alt="" /> <img src="" alt="" /> <img src="" alt="" />
-              </td>
-              <td
-                className={classNames({
-                  selected: feature === Features.pro,
-                  "u-align--center": true,
-                })}
-              >
-                <i className="p-icon--success"></i> Included
-              </td>
-              <td
-                className={classNames({
-                  selected: feature === Features.infra,
-                  "u-align--center": true,
-                })}
-              >
-                <i className="p-icon--success"></i> Included
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  Over 23,000 open source deb packages in Ubuntu Universe
-                  repository until 2030, including:
-                </p>
-              </td>
-              <td
-                className={classNames({
-                  selected: feature === Features.pro,
-                  "u-align--center": true,
-                })}
-              >
-                <i className="p-icon--success"></i> Included
-              </td>
-              <td
-                className={classNames({
-                  selected: feature === Features.infra,
-                  "u-align--center": true,
-                })}
-              >
-                <i className="p-icon--error"></i> Not Included
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                <hr />
+                <div className="included">
+                  <i className="p-icon--success"></i>Included
+                </div>
+                <hr />
+                <div className="included">
+                  <i className="p-icon--error"></i> Not included
+                </div>
+              </span>
+            </label>
+          </div>
         </Col>
-        <Col className="u-hide u-show--small" size={12} small={4}>
-          <RadioInput
-            label="Ubuntu Pro"
-            value={Features.pro}
-            checked={feature === Features.pro}
-            onChange={handleChange}
-          />
-          <RadioInput
-            label="Ubuntu Pro, Infra Only"
-            value={Features.infra}
-            checked={feature === Features.infra}
-            onChange={handleChange}
-            disabled={infraOnlyDisabled}
-          />
-        </Col>
-        <Col className="p-divider__block u-align--center u-hide u-show--small"  size={6} small={2}>
-          <h4>2,300+</h4>
-          <p>packages in Ubuntu main, including:</p>
-        </Col>
-        <Col className={classNames({"p-divider__block u-align--center u-hide u-show--small": true, "u-disable": Features.infra === feature})} size={6} small={2}>
-          <h4>23,000+</h4>
-          <p>packages in Ubuntu universe, including:</p>
-        </Col>
+      </Row>
+      <Col className="u-hide u-show--small" size={12} small={4}>
+        <RadioInput
+          label="Ubuntu Pro"
+          value={Features.pro}
+          checked={feature === Features.pro}
+          onChange={handleChange}
+        />
+        <RadioInput
+          label="Ubuntu Pro, Infra Only"
+          value={Features.infra}
+          checked={feature === Features.infra}
+          onChange={handleChange}
+          disabled={infraOnlyDisabled}
+        />
+      </Col>
+      <Col
+        className="p-divider__block u-align--center u-hide u-show--small"
+        size={6}
+        small={2}
+      >
+        <h4>2,300+</h4>
+        <p>packages in Ubuntu main, including:</p>
+      </Col>
+      <Col
+        className={classNames({
+          "p-divider__block u-align--center u-hide u-show--small": true,
+          "u-disable": Features.infra === feature,
+        })}
+        size={6}
+        small={2}
+      >
+        <h4>23,000+</h4>
+        <p>packages in Ubuntu universe, including:</p>
+      </Col>
     </div>
   );
 };
