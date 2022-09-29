c772d2aa21b7eec6ae62b9ad133f5a2b23ae1574
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-09-29 16:01:00 +0530

    start work on enterprise purchasing

---
+++ static/js/src/advantage/credentialling/components/CredManage/CredManage.tsx
@@ -0,0 +1,11 @@
+import { Strip } from "@canonical/react-components";
+import React from "react";
+const CredManage = ()=>{
+    return <section>
+        <Strip className="p-strip--suru-topped">
+            
+        </Strip>
+
+    </section>
+}
+export default CredManage;
