596af1c9a9a6bb74f45a8a81057ade70a815b56f
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-10-12 20:39:07 +0000

    trying to do hashmaps in typedcript

--- static/js/src/advantage/credentialling/components/CredManage/CredManage.tsx
+++ static/js/src/advantage/credentialling/components/CredManage/CredManage.tsx
@@ -1,43 +1,24 @@
-import { Row, Spinner } from "@canonical/react-components";
+import { CheckboxInput, ContextualMenu, MainTable, Row, Spinner } from "@canonical/react-components";
 import { listAllKeys } from "advantage/credentialling/api/keys";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
-import KeyTable from "./KeyTable";
 
 type ActivationKey = {
     contractItemID: string,
     expirationDate: Date,
     key: string,
+    activatedBy?: string,
     productID: string
 }
 const CredManage = () => {
     const [tab, changeTab] = useState(0);
-    const [sortAttributes, setsortAttributes] = useState<[string, boolean]>(["expirationDate", true]);
     const inputRefs = useRef<HTMLButtonElement[] | null[]>([]);
+    const [actionLinks, updateActionLinks] = useState<{ children: string, onClick: () => void }[]>([]);
     const { isLoading, data } = useQuery(["ActivationKeys"],
         async () => {
             return listAllKeys("cANU9TzI1bfZ2nnSSSnPdlp30TwdVkLse2vzi1TzKPBc");
         }
     );
-
-    const sortTable = (attr: string) => {
-        if (attr == "key") {
-            data.sort((x: ActivationKey, y: ActivationKey) => { return x[attr].localeCompare(y[attr]) });
-        }
-        else if (attr == "expirationDate") {
-            data.sort((x: ActivationKey, y: ActivationKey) => { return x[attr] - y[attr] });
-        }
-        if (attr == sortAttributes[0]) {
-            if (sortAttributes[1] == true) {
-                data.reverse();
-            }
-            setsortAttributes([attr, !sortAttributes[1]]);
-        }
-        else {
-            setsortAttributes([attr, true]);
-        }
-        console.log(data);
-    }
 
     const switchTab = (
         event: React.KeyboardEvent<HTMLButtonElement>, currentIndex: number
@@ -53,6 +34,62 @@ const CredManage = () => {
         }
     };
 
+    const [selectedKeyIds, setSelectedKeyIds] = useState<string[]>([]);
+    var selectedKeys = new Map<string, ActivationKey>();
+    const handleCheckbox = (keyValue: string, key: ActivationKey) => {
+        console.log(keyValue, selectedKeyIds);
+        if (selectedKeyIds.includes(keyValue)) {
+            setSelectedKeyIds(selectedKeyIds => selectedKeyIds.filter(id => id != keyValue));
+            selectedKeys.delete(keyValue);
+            console.log(selectedKeys);
+            console.log("r", selectedKeyIds);
+        }
+        else {
+            setSelectedKeyIds(selectedKeyIds.concat(keyValue));
+            selectedKeys.set(keyValue, key);
+            console.log(selectedKeys);
+        }
+    };
+    const copyToClipboard = (value: string) => {
+        console.log(value);
+        navigator.clipboard.writeText(value);
+    };
+
+    const isArchiveable = () => {
+        for (var i of selectedKeyIds) {
+            console.log("archivable", i, selectedKeys.get(i));
+            if ("activatedBy" in selectedKeys.get(i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+    const isUnused = () => {
+        return true;
+    }
+    useEffect(() => {
+        let newList = [];
+        if (isArchiveable()) {
+            newList.push({
+                children: "Archive",
+                onClick: () => { }
+            });
+        }
+        if (isUnused()) {
+            newList.push({
+                children: "Copy List",
+                onClick: () => { navigator.clipboard.writeText(selectedKeyIds.join(",")) }
+            });
+            newList.push({
+                children: "Refresh Keys",
+                onClick: () => { }
+            });
+        }
+        updateActionLinks(newList);
+
+    }, [selectedKeyIds])
+
     return (<div>
         <Row>
             <h1>Manage exam attempts</h1>
@@ -60,7 +97,7 @@ const CredManage = () => {
             <p>To redeem an attempt, copy the associated key and paste it where the system asks you for the attempt key</p>
         </Row>
         <Row>
-            <div className="p-segmented-control">
+            <div className="p-segmented-control col-6">
                 <div
                     className="p-segmented-control__list"
                     role="tablist"
@@ -135,21 +172,80 @@ const CredManage = () => {
                     </button>
                 </div>
             </div>
+            <div className="col-6">
+                {selectedKeyIds.length > 0 ?
+                    <ContextualMenu
+                        hasToggleIcon
+                        links={actionLinks}
+                        position="right"
+                        toggleLabel="Actions"
+                    /> : ""}
+            </div>
         </Row>
         <Row>
-            <table aria-label="Manage activation keys">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th aria-sort="none" onClick={() => { sortTable("key") }}>Exam Key Id</th>
-                        <th></th>
-                        <th>Asignee</th>
-                        <th>Exam</th>
-                        <th aria-sort="none" onClick={() => { sortTable("expirationDate") }}>Expiration Date</th>
-                    </tr>
-                </thead>
-                {isLoading ? <Spinner /> : <KeyTable keyList={data} reload={sortAttributes} />}
-            </table>
+            {isLoading ? <Spinner /> :
+                <MainTable
+                    headers={
+                        [
+                            {
+                                content: "",
+                                sortKey: "checkbox"
+                            },
+                            {
+                                content: "Exam Key Id",
+                                sortKey: "key"
+                            },
+                            {
+                                content: "",
+                            },
+                            {
+                                content: "Assignee",
+                                sortKey: "activatedBy"
+                            },
+                            {
+                                content: "Exam",
+                                sortKey: "productID",
+                            },
+                            {
+                                content: "ExpirationDate",
+                                sortKey: "expirationDate"
+                            }
+                        ]
+                    }
+                    rows={
+                        data.map((keyitem: ActivationKey) => {
+                            return ({
+                                columns: [
+                                    {
+                                        content: <CheckboxInput onChange={() => { handleCheckbox(keyitem["key"], keyitem) }} label="" id={keyitem["key"] + "_checkbox"} />
+                                    },
+                                    {
+                                        content: keyitem["key"]
+                                    },
+                                    {
+                                        content: <a onClick={() => { copyToClipboard(keyitem.key); }}><i className="p-icon--copy"></i></a>
+                                    },
+                                    {
+                                        content: keyitem["activatedBy"] ? keyitem["activatedBy"] : "N/A"
+                                    },
+                                    {
+                                        content: keyitem.productID
+                                    },
+                                    {
+                                        content: keyitem.expirationDate.toString()
+                                    },
+                                ],
+                                sortData: {
+                                    ...keyitem,
+                                    checkbox: keyitem["key"]
+                                }
+                            }
+                            );
+                        })
+                    }
+                    sortable
+                />
+            }
         </Row>
     </div>);
 };
