4385ab4b25ac82fa1f68f70ba7fcffe33fdc876d
Scott Mason Nash <sowasred2012@gmail.com>
2022-10-17 17:24:14 +0100

    add global nav

--- static/js/src/core.js
+++ static/js/src/core.js
@@ -1,4 +1,7 @@
 import { cookiePolicy } from "@canonical/cookie-policy";
+import { createNav } from "@canonical/global-nav";
 
 // Initalise the cookie policy notification.
 cookiePolicy();
+
+createNav();
