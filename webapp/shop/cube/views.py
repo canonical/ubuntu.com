0a4e820cec5ed93db356b790bcac89747d3756df
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-10-18 12:13:17 +0000

    added tooltips and view now fetches hardcoded values

--- webapp/shop/cube/views.py
+++ webapp/shop/cube/views.py
@@ -439,11 +439,11 @@ def cube_shop(**kwargs):
 
 @shop_decorator(area="cube", permission="user", response="json")
 def get_activation_keys(ua_contracts_api, **kwargs):
-    contract_id = kwargs.get("contract_id")
-    keys = ua_contracts_api.list_activation_keys(contract_id)
-    return flask.jsonify(keys)
-    # keys = json.load(open("webapp/shop/cube/demo_keys.json", "r"))
+    # contract_id = kwargs.get("contract_id")
+    # keys = ua_contracts_api.list_activation_keys(contract_id)
     # return flask.jsonify(keys)
+    keys = json.load(open("webapp/shop/cube/demo_keys.json", "r"))
+    return flask.jsonify(keys)
 
 
 @shop_decorator(area="cube", permission="user", response="json")
