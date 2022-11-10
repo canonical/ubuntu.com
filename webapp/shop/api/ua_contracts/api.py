d2626ec8db35ae70ae135bb846ea148cd25a5351
Albert Kolozsvari <albertartk@gmail.com>
2022-11-10 13:47:24 +0000

    Switch to auto-cancel pending purhcases

--- webapp/shop/api/ua_contracts/api.py
+++ webapp/shop/api/ua_contracts/api.py
@@ -272,6 +272,26 @@ class UAContractsAPI:
             error_rules=["default"],
         ).json()
 
+    def web_purchase_from_marketplace(
+        self, marketplace: str, purchase_request: dict
+    ) -> dict:
+        return self._request(
+            method="post",
+            path=f"web/marketplace/{marketplace}/purchase",
+            json=purchase_request,
+            error_rules=["default"],
+        ).json()
+
+    def web_preview_purchase_from_marketplace(
+        self, marketplace: str, purchase_request: dict
+    ) -> dict:
+        return self._request(
+            method="post",
+            path=f"web/marketplace/{marketplace}/purchase/preview",
+            json=purchase_request,
+            error_rules=["default"],
+        ).json()
+
     def cancel_subscription(self, subscription_id: str) -> dict:
         self._request(
             method="delete",
