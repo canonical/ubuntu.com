6a74edf514454a21f89a6079bdbd683c020f57ad
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-01-30 13:12:05 +0000

    makes link a button

--- static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/SubscriptionDetails.tsx
+++ static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/SubscriptionDetails.tsx
@@ -81,7 +81,7 @@ export const SubscriptionDetails = forwardRef<HTMLDivElement, Props>(
         if (
           subscription?.current_number_of_machines &&
           (subscription?.current_number_of_machines ?? 0) <
-            (subscription?.number_of_machines ?? 0)
+          (subscription?.number_of_machines ?? 0)
         ) {
           setNotification({
             severity: "caution",
@@ -153,7 +153,7 @@ export const SubscriptionDetails = forwardRef<HTMLDivElement, Props>(
                     </>
                   ) : null}
                   {subscription.type == "monthly" ||
-                  subscription.type == "yearly" ? (
+                    subscription.type == "yearly" ? (
                     <>
                       {subscription.statuses.is_renewed ? (
                         <button className="p-chip--positive">
@@ -182,7 +182,7 @@ export const SubscriptionDetails = forwardRef<HTMLDivElement, Props>(
             {isFree ? null : (
               <>
                 {subscription.statuses.has_access_to_support &&
-                subscription.type !== UserSubscriptionType.Trial ? (
+                  subscription.type !== UserSubscriptionType.Trial ? (
                   <Button
                     appearance="positive"
                     className="p-subscriptions__details-action"
@@ -196,7 +196,6 @@ export const SubscriptionDetails = forwardRef<HTMLDivElement, Props>(
                 ) : null}
                 {subscription.marketplace == "canonical-ua" ? (
                   <Button
-                    appearance="neutral"
                     className="p-subscriptions__details-action"
                     data-test="attach-button"
                     disabled={editing}
