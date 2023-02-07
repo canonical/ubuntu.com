ec4b2cca5b2fecb01ed8b83ed553d8a48c83c707
Albert Kolozsvari <albertartk@gmail.com>
2023-02-07 11:03:09 +0000

    Add extra RenewalButton test

--- static/js/src/advantage/react/components/Subscriptions/RenewalButton/RenewalButton.test.tsx
+++ static/js/src/advantage/react/components/Subscriptions/RenewalButton/RenewalButton.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { UserSubscriptionPeriod } from "advantage/api/enum";
 import { userSubscriptionFactory } from "advantage/tests/factories/api";
 import RenewalButton from "./RenewalButton";
 
@@ -91,6 +92,50 @@ describe("RenewalButton", () => {
     });
 
     const subscription = userSubscriptionFactory.build();
+
+    const action = "purchase";
+    const editing = false;
+
+    render(
+      <RenewalButton
+        subscription={subscription}
+        action={action}
+        editing={editing}
+      ></RenewalButton>
+    );
+
+    const renewButton = screen.getByRole("button") as HTMLInputElement;
+
+    expect(renewButton).toBeInTheDocument();
+
+    userEvent.click(renewButton);
+
+    expect(window.location.href).toBe("/account/checkout");
+    expect(localStorage.getItem("shop-checkout-data")).toBeDefined();
+
+    expect(window.dataLayer).toStrictEqual([
+      {
+        event: "GAEvent",
+        eventCategory: "Advantage",
+        eventAction: "subscription-rebuy-expired-modal",
+        eventLabel: "subscription rebuy expired modal opened",
+      },
+    ]);
+  });
+  it("sets session and redirects for rebuying for monthly", () => {
+    Object.defineProperty(window, "location", {
+      value: {
+        href: "/old/url",
+      },
+    });
+    Object.defineProperty(window, "dataLayer", {
+      value: [],
+      writable: true,
+    });
+
+    const subscription = userSubscriptionFactory.build({
+      period: UserSubscriptionPeriod.Monthly,
+    });
     const action = "purchase";
     const editing = false;
 
