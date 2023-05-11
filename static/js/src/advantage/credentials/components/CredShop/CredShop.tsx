01aef9f5393d0bc2cec83934261541a94e35ba9e
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-05-12 02:09:59 +0530

    writes product info to localstorage

--- static/js/src/advantage/credentials/components/CredShop/CredShop.tsx
+++ static/js/src/advantage/credentials/components/CredShop/CredShop.tsx
@@ -1,12 +1,95 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ExamSummary from "./ExamSummary/ExamSummary";
+import React, { useState } from "react";
+import { Product } from "advantage/subscribe/react/utils/utils";
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Row,
+  Strip,
+} from "@canonical/react-components";
+import { currencyFormatter } from "advantage/react/utils";
+
 const CredShop = () => {
+  const CUEExamKey: Product = {
+    id: "cue-activation-key",
+    longId: "lAMGrt4buzUR0-faJqg-Ot6dgNLn7ubIpWiyDgOrsDCg",
+    name: "CUE Activation Key",
+    price: { value: 4900, currency: "USD" },
+    productID: "cue-activation-key",
+    canBeTrialled: false,
+    private: false,
+    marketplace: "canonical-ua",
+  };
+  const [quantity, setQuantity] = useState(1);
+  const handleChange: React.ChangeEventHandler = (event: React.ChangeEvent) => {
+    event.preventDefault();
+    setQuantity(event.target.value);
+  };
   return (
     <>
-      <h1>Shop</h1>
-      <Link to={"/manage"}>manage</Link>
-      <ExamSummary />
+      <Strip>
+        <Row>
+          <h3>How many exams attempts do you need?</h3>
+        </Row>
+        <Row>
+          <Col size={6}>
+            <Form>
+              <Input
+                type="number"
+                id="keyQuantity"
+                value={quantity}
+                onChange={handleChange}
+              />
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <p>
+            Each exam attempt allows you to register for one of the following
+            certifications:
+            <ul>
+              <li>CUE.01: Linux</li>
+              <li>CUE.02: Desktop</li>
+              <li>CUE.03: Server</li>
+            </ul>
+          </p>
+        </Row>
+      </Strip>
+      <section className="p-strip--light is-shallow p-shop-cart">
+        <Row className="u-sv3">
+          <Col size={6} className="p-heading--2">
+            Your Order
+          </Col>
+        </Row>
+        <Row className="u-sv3">
+          <Col size={6}>{quantity} x Exam attempt key</Col>
+          <Col size={3}>
+            {currencyFormatter.format(
+              ((CUEExamKey?.price.value ?? 0) / 100) * (Number(quantity) ?? 0)
+            )}
+          </Col>
+          <Col size={3}>
+            <Button
+              appearance="positive"
+              onClick={(e) => {
+                e.preventDefault();
+                localStorage.setItem(
+                  "shop-checkout-data",
+                  JSON.stringify({
+                    product: CUEExamKey,
+                    quantity: quantity,
+                    action: "purchase",
+                  })
+                );
+                location.href = "/credentials/shop/checkout";
+              }}
+            >
+              Buy Now
+            </Button>
+          </Col>
+        </Row>
+      </section>
     </>
   );
 };
