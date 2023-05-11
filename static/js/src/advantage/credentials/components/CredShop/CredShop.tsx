230cce93c344e1c1d29e297975ae2960391611fe
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-05-11 21:21:37 +0530

    checkout works. Work on product selector

--- static/js/src/advantage/credentials/components/CredShop/CredShop.tsx
+++ static/js/src/advantage/credentials/components/CredShop/CredShop.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ExamSummary from "./ExamSummary/ExamSummary";
 const CredShop = () => {
   return (
     <>
       <h1>Shop</h1>
       <Link to={"/manage"}>manage</Link>
+      <ExamSummary />
     </>
   );
 };
