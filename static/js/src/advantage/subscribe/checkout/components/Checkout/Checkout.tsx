8dd5d9f6aeadc1a18fb47887ca8c98c97bf87770
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-05-12 17:42:32 +0530

    Redirect works

--- static/js/src/advantage/subscribe/checkout/components/Checkout/Checkout.tsx
+++ static/js/src/advantage/subscribe/checkout/components/Checkout/Checkout.tsx
@@ -22,9 +22,10 @@ type Props = {
   product: Product;
   quantity: number;
   action: Action;
+  redirectURL: string;
 };
 
-const Checkout = ({ product, quantity, action }: Props) => {
+const Checkout = ({ product, quantity, action, redirectURL }: Props) => {
   const [error, setError] = useState<React.ReactNode>(null);
   const { data: userInfo, isLoading: isUserInfoLoading } = useCustomerInfo();
   const isGuest = !userInfo?.customerInfo?.email;
@@ -132,6 +133,7 @@ const Checkout = ({ product, quantity, action }: Props) => {
                                 quantity={quantity}
                                 action={action}
                                 setError={setError}
+                                redirectURL={redirectURL}
                               ></BuyButton>
                             </Col>
                           </Row>
