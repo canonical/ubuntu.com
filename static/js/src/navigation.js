b4e6497e4831d53e358453c6bc79dba8d3d4eb15
Pete <peterfrench94@gmail.com>
2023-05-15 16:22:31 +0200

    Add temp fix for 'Ubuntu Advantage in global nav

--- static/js/src/navigation.js
+++ static/js/src/navigation.js
@@ -299,3 +299,19 @@ if (accountContainer && accountContainerSmall) {
       );
     });
 }
+
+// TEMP FIX - PETE F 15.05.23
+// UPDATES 'UBUNTU ADVANTAGE' TO 'UBUNTU PRO' IN THE MEGANAV UNTIL
+// WE MERGE THE NEW MEGANAV
+function replaceUbuntuAdvantage() {
+  const globalNav = document.querySelector("#canonical-global-nav");
+  const targetElementsArray = globalNav.querySelectorAll("[href='https://ubuntu.com/support']");
+  targetElementsArray.forEach(function(element) {
+    if (element.children.length) {
+      element.querySelector("h4").innerText = "Ubuntu Pro ›";
+    } else {
+      element.innerText = "Ubuntu Pro";
+    }
+  })
+}
+replaceUbuntuAdvantage()
