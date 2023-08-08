6ce46659ec7efd5f68baccc0cadee83ee0db9690
Albert Kolozsvari <albertartk@gmail.com>
2023-08-08 11:47:10 +0100

    Change cost to use currect number of machines

--- static/js/src/advantage/react/utils/getSubscriptionCost.ts
+++ static/js/src/advantage/react/utils/getSubscriptionCost.ts
@@ -14,8 +14,12 @@ export const getSubscriptionCost = (
     minimumFractionDigits: 0,
     style: "currency",
   });
+
   // The provided price is in cents, so this converts it to dollars:
-  let price = (subscription.price || 0) / 100;
+  const old_price = (subscription.price || 0) / 100;
+  const price_per_unit = old_price / subscription.number_of_machines;
+  let price = price_per_unit * subscription.current_number_of_machines;
+
   if (subscription.period === UserSubscriptionPeriod.Monthly) {
     price *= 12;
   }
