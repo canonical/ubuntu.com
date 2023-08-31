035e60de3a23abcbf1ee7faf7690cdf3aff2df48
Carlos Wu <carlos.fei@canonical.com>
2023-08-31 16:30:58 +0100

    Revert "WD-4158 Revamp amdxilinx "

--- static/js/src/tabbed-content.js
+++ static/js/src/tabbed-content.js
@@ -191,25 +191,3 @@
     initTabs(".js-tabbed-content");
   });
 })();
-
-(function () {
-  // Toggles show board based on selection on small screens
-
-  const boards = document.querySelectorAll(`[role=tabpanel]`);
-  const dropdownSelect = document.getElementById("boardSelect");
-
-  dropdownSelect.addEventListener("change", (event) => {
-    selectBoard();
-  });
-
-  function selectBoard() {
-    boards.forEach((board) => {
-      if (board.id === dropdownSelect.value) {
-        board.classList.remove("u-hide");
-        board.focus();
-      } else {
-        board.classList.add("u-hide");
-      }
-    });
-  }
-})();
