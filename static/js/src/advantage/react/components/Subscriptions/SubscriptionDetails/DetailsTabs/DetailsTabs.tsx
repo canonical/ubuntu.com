0c8e18c715a69808773eb0c7ff5c73348a821b96
britneywwc <britneywwc@gmail.com>
2023-10-03 17:55:50 +0800

    Added Pro network requirements to dashboard

--- static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/DetailsTabs/DetailsTabs.tsx
+++ static/js/src/advantage/react/components/Subscriptions/SubscriptionDetails/DetailsTabs/DetailsTabs.tsx
@@ -181,6 +181,13 @@ const DetailsTabs = ({
             </a>
           ),
         }))
+        .concat({
+          label: (
+            <a data-test="doc-link" href="https://canonical-ubuntu-pro-client.readthedocs-hosted.com/en/latest/references/network_requirements.html">
+              Ubuntu Pro network requirements
+            </a>
+          ),
+        })
         .concat(
           token
             ? [
