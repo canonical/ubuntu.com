868fddb899b5a88e9c8780772c6e06bddf17cd71
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-10-09 00:47:51 +0530

    prevent users from seeing our webhook responses

--- webapp/shop/cred/views.py
+++ webapp/shop/cred/views.py
@@ -630,6 +630,7 @@ def cred_beta_activation(**_):
 
 
 @shop_decorator(area="cred", permission="user", response="json")
+@canonical_staff()
 def get_filtered_webhook_responses(trueability_api, **kwargs):
     ability_screen_id = flask.request.args.get("ability_screen_id", None)
     page = flask.request.args.get("page", 1)
@@ -668,6 +669,7 @@ def get_filtered_webhook_responses(trueability_api, **kwargs):
 
 
 @shop_decorator(area="cred", permission="user", response="json")
+@canonical_staff()
 def get_webhook_response(trueability_api, **kwargs):
     webhook_id = flask.request.args.get("webhook_id", None)
     webhook_responses = trueability_api.get_webhook_response(
