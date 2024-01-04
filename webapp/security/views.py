e1b0b46dad38c84ceec4b61f094b61c0e043256d
MariaPaula Trujillo <mtruj013@fiu.edu>
2024-01-04 15:11:48 +0100

    Switch to cve cards

--- webapp/security/views.py
+++ webapp/security/views.py
@@ -408,14 +408,18 @@ def cve_index():
     }
 
     for cve in cves:
-        for cve_package in cve["packages"]:
-            cve_package["release_statuses"] = {}
-            for status in cve_package["statuses"]:
-                cve_package["release_statuses"][status["release_codename"]] = {
-                    "slug": status["status"],
-                    "name": friendly_names[status["status"]],
-                    "pocket": status["pocket"],
-                }
+        cve["summarized_status"] = {}
+        get_summarized_status(
+            cve, ignored_low_indicators, vulnerable_indicators
+        )
+        # for cve_package in cve["packages"]:
+        #     cve_package["release_statuses"] = {}
+        #     for status in cve_package["statuses"]:
+        #         cve_package["release_statuses"][status["release_codename"]] = {
+        #             "slug": status["status"],
+        #             "name": friendly_names[status["status"]],
+        #             "pocket": status["pocket"],
+        #         }
 
     return flask.render_template(
         "security/cve/index.html",
