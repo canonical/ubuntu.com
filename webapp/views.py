b370c3971dbe9901e2436a42a708f18e261c3e6e
Pete <peterfrench94@gmail.com>
2024-02-13 14:15:01 +0100

    Fetch user IP with 'X-Forward-For'

--- webapp/views.py
+++ webapp/views.py
@@ -1072,9 +1072,13 @@ def thank_you():
 
 
 def get_user_country_by_ip():
-    client_ip = flask.request.headers.get(
-        "X-Real-IP", flask.request.remote_addr
-    )
+    x_forwarded_for = flask.request.headers.get('X-Forwarded-For')
+
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(',')[0]
+    else:
+        client_ip = flask.request.remote_addr
+    
     ip_location = ip_reader.get(client_ip)
 
     try:
