d6d5a1d6b81ce81ab4f5760f50b569156fd13935
Abhigyan Ghosh <abhigyanghosh30@gmail.com>
2024-02-21 20:47:34 +0530

    refactor: lint code

--- static/js/src/advantage/subscribe/react/components/Form/Feature/Feature.tsx
+++ static/js/src/advantage/subscribe/react/components/Form/Feature/Feature.tsx
@@ -27,15 +27,21 @@ const Feature = () => {
   return (
     <>
       <Row>
-        <Col size={6}><h2>What security coverage do you need?</h2></Col>
+        <Col size={6}>
+          <h2>What security coverage do you need?</h2>
+        </Col>
         <Col size={6}>
           <div
             className={classNames({
               row: true,
               "p-divider": true,
             })}
-            data-testid="wrapper">
-            <p>Not sure? Run <code> pro security-status</code> to find out which packages and repositories you are currently using.</p>
+            data-testid="wrapper"
+          >
+            <p>
+              Not sure? Run <code> pro security-status</code> to find out which
+              packages and repositories you are currently using.
+            </p>
             <RadioInput
               label="Pro - all repositories"
               checked={feature === Features.pro}
@@ -44,7 +50,8 @@ const Feature = () => {
               disabled={proDisabled}
             />
             <hr />
-            <RadioInput label="Infra only - limited subset"
+            <RadioInput
+              label="Infra only - limited subset"
               checked={feature === Features.infra}
               value={Features.infra}
               onChange={handleChange}
@@ -56,21 +63,33 @@ const Feature = () => {
           <hr className="is-fixed-width" />
         </Col>
       </Row>
-      <Row >
+      <Row>
         <Col size={2} emptyMedium={2} emptyLarge={2}>
           <h1>{feature === Features.pro ? "25000" : "2300"}</h1>
         </Col>
         <Col size={3}>
-          <p>Covers {feature === Features.pro ? "25000" : "2300"} packages in Ubuntu Main {feature === Features.pro ? "and Universe" : ""}</p>
+          <p>
+            Covers {feature === Features.pro ? "25000" : "2300"} packages in
+            Ubuntu Main {feature === Features.pro ? "and Universe" : ""}
+          </p>
         </Col>
         <Col size={6}>
           <div className="p-graphic-main" />
-          <div className={classNames({
-            "p-graphic-universe": feature === Features.pro,
-            "p-graphic-universe--empty": feature === Features.infra
-          })} />
-          <p className="p-text--small graphic-legend-main">LTS standard security maintenance for Ubuntu Main (initial 5 years)</p>
-          {feature === Features.pro && <p className="p-text--small graphic-legend-universe">LTS Expanded Security Maintenance (ESM) for Ubuntu Main (additional 5 years)</p>}
+          <div
+            className={classNames({
+              "p-graphic-universe": feature === Features.pro,
+              "p-graphic-universe--empty": feature === Features.infra,
+            })}
+          />
+          <p className="p-text--small graphic-legend-main">
+            LTS standard security maintenance for Ubuntu Main (initial 5 years)
+          </p>
+          {feature === Features.pro && (
+            <p className="p-text--small graphic-legend-universe">
+              LTS Expanded Security Maintenance (ESM) for Ubuntu Main
+              (additional 5 years)
+            </p>
+          )}
         </Col>
       </Row>
       <Row>
@@ -84,7 +103,7 @@ const Feature = () => {
                   width="67"
                   height="64"
                   className="p-logo-section__logo"
-                  />
+                />
               </div>
               <div className="p-logo-section__item u-sv1">
                 <img
@@ -140,171 +159,172 @@ const Feature = () => {
                   className="p-logo-section__logo"
                 />
               </div>
-              { feature===Features.pro &&
-              <>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/6cdc1a69-ansible logo.png"
-                  alt=""
-                  width="124"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/605af026-apache tomcat logo.png"
-                  alt=""
-                  width="204"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/5dbf023e-docker logo.png"
-                  alt=""
-                  width="244"
-                  height="245"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/933cc587-etcd logo.png"
-                  alt=""
-                  width="196"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/1f55452f-glusterfs logo.png"
-                  alt=""
-                  width="196"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/973c8e10-Frame 19.png"
-                  alt=""
-                  width="232"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/3ec28413-powerdns logo.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/7e68a46f-perl logo.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/35e5c439-puppet logo.png"
-                  alt=""
-                  width="236"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/2ad3b2ba-python logo.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/8279fc01-go logo.png"
-                  alt=""
-                  width="168"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/94f1e342-nagios logo.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/5b13a988-nginex logo-1.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/56d8741e-node-logo.png"
-                  alt=""
-                  width="196"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/fa78f5b4-openjdk logo.png"
-                  alt=""
-                  width="240"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/05c01589-redis logo.png"
-                  alt=""
-                  width="252"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/850af73c-ruby-rails logo.png"
-                  alt=""
-                  width="240"
-                  height="253"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              <div className="p-logo-section__item u-sv1">
-                <img
-                  src="https://assets.ubuntu.com/v1/93af6c88-rust logo.png"
-                  alt=""
-                  width="120"
-                  height="252"
-                  className="p-logo-section__logo"
-                />
-              </div>
-              </>}
+              {feature === Features.pro && (
+                <>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/6cdc1a69-ansible logo.png"
+                      alt=""
+                      width="124"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/605af026-apache tomcat logo.png"
+                      alt=""
+                      width="204"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/5dbf023e-docker logo.png"
+                      alt=""
+                      width="244"
+                      height="245"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/933cc587-etcd logo.png"
+                      alt=""
+                      width="196"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/1f55452f-glusterfs logo.png"
+                      alt=""
+                      width="196"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/973c8e10-Frame 19.png"
+                      alt=""
+                      width="232"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/3ec28413-powerdns logo.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/7e68a46f-perl logo.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/35e5c439-puppet logo.png"
+                      alt=""
+                      width="236"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/2ad3b2ba-python logo.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/8279fc01-go logo.png"
+                      alt=""
+                      width="168"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/94f1e342-nagios logo.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/5b13a988-nginex logo-1.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/56d8741e-node-logo.png"
+                      alt=""
+                      width="196"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/fa78f5b4-openjdk logo.png"
+                      alt=""
+                      width="240"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/05c01589-redis logo.png"
+                      alt=""
+                      width="252"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/850af73c-ruby-rails logo.png"
+                      alt=""
+                      width="240"
+                      height="253"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                  <div className="p-logo-section__item u-sv1">
+                    <img
+                      src="https://assets.ubuntu.com/v1/93af6c88-rust logo.png"
+                      alt=""
+                      width="120"
+                      height="252"
+                      className="p-logo-section__logo"
+                    />
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </Col>
