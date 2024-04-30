edaf9ac7083e94beb689ef8000ebba994eb9f184
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2024-04-30 23:58:07 +0530

    Fix exam name on shop page

--- static/js/src/advantage/credentials/utils/utils.ts
+++ static/js/src/advantage/credentials/utils/utils.ts
@@ -6,8 +6,10 @@ export type Product = {
   price?: {
     value: number;
     currency: string;
+    original?: number;
   };
   id: string;
   marketplace?: UserSubscriptionMarketplace;
   metadata: Array<{ key: string; value: string }>;
+  displayName?: string;
 };
