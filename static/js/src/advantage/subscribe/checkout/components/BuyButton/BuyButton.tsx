f0cce593ef451987d7a9eeb3fe6931c32e9ed4e2
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2024-05-02 18:47:50 +0530

    prettier formatting

--- static/js/src/advantage/subscribe/checkout/components/BuyButton/BuyButton.tsx
+++ static/js/src/advantage/subscribe/checkout/components/BuyButton/BuyButton.tsx
@@ -315,16 +315,15 @@ const BuyButton = ({ setError, quantity, product, action }: Props) => {
         if (request.readyState === 4) {
           localStorage.removeItem("shop-checkout-data");
           if (product.marketplace == "canonical-cube") {
-            if(product.name==="cue-linux-essentials-free"){
+            if (product.name === "cue-linux-essentials-free") {
               location.href = `/credentials/shop/order-thank-you?productName=${encodeURIComponent(
                 "CUE.01 Linux"
               )}&quantity=${quantity}`;
+            } else {
+              location.href = `/credentials/shop/order-thank-you?productName=${encodeURIComponent(
+                product.name
+              )}&quantity=${quantity}`;
             }
-            else{
-          location.href = `/credentials/shop/order-thank-you?productName=${encodeURIComponent(
-            product.name
-          )}&quantity=${quantity}`;
-          }
           } else if (!window.loginSession) {
             const email = userInfo?.customerInfo?.email || values.email || "";
             let urlBase = "/pro/subscribe";
