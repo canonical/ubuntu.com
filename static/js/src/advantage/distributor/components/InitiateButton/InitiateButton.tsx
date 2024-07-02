f0c36997416adf632166296e45d785354db63153
minkyngkm <min.kim@canonical.com>
2024-07-02 02:16:53 +0100

    Remove /pro/help page

--- static/js/src/advantage/distributor/components/InitiateButton/InitiateButton.tsx
+++ static/js/src/advantage/distributor/components/InitiateButton/InitiateButton.tsx
@@ -9,35 +9,21 @@ type Prop = {
 
 export default function InitiateButton({ offer }: Prop) {
   const { setOffer } = useContext(FormContext);
-  const actionButton = offer.purchase ? (
-    <Button
-      className="u-no-margin--bottom"
-      disabled
-      style={{ backgroundColor: "#262626", color: "#fff" }}
-    >
-      Already used
-    </Button>
-  ) : offer.actionable ? (
-    <Button
-      className="u-no-margin--bottom"
-      onClick={(e) => {
-        e.preventDefault();
-        setOffer(offer);
-        localStorage.setItem("channel-offer-data", JSON.stringify(offer));
-        location.href = "/pro/distributor/shop";
-      }}
-    >
-      Initiate order
-    </Button>
-  ) : (
-    <Button
-      className="u-no-margin--bottom"
-      appearance="negative"
-      onClick={() => (location.href = "/pro/help")}
-    >
-      Report issue
-    </Button>
-  );
 
-  return <div className="distributor-initiate-button">{actionButton}</div>;
+  return (
+    <div className="distributor-initiate-button">
+      <Button
+        className="u-no-margin--bottom"
+        onClick={(e) => {
+          e.preventDefault();
+          setOffer(offer);
+          localStorage.setItem("channel-offer-data", JSON.stringify(offer));
+          location.href = "/pro/distributor/shop";
+        }}
+        disabled={!offer.actionable || offer.purchase}
+      >
+        {offer.purchase ? "Already used" : "Initiate order"}
+      </Button>
+    </div>
+  );
 }
