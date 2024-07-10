cf25724199d1bbfe47af098def1c5d62b29dfc08
usamabinnadeem-10 <usamabinnadeem10@gmail.com>
2024-07-10 17:24:36 +0500

    fix expiration for cue exam to one month

--- webapp/shop/cred/views.py
+++ webapp/shop/cred/views.py
@@ -854,6 +854,7 @@ def cred_shop(ua_contracts_api, **kwargs):
                 exam["period"] = product["period"]
                 exam["marketplace"] = product["marketplace"]
                 exam["name"] = product["name"]
+                exam["period"] = "monthly"
 
     # purchase account required for purchasing from marketplace
     ua_contracts_api.ensure_purchase_account("canonical-cube")
