110b32ea82a6fbbfe6fbe322e41e645b42bbce73
Britney Wang <britney.wang@canonical.com>
2024-07-12 14:01:52 +0800

    Add js functionality for required checkbox fields

--- static/js/src/static-forms.js
+++ static/js/src/static-forms.js
@@ -198,9 +198,7 @@ if (forms.length) {
  * - When all visible checkboxes or any __other checkbox is unchecked, all checkboxes will be enabled
  */
 function toggleCheckboxVisibility(fieldset, checklistItem) {
-  const checkboxes = fieldset.querySelectorAll(
-    ".js-checkbox-visibility"
-  );
+  const checkboxes = fieldset.querySelectorAll(".js-checkbox-visibility");
   const otherCheckboxes = fieldset.querySelectorAll(
     ".js-checkbox-visibility__other"
   );
@@ -216,7 +214,7 @@ function toggleCheckboxVisibility(fieldset, checklistItem) {
         checkbox.disabled = true;
       });
       otherCheckboxes.forEach((checkbox) => {
-        checklistItem == checkbox ? null : checkbox.disabled = true ;        
+        checklistItem == checkbox ? null : (checkbox.disabled = true);
       });
     }
   } else {
@@ -240,10 +238,38 @@ function toggleCheckboxVisibility(fieldset, checklistItem) {
     }
   }
 }
-
-const ubuntuVersionsFieldset = document.querySelector(
+const ubuntuVersionCheckboxes = document.querySelector(
   "fieldset.js-toggle-checkbox-visibility"
 );
-ubuntuVersionsFieldset.addEventListener("change", function (event) {
-  toggleCheckboxVisibility(ubuntuVersionsFieldset, event.target);
+ubuntuVersionCheckboxes.addEventListener("change", function (event) {
+  toggleCheckboxVisibility(ubuntuVersionCheckboxes, event.target);
+});
+
+/**
+ *
+ * @param {*} fieldset
+ * @param {*} target
+ * Disables submit button for required checkboxes field
+ */
+function requiredCheckbox(fieldset, target) {
+  const submitButton = document.querySelector(".js-submit-button");
+  const checkboxes = fieldset.querySelectorAll("input[type='checkbox']");
+  if (target.checked) {
+    submitButton.disabled = false;
+  } else {
+    var disableSubmit = true;
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked ? (disableSubmit = false) : null;
+    });
+    submitButton.disabled = disableSubmit;
+  }
+}
+
+const requiredFieldset = document.querySelectorAll(
+  "fieldset.js-required-checkbox"
+);
+requiredFieldset?.forEach((fieldset) => {
+  fieldset.addEventListener("change", function (event) {
+    requiredCheckbox(fieldset, event.target);
+  });
 });
