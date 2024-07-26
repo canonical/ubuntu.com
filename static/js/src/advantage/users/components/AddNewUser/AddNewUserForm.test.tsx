134352d1aeacf58fb583cdab2a562ea9fbed232d
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2024-07-26 12:47:08 +0500

    fix linters

--- static/js/src/advantage/users/components/AddNewUser/AddNewUserForm.test.tsx
+++ static/js/src/advantage/users/components/AddNewUser/AddNewUserForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import AddNewUserForm from "./AddNewUserForm";
