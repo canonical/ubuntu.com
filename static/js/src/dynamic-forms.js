a5367caec22eef4d0d0204f84f85bf623d8df9d5
yelyzaveta <yelyzaveta@MacBook-Pro-Liza.local>
2024-09-05 12:41:04 +0300

    WD-13861 -  formt

--- static/js/src/dynamic-forms.js
+++ static/js/src/dynamic-forms.js
@@ -338,61 +338,63 @@ import { prepareInputFields } from "./prepare-form-inputs.js";
               message += fieldTitle.innerText + "\r\n";
             }
 
-          inputs.forEach(function (input) {
-            switch (input.type) {
-              case "select-one":
-                message +=
-                  input.options[input.selectedIndex]?.textContent + comma + " ";
-                break;
-              case "radio":
-                if (input.checked) {
-                  message += input.value + comma + " ";
-                }
-                break;
-              case "checkbox":
-                if (input.checked) {
-                  if (fieldsetForm) {
+            inputs.forEach(function (input) {
+              switch (input.type) {
+                case "select-one":
+                  message +=
+                    input.options[input.selectedIndex]?.textContent +
+                    comma +
+                    " ";
+                  break;
+                case "radio":
+                  if (input.checked) {
                     message += input.value + comma + " ";
-                  } else {
-                    // Forms that have column separation
-                    var subSectionText = "";
-                    if (
-                      input.closest('[class*="col-"]') &&
-                      input
-                        .closest('[class*="col-"]')
-                        .querySelector(".js-sub-section")
-                    ) {
-                      var subSection = input
-                        .closest('[class*="col-"]')
-                        .querySelector(".js-sub-section");
-                      subSectionText = subSection.innerText + ": ";
-                    }
-
-                    var label = formField.querySelector(
-                      "span#" + input.getAttribute("aria-labelledby"),
-                    );
-
-                    if (label) {
-                      label = subSectionText + label.innerText;
-                    } else {
-                      label = input.getAttribute("aria-labelledby");
-                    }
-                    message += label + comma + "\r\n\r\n";
                   }
-                }
-                break;
-              case "text":
-              case "number":
-              case "textarea":
-                if (input.value !== "") {
-                  message += input.value + comma + " ";
-                }
-                break;
-            }
+                  break;
+                case "checkbox":
+                  if (input.checked) {
+                    if (fieldsetForm) {
+                      message += input.value + comma + " ";
+                    } else {
+                      // Forms that have column separation
+                      var subSectionText = "";
+                      if (
+                        input.closest('[class*="col-"]') &&
+                        input
+                          .closest('[class*="col-"]')
+                          .querySelector(".js-sub-section")
+                      ) {
+                        var subSection = input
+                          .closest('[class*="col-"]')
+                          .querySelector(".js-sub-section");
+                        subSectionText = subSection.innerText + ": ";
+                      }
+
+                      var label = formField.querySelector(
+                        "span#" + input.getAttribute("aria-labelledby"),
+                      );
+
+                      if (label) {
+                        label = subSectionText + label.innerText;
+                      } else {
+                        label = input.getAttribute("aria-labelledby");
+                      }
+                      message += label + comma + "\r\n\r\n";
+                    }
+                  }
+                  break;
+                case "text":
+                case "number":
+                case "textarea":
+                  if (input.value !== "") {
+                    message += input.value + comma + " ";
+                  }
+                  break;
+              }
+            });
+            message += "\r\n\r\n";
           });
-          message += "\r\n\r\n";
-        });
-        return message;
+          return message;
         }
       }
 
