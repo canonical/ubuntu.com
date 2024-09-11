42f99144beffcee9ed257e9859d6d01121e19f8c
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2024-09-12 03:26:54 +0500

    app apis for proctor360

--- webapp/shop/api/proctor/api.py
+++ webapp/shop/api/proctor/api.py
@@ -1,5 +1,3 @@
-from typing import List
-from urllib.parse import urlencode
 from requests import Session
 import os
 import datetime
@@ -62,6 +60,32 @@ class ProctorAPI:
 
         return response
 
+    def __check_keys_exist(self, keys, dictionary):
+        """
+        Check if all keys in the list are present in the dictionary.
+
+        :param keys: List of keys to check.
+        :param dictionary: Dictionary to check against.
+        :raises KeyError: If any key is not present in the dictionary.
+        """
+        missing_keys = set(keys) - dictionary.keys()
+        if missing_keys:
+            raise KeyError(
+                f"The following keys are missing: {', '.join(missing_keys)}"
+            )
+
+    def filter_dict_by_keys(self, keys, dictionary):
+        """
+        Return a new dictionary containing only the keys found in
+        both the keys list and the dictionary.
+
+        :param keys: List of keys to filter.
+        :param dictionary: Dictionary to filter.
+        :return: A new dictionary with keys that are present in both the keys
+        list and the dictionary.
+        """
+        return {key: dictionary[key] for key in keys if key in dictionary}
+
     def authenticate(self):
         uri = "/api/v2/organisations/132/oauth/token"
         body = {
@@ -77,219 +101,53 @@ class ProctorAPI:
         uri = "/api/v2/exams"
         return self.make_request("GET", uri).json()
 
-    def paginate(self, action, result_list_name=str, **kwargs):
-        result = {result_list_name: []}
+    def create_student(self, student: dict):
+        uri = "/api/v2/student"
+        required_keys = ["first_name", "last_name", "email"]
+        optional_keys = ["ext_tenant_id", "ext_student_id"]
+        self.__check_keys_exist(required_keys, student)
+        data = self.filter_dict_by_keys(required_keys + optional_keys, student)
+        return self.make_request("POST", uri, json=data).json()
 
-        page = 1
-        while True:
-            response = action(page=page, **kwargs)
-            result_list = response.get(result_list_name, [])
-            result[result_list_name] = result[result_list_name] + result_list
+    def get_student(self, email: str):
+        uri = f"/api/v2/student?email={email}"
+        return self.make_request("GET", uri, retry=False).json()
 
-            page = response.get("meta", {}).get("next_page")
-            if not page:
-                break
+    def get_or_create_student(self, student: dict):
+        response = self.get_student(student["email"])
+        if response["student"]["id"]:
+            return response
+        return self.create_student(student)
 
-        return result
-
-    def get_ability_screens(self):
-        uri = "/api/v1/ability_screens"
-        return self.make_request("GET", uri).json()
-
-    def update_ability_screen(self):
-        pass
-
-    def get_assessment_reservation(self, uuid: str = ""):
-        uri = f"/api/v1/assessment_reservations/{uuid}"
-        return self.make_request("GET", uri).json()
-
-    def get_assessment_reservations(
-        self,
-        ability_screen_id: int = None,
-        page: int = 1,
-        per_page: int = 50,
-        sort: str = None,
-        group: str = None,
-        state: List[str] = None,
-        assessment_state: str = None,
-    ):
-        params = {
-            "ability_screen_id": ability_screen_id,
-            "page": page,
-            "per_page": per_page,
-            "sort": sort,
-            "group": group,
-            "state[]": state,
-            "assessment_state[]": assessment_state,
-        }
-        filtered_params = {k: v for k, v in params.items() if v is not None}
-        uri = "/api/v1/assessment_reservations?" + urlencode(
-            filtered_params, True
+    def create_student_session(self, student_session: dict):
+        uri = "/api/v2/student-sessions"
+        required_keys = [
+            "first_name",
+            "student_email",
+            "exam_date_time",
+            "client_exam_id",
+        ]
+        optional_keys = ["last_name", "timezone", "ai_enabled"]
+        self.__check_keys_exist(required_keys, student_session)
+        data = self.filter_dict_by_keys(
+            required_keys + optional_keys, student_session
         )
-        return self.make_request("GET", uri).json()
+        return self.make_request("POST", uri, json=data).json()
 
-    def post_assessment_reservation(
-        self,
-        ability_screen_id: int,
-        starts_at: str,
-        email: str,
-        first_name: str,
-        last_name: str,
-        timezone: str,
-        country_code: str,
-    ):
-        uri = "/api/v1/assessment_reservations"
-        body = {
-            "assessment_reservation": {
-                "ability_screen_id": ability_screen_id,
-                "starts_at": starts_at,
-                "address_attributes": {
-                    "country_code": country_code,
-                },
-            },
-            "user": {
-                "email": email,
-                "first_name": first_name,
-                "last_name": last_name,
-                "time_zone": timezone,
-            },
-        }
-        return self.make_request("POST", uri, json=body).json()
-
-    def patch_assessment_reservation(
-        self, starts_at: str, timezone: str, country_code: str, uuid: str
-    ):
-        uri = f"/api/v1/assessment_reservations/{uuid}"
-        body = {
-            "assessment_reservation": {
-                "starts_at": starts_at,
-                "address_attributes": {
-                    "country_code": country_code,
-                },
-            },
-            "user": {"time_zone": timezone},
-        }
-        return self.make_request("PATCH", uri, json=body).json()
-
-    def delete_assessment_reservation(self, uuid: str):
-        uri = f"/api/v1/assessment_reservations/{uuid}"
-        return self.make_request("DELETE", uri).json()
-
-    def get_assessments(
-        self,
-        ability_screen_id: int = None,
-        uuid: str = None,
-        page: int = 1,
-        per_page: int = 500,
-    ):
-        params = {
-            "ability_screen_id": ability_screen_id,
-            "uuid": uuid,
-            "page": page,
-            "per_page": per_page,
-        }
-        filtered_params = {k: v for k, v in params.items() if v is not None}
-        uri = "/api/v1/assessments?" + urlencode(filtered_params)
-        return self.make_request("GET", uri).json()
-
-    def get_assessment(self, id: str):
-        uri = f"/api/v1/assessments/{id}"
-        return self.make_request("GET", uri).json()
-
-    def get_assessment_redirect(self, id: str):
-        uri = "/api/v1/assessments/redirect_to_environment" + (
-            f"?id={id}" if id else ""
-        )
-        response = self.make_request("GET", uri, allow_redirects=False)
-
-        if response.status_code == 302 and "Location" in response.headers:
-            return response.headers["Location"]
-
-        return None
-
-    def get_results(
-        self,
-        page: int = 1,
-        per_page: int = 50,
-        state: str = None,
-        ability_screen_id: list = None,
-    ):
-        params = {
-            "state": state,
-            "page": page,
-            "per_page": per_page,
-            "ability_screen_id[]": ability_screen_id,
-        }
-        filtered_params = {k: v for k, v in params.items() if v is not None}
-        uri = "/api/v1/results?" + urlencode(filtered_params)
-        return self.make_request("GET", uri).json()
-
-    def get_candidate_access_token_status(self, id: int):
-        uri = f"/api/v1/candidate_access_tokens/{id}"
-        return self.make_request("GET", uri).json()
-
-    def get_candidate_invitations(
-        self, ability_screen_id: int = None, code: str = None
-    ):
-        uri = (
-            "/api/v1/candidate_invitations"
-            + (f"/{code}" if code else "")
-            + (
-                f"?ability_screen_id={ability_screen_id}"
-                if ability_screen_id
-                else ""
-            )
-        )
-        return self.make_request("GET", uri).json()
-
-    def post_candidate_invitation(
-        self,
-        ability_screen_id: int,
-        email: str,
-        first_name: str,
-        last_name: str,
-        expires_at: str,
-    ):
-        uri = "/api/v1/candidate_invitations"
-        # headers = {"Content-Type": "application/json"}
-        body = {
-            "candidate_invitation": {
-                "ability_screen_id": ability_screen_id,
-                "email": email,
-                "first_name": first_name,
-                "last_name": last_name,
-                "expires_at": expires_at,
-            }
-        }
-        return self.make_request("POST", uri, json=body).json()
-
-    def get_companies(self, hashid: str = None):
-        uri = "/api/v1/companies" + (f"/{hashid}" if hashid else "")
-        return self.make_request("GET", uri).json()
-
-    def get_webhooks(self, ability_screen_id: int):
-        uri = "/api/v1/webhooks" f"?ability_screen_id={ability_screen_id}"
-        return self.make_request("GET", uri).json()
-
-    def get_system_status(self):
-        uri = "/api/v1/system_status"
-        return self.make_request("GET", uri).json()
-
-    def get_webhook_response(self, webhook_id: int):
-        uri = f"/api/v1/webhook_responses/{webhook_id}"
-        return self.make_request("GET", uri).json()
-
-    def get_filtered_webhook_responses(
-        self, ability_screen_id: str = None, page: int = 1
-    ):
-        uri = (
-            f"{self.base_url}"
-            "/api/v1/webhook_responses"
-            f"?ability_screen_id={ability_screen_id}"
-            f"&page={page}"
-        )
-        headers = {"X-API-KEY": self.api_key}
-        response = self.session.request(
-            method="GET", url=uri, headers=headers
-        ).json()
-        return response
+    def update_student_session(self, session_id: str, student_session: dict):
+        uri = f"/api/v2/student-session/{session_id}"
+        optional_keys = [
+            "exam_id",
+            "exam_date_time",
+            "live_proctoring",
+            "mobile_camera",
+            "fullview_camera",
+            "second_id_card",
+            "camspin",
+            "face_verification",
+            "secure_browsing",
+            "secure_browsing_id",
+            "can_upload_attachment",
+        ]
+        data = self.filter_dict_by_keys(optional_keys, student_session)
+        return self.make_request("PATCH", uri, json=data).json()
