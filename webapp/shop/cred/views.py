63820958e68b71cc5bfe3a6492c3526142269a5c
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2024-09-12 23:38:23 +0500

    Refactor ProctorAPI class to handle student sessions and create/update student sessions with proctor

--- webapp/shop/cred/views.py
+++ webapp/shop/cred/views.py
@@ -388,6 +388,9 @@ def cred_schedule(
                             "client_exam_id": 1,
                             "timezone": timezone,
                             "ai_enabled": "1",
+                            "exam_link": "https://ubuntu.com/"
+                            + "credentials/exam?uuid="
+                            + f"{response['assessment_reservation']['uuid']}",
                         }
                         proctor_api.update_student_session(
                             student_session_id,
@@ -406,7 +409,7 @@ def cred_schedule(
                             "timezone": timezone,
                             "ai_enabled": "1",
                             "exam_link": "https://ubuntu.com/"
-                            + "credentials/exam?id="
+                            + "credentials/exam?uuid="
                             + f"{response['assessment_reservation']['uuid']}",
                         }
                         proctor_api.create_student_session(
@@ -484,7 +487,7 @@ def cred_schedule(
                         "timezone": timezone,
                         "ai_enabled": "1",
                         "exam_link": "https://ubuntu.com/credentials/"
-                        + f"exam?id={uuid}",
+                        + f"exam?uuid={uuid}",
                     }
                     proctor_api.create_student_session(student_session_data)
                 except Exception as error:
