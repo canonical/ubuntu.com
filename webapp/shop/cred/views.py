4465c48f0080896e7a5a48cc3167d318856d0603
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2024-09-13 15:34:08 +0500

    Dummy commit

--- webapp/shop/cred/views.py
+++ webapp/shop/cred/views.py
@@ -241,8 +241,8 @@ def cred_schedule(
     proctor_api,
     **_,
 ):
-    base_url = flask.request.url_root
     user = user_info(flask.session)
+    base_url = flask.request.url_root
     error = None
     now = datetime.utcnow()
     min_date = (now + timedelta(minutes=30)).strftime("%Y-%m-%d")
