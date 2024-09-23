92343425f8c6502edc936d3081c641c9565ff63b
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-09-23 11:51:36 +0200

    add more details in the success message for organisations

--- static/js/src/canonical-cla/pages/SuccessPage.tsx
+++ static/js/src/canonical-cla/pages/SuccessPage.tsx
@@ -2,12 +2,17 @@ import { Button, Notification } from "@canonical/react-components";
 import { useSignForm } from "../contexts/SignForm";
 
 const SuccessPage = () => {
-  const { changeStep } = useSignForm();
-
+  const { changeStep, agreementType } = useSignForm();
   return (
     <div>
       <Notification title="Success" severity="positive">
-        Thank you for signing the Contributor License Agreement.
+        <p>Thank you for signing the Contributor License Agreement.</p>
+        {agreementType === "organization" && (
+          <p>
+            A member of the Canonical team will review your organization&apos;s
+            CLA and get back to you shortly.
+          </p>
+        )}
       </Notification>
       <Button onClick={() => changeStep("sign")}>Back to form</Button>
     </div>
