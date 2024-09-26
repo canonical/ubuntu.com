edeb9db3aae27f83cc6758d232eb267cd2fe9664
Pete <peterfrench94@gmail.com>
2024-09-26 07:37:03 +0200

    refactor: Remove unused logs

--- static/js/src/search.js
+++ static/js/src/search.js
@@ -7,15 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     searchForm.addEventListener("submit", function (event) {
       event.preventDefault();
       const honeyPotField = searchForm.querySelector("input[name='search']");
-      // If the honeypot field has a value, it might be a bot, so redirect
       if (honeyPotField && honeyPotField.value !== "") {
-        console.log("Honeypot field has a value, redirecting to search page");
         window.location.replace("/search");
         return;
       }
-      // Remove the honeypot field before manual submission
       if (honeyPotField) {
-        console.log("Removing honeypot field", honeyPotField);
         honeyPotField.remove();
       }
       searchForm.submit();
