2d839dec39aea3b330b5e982612f6a7c2d937e92
Pete <peterfrench94@gmail.com>
2024-12-05 10:12:58 +0100

    refactor: Remove rogue console.log

--- static/js/src/navigation.js
+++ static/js/src/navigation.js
@@ -83,7 +83,6 @@ mainList.addEventListener("click", function (e) {
 // The current set up of the meganav doesn't work well with notifications. The simplest fix for this is to close all notifications when a dropdown is clicked. Can be removed on migration to vanilla meganav.
 function closeNotifications() {
   const notification = document.querySelector(".p-popup-notification:target");
-  console.log(notification);
   notification.style.display = "none";
 }
 
