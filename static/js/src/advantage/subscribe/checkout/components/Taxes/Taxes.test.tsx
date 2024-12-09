b23ad181ffb4ed0007ea4b44f5c007a7823ddd5d
minkyngkm <min.kim@canonical.com>
2024-12-09 13:33:39 +0900

    Add tests

--- static/js/src/advantage/subscribe/checkout/components/Taxes/Taxes.test.tsx
+++ static/js/src/advantage/subscribe/checkout/components/Taxes/Taxes.test.tsx
@@ -2,9 +2,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Formik } from "formik";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { fireEvent, render, screen } from "@testing-library/react";
-import { UAProduct } from "../../utils/test/Mocks";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { DistributorProduct, UAProduct } from "../../utils/test/Mocks";
 import Taxes from "./Taxes";
+import userEvent from "@testing-library/user-event";
+import { UserSubscriptionMarketplace } from "advantage/api/enum";
 
 describe("TaxesTests", () => {
   let queryClient: QueryClient;
@@ -12,6 +14,10 @@ describe("TaxesTests", () => {
 
   beforeEach(async () => {
     queryClient = new QueryClient();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("renders country select correctly", () => {
@@ -211,5 +217,97 @@ describe("TaxesTests", () => {
     fireEvent.click(screen.getByRole("button", { name: "Cancel" }));
     expect(screen.getByTestId("country")).toHaveTextContent("United Kingdom");
     expect(screen.getByTestId("vat-number")).toHaveTextContent("GB123123123");
+  });
+
+  it("Edit should be available for pro users", async () => {
+    global.window = Object.create(window);
+    Object.defineProperty(window, "accountId", { value: "ABCDEF" });
+
+    const initialValues = {
+      country: "GB",
+      VATNumber: "GB123123123",
+      marketPlace: "canonical-ua",
+    };
+
+    const products = [
+      {
+        product: UAProduct,
+        quantity: 1,
+      },
+    ];
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Formik initialValues={initialValues} onSubmit={jest.fn()}>
+          <Elements stripe={stripePromise}>
+            <Taxes products={products} setError={jest.fn()} />
+          </Elements>
+        </Formik>
+      </QueryClientProvider>,
+    );
+
+    expect(screen.queryByRole("button", { name: "Edit" })).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "Edit" }));
+    await waitFor(() => {
+      expect(screen.getByTestId("select-country")).toBeInTheDocument();
+      expect(screen.getByTestId("field-vat-number")).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "Save" })).toBeInTheDocument();
+    });
+  });
+
+  it("Edit button should not be displayed for channel users", () => {
+    global.window = Object.create(window);
+    Object.defineProperty(window, "accountId", { value: "ABCDEF" });
+    const initialValues = {
+      country: "GB",
+      VATNumber: "GB123123123",
+      marketPlace: UserSubscriptionMarketplace.CanonicalProChannel,
+    };
+    const products = [
+      {
+        product: DistributorProduct,
+        quantity: 1,
+      },
+    ];
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Formik initialValues={initialValues} onSubmit={jest.fn()}>
+          <Elements stripe={stripePromise}>
+            <Taxes products={products} setError={jest.fn()} />
+          </Elements>
+        </Formik>
+      </QueryClientProvider>,
+    );
+
+    expect(screen.queryByTestId("tax-edit-button")).toBeInTheDocument();
+  });
+
+  it("New channel users should be able to add their tax info", async () => {
+    global.window = Object.create(window);
+    Object.defineProperty(window, "accountId", { value: "ABCDEF" });
+
+    const initialValues = {
+      country: "",
+      VATNumber: "",
+      marketPlace: UserSubscriptionMarketplace.CanonicalProChannel,
+    };
+
+    const products = [
+      {
+        product: DistributorProduct,
+        quantity: 1,
+      },
+    ];
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Formik initialValues={initialValues} onSubmit={jest.fn()}>
+          <Elements stripe={stripePromise}>
+            <Taxes products={products} setError={jest.fn()} />
+          </Elements>
+        </Formik>
+      </QueryClientProvider>,
+    );
+
+    expect(screen.queryByTestId("tax-edit-button")).not.toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Save" })).toBeInTheDocument();
   });
 });
