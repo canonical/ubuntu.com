d01c227bc45839dfb942ba99b0ddbb11a816006e
Olwe Samuel <sam.olwe@gmail.com>
2024-12-13 12:41:58 +0300

    Rate limit marketo endpoint

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -1,8 +1,12 @@
 # Core packages
 import functools
+import json
+from datetime import datetime, timedelta
+from typing import Callable
 
 # Third party packages
 import flask
+
 from webapp.login import user_info
 
 
@@ -20,3 +24,49 @@ def login_required(func):
         return func(*args, **kwargs)
 
     return is_user_logged_in
+
+
+def rate_limit_with_backoff(func: Callable):
+    """
+    Decorator to rate limit function calls based on the users' session.
+    The rate limit restricts users to:
+    - 1 request every 2 seconds
+    - 2 request every 4 seconds
+    - 3 request every 8 seconds
+    """
+    rate_limit_attempt_map = {
+        1: timedelta(seconds=2),
+        2: timedelta(seconds=4),
+        3: timedelta(seconds=8),
+    }
+    ATTEMPT_LIMIT = 3
+
+    @functools.wraps(func)
+    def rate_limited(*args, **kwargs):
+        # Get the initial request timestamp, or update the session with the
+        # timestamp from the most recent successful request
+        if initial_request := json.loads(flask.session.get(func.__name__)):
+            # Get the current limit
+            current_limit = rate_limit_attempt_map.get(initial_request["attempts"])
+
+            time_since_last_request = datetime.now() - datetime.fromtimestamp(
+                initial_request["timestamp"]
+            )
+            # Abort if the time is too early for this number of attempts
+            if time_since_last_request.total_seconds() < current_limit.total_seconds():
+                # Increment the number of attempts. 3 is a hard upper limit.
+                if initial_request["attempts"] < ATTEMPT_LIMIT:
+                    initial_request["attempts"] += 1
+                    flask.session[func.__name__] = json.dumps(
+                        initial_request["attempts"]
+                    )
+
+                return flask.abort(429)
+
+        # Set values for a successful request
+        flask.session[func.__name__] = json.dumps(
+            {"timestamp": datetime.now().timestamp(), "attempts": 1}
+        )
+        return func(*args, **kwargs)
+
+    return rate_limited
