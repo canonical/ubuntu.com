ec641bb641e133a665110ee8739eb94be0c73980
Samuel Olwe <samuel.olwe@canonical.com>
2024-12-20 20:32:13 +0300

    feat: allow passing rate limit params

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -1,13 +1,11 @@
 # Core packages
-import contextlib
 import functools
 import json
 from datetime import datetime, timedelta
-from typing import Callable
+from typing import Callable, Optional
 
 # Third party packages
 import flask
-
 from webapp.login import user_info
 
 
@@ -27,51 +25,70 @@ def login_required(func):
     return is_user_logged_in
 
 
-def rate_limit_with_backoff(func: Callable):
+def rate_limit_with_backoff(
+    func: Callable, limits: Optional[tuple[int, int]] = None
+) -> Callable:
     """
-    Decorator to rate limit function calls based on the users' session.
-    The rate limit restricts users to:
-    - 1 request every 8 seconds
-    - 2 request every 16 seconds
-    - 3 request every 32 seconds
+    Decorator to rate limit function calls based on the users'
+    session. The default rate limit restricts users to:
+    - 1 request every 4 seconds
+    - 4 requests every 60 seconds
+
+    This can be overwritten with the limits argument e.g.
+        @rate_limit_with_backoff(limits=(1, 10))
+    for 1 request every 10 seconds.
+
+    @param func: Function to decorate
+    @param limits: Tuple of (requests, seconds) request limit mappings
     """
+
     rate_limit_attempt_map = {
-        1: timedelta(seconds=8),
-        2: timedelta(seconds=16),
-        3: timedelta(seconds=32),
+        1: timedelta(seconds=4),
+        4: timedelta(seconds=60),
     }
-    ATTEMPT_LIMIT = 3
+
+    if limits:
+        additional_limits = {i: timedelta(seconds=j) for i, j in limits}
+        rate_limit_attempt_map = additional_limits
+
+    # The request limit is derived from the limit attempt map
+    request_limit = max(sorted(rate_limit_attempt_map.keys(), reverse=True))
 
     @functools.wraps(func)
     def rate_limited(*args, **kwargs):
-        # Get the initial request timestamp, or update the session with the
-        # timestamp from the most recent successful request
-        with contextlib.suppress(TypeError):
-            initial_request = json.loads(flask.session.get(func.__name__))
+        try:
+            # Get the initial request
+            initial_request = json.loads(flask.session[func.__name__])
             # Get the current limit
-            current_limit = rate_limit_attempt_map.get(
-                initial_request["attempts"]
-            )
+            seconds_limit = rate_limit_attempt_map.get(request_limit)
+            for limit in sorted(rate_limit_attempt_map.keys()):
+                seconds_limit = rate_limit_attempt_map.get(limit)
+                if limit > initial_request["attempts"]:
+                    break
 
             time_since_last_request = datetime.now() - datetime.fromtimestamp(
                 initial_request["timestamp"]
             )
             # Abort if the time is too early for this number of attempts
-            if (
-                time_since_last_request.total_seconds()
-                < current_limit.total_seconds()
-            ):
-                # Increment the number of attempts. 3 is a hard upper limit.
-                if initial_request["attempts"] < ATTEMPT_LIMIT:
-                    initial_request["attempts"] += 1
-                    flask.session[func.__name__] = json.dumps(initial_request)
+            # Or if the max number of attempts has been exceeded
+            if initial_request["attempts"] >= request_limit:
+                # Reset the timer if we have exceeded the limit
+                if (
+                    time_since_last_request.total_seconds()
+                    < seconds_limit.total_seconds()
+                ):
+                    return flask.abort(429)
+                else:
+                    initial_request["timestamp"] = datetime.now()
 
-                return flask.abort(429)
-
-        # Set values for a successful request
-        flask.session[func.__name__] = json.dumps(
-            {"timestamp": datetime.now().timestamp(), "attempts": 1}
-        )
+            # Otherwise update the session
+            initial_request["attempts"] += 1
+            flask.session[func.__name__] = json.dumps(initial_request)
+        except (KeyError, TypeError):
+            # Set values for initial request
+            flask.session[func.__name__] = json.dumps(
+                {"timestamp": datetime.now().timestamp(), "attempts": 1}
+            )
         return func(*args, **kwargs)
 
     return rate_limited
