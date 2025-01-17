705fcf7df7808a506e824aea3e08e7c09696e345
Samuel Olwe <samuel.olwe@canonical.com>
2025-01-17 04:05:26 +0300

    fix: blank page when opening anchors

--- static/js/src/tutorial.js
+++ static/js/src/tutorial.js
@@ -26,6 +26,36 @@ function setActiveLink(navigationItems, hash) {
   });
 }
 
+function scrollToRegularAnchor() {
+  /**
+   * As a result of navigation being tightly coupled to css, anchors for
+   * regular links are not working as expected. This function is a workaround.
+   */
+  console.log("scrollToRegularAnchor");
+  const match = window.location.hash.match(/^#(.*)$/);
+  const digitMatch = window.location.hash.match(/^#(\d+).*$/);
+  if (match && !digitMatch) {
+    // Get the section id from the anchor link
+    const header = document.querySelectorAll(
+      `a[href='${window.location.hash}']`,
+    )[0];
+    if (header) {
+      const sectionId = header.parentNode.parentNode.parentNode.id;
+
+      const reloadSection = new Promise((resolve) => {
+        window.location.hash = sectionId;
+        resolve();
+      });
+      reloadSection.then(() => {
+        header.scrollIntoView({ behavior: "smooth", block: "center" });
+      });
+    } else {
+      // Go to the default section
+      window.location.href = window.location.href.split("#")[0];
+    }
+  }
+}
+
 const navigationItems = document.querySelectorAll(".l-tutorial__nav-item");
 const toggleButton = document.querySelector(".l-tutorial__nav-toggle");
 
@@ -43,6 +73,7 @@ navigationItems.forEach((item) => {
 window.addEventListener("hashchange", (e) => {
   e.preventDefault();
   setActiveLink(navigationItems, window.location.hash);
+  scrollToRegularAnchor();
 });
 
 const sectionIds = [];
@@ -65,14 +96,7 @@ if (!window.location.hash) {
   }
 } else {
   // Redirect #0, #1 etc. to the correct section
-  const match = window.location.hash.match(/^#(\d+)$/);
-
-  if (match) {
-    const index = parseInt(match[1]);
-    const sectionId = sectionIds[index];
-    window.location.hash = "#" + sectionId;
-    window.location.reload();
-  }
+  scrollToRegularAnchor();
 }
 
 const tutorialFeedbackOptions = document.querySelector(
