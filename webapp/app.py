b2320968db52b9e2401da44c7fed8d70d3760dda
Olwe Samuel <samuel.olwe@canonical.com>
2025-01-31 15:54:56 +0300

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1225,7 +1225,8 @@ app.add_url_rule("/supermicro", view_func=render_supermicro_blogs)
 
 
 def testip():
-    flask.Response.cache_control  = "no-store max-age=0"
+    flask.Response.cache_control.no_store = True
+    flask.Response.cache_control.max_age = 0
     return flask.jsonify(
         {
             "ip": flask.request.remote_addr,
