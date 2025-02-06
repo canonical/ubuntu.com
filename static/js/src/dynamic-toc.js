dd5d82d7cc2fc04685ef4103901189bdac7470a7
Peter French <peterfrench94@gmail.com>
2025-02-06 14:02:29 +0100

    fix: Adjust intersectionobserver parameters to change tab at the appropriate time

--- static/js/src/dynamic-toc.js
+++ static/js/src/dynamic-toc.js
@@ -1,37 +1,46 @@
 import { toggleDrawer } from "./side-navigation.js";
 
 function setUpDynamicSideNav() {
-  const sideNavigationElement = document.querySelector(".p-side-navigation--raw-html");
+  const sideNavigationElement = document.querySelector(
+    ".p-side-navigation--raw-html",
+  );
   const sideNavigationToggle = document.querySelector(".js-drawer-toggle");
   const sections = Array.prototype.slice.call(
-    document.querySelectorAll(".section-heading")
+    document.querySelectorAll(".section-heading"),
   );
   const navigationLinks = Array.prototype.slice.call(
-    document.querySelectorAll(".highlight-link")
+    document.querySelectorAll(".highlight-link"),
   );
-  navigationLinks.forEach(function(link) {
-    link.addEventListener("click", () => toggleDrawer(sideNavigationElement, false));
+  navigationLinks.forEach(function (link) {
+    link.addEventListener("click", () =>
+      toggleDrawer(sideNavigationElement, false),
+    );
   });
-  sideNavigationToggle.addEventListener("click", () => toggleDrawer(sideNavigationElement, true));
+  sideNavigationToggle.addEventListener("click", () =>
+    toggleDrawer(sideNavigationElement, true),
+  );
 
-  sections.forEach(function(section) {
-    const observer = new IntersectionObserver(function(entry) {
-      if (entry[0].isIntersecting) {
-        const sectionId = entry[0].target.id;
-        navigationLinks.forEach(function(link) {
-          if (link.getAttribute("href") === `#${sectionId}`) {
-            link.classList.add("is-active");
-          } else {
-            link.classList.remove("is-active");
-          }
-        });
-      }
-    }, {
-      rootMargin: "-200px 0px -400px",
-      threshold: 0.5,
-    });
+  sections.forEach(function (section) {
+    const observer = new IntersectionObserver(
+      function (entry) {
+        if (entry[0].isIntersecting) {
+          const sectionId = entry[0].target.id;
+          navigationLinks.forEach(function (link) {
+            if (link.getAttribute("href") === `#${sectionId}`) {
+              link.classList.add("is-active");
+            } else {
+              link.classList.remove("is-active");
+            }
+          });
+        }
+      },
+      {
+        rootMargin: "-10% 0px -85% 0px",
+        threshold: 0.5,
+      },
+    );
 
     observer.observe(section);
   });
-};
+}
 setUpDynamicSideNav();
