c08dc2448b48b50970aaf5be4727dc823cde14b4
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2025-02-07 02:40:38 +0500

    run formatters

--- static/js/src/advantage/credentials/dashboard/utils/common.ts
+++ static/js/src/advantage/credentials/dashboard/utils/common.ts
@@ -16,13 +16,13 @@ export const getFormattedDate = (date: string) => {
 
 export const formatDatetimeForDatePicker = (datetime: string) => {
   const date = new Date(datetime);
-  
+
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
   return `${year}-${month}-${day}T${hours}:${minutes}`;
 };
 
