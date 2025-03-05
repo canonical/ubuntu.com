976b62317f77449276b3db1a9940f5f781164bf3
Peter French <peterfrench94@gmail.com>
2025-03-05 10:15:24 +0100

    Implement a specific cache policy for vulnerabilities & use latest discourse module branch (#14789)
    * test: Use latest discourse module branch for demoing
    
    * feat: Create a fallback for incorrect formatting
    
    * feat: Add a max age to vulnerabilities content
    
    * feat: Use more granular caching
    
    * refactor: Bump discourse version and format python

--- webapp/views.py
+++ webapp/views.py
@@ -1231,15 +1231,22 @@ def build_vulnerabilities_list(security_vulnerabilities, path=None):
                     v for v in vulnerabilities if v["year"] in top_years
                 ]
 
-            return flask.render_template(
-                template_path,
-                topics=topics,
-                vulnerabilities=vulnerabilities,
+            response = flask.make_response(
+                flask.render_template(
+                    template_path,
+                    topics=topics,
+                    vulnerabilities=vulnerabilities,
+                )
             )
+            response.headers["Cache-Control"] = (
+                "max-age=900, stale-while-revalidate=900, stale-if-error=3600"
+            )
+            return response
         except HTTPError as e:
             flask.current_app.extensions["sentry"].captureException(
                 f"Error fetching vulnerabilities: {e}"
             )
+            return flask.Response("Internal Server Error", status=500)
 
     return vulnerabilities_list
 
@@ -1259,14 +1266,21 @@ def build_vulnerabilities(security_vulnerabilities):
                     document_metadata = item
                     break
 
-            return flask.render_template(
-                "security/vulnerabilities/vulnerability-detailed.html",
-                metadata=document_metadata,
-                document=document,
+            response = flask.make_response(
+                flask.render_template(
+                    "security/vulnerabilities/vulnerability-detailed.html",
+                    metadata=document_metadata,
+                    document=document,
+                )
             )
+            response.headers["Cache-Control"] = (
+                "max-age=900, stale-while-revalidate=900, stale-if-error=3600"
+            )
+            return response
         except HTTPError as e:
             flask.current_app.extensions["sentry"].captureException(
                 f"Error fetching vulnerabilities: {e}"
             )
+            return flask.Response("Internal Server Error", status=500)
 
     return vulnerability
