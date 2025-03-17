7663c7047c731fc733185c405f40796e6e3a3854
immortalcodes <21112002mj@gmail.com>
2025-03-17 17:57:45 +0530

    experimenting with percy

---
+++ snapshots.js
@@ -0,0 +1,14 @@
+
+
+module.exports = [
+  {
+    name: 'Homepage',
+    url: 'http://localhost:8001/',
+    execute: {
+      beforeSnapshot() {
+        document.querySelector(".cookie-policy").style.display = "none";
+      },
+    }
+  }
+]
+
