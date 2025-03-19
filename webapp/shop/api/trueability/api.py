7f15d2683e44943ca0870b3971f49e66fcbb507a
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2025-03-19 20:33:54 +0530

    add proxy config

--- webapp/shop/api/trueability/api.py
+++ webapp/shop/api/trueability/api.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 from urllib.parse import urlencode
 from requests import Session
@@ -26,6 +27,17 @@ class TrueAbilityAPI:
     ):
         uri = f"{self.base_url}{path}"
         headers["X-API-KEY"] = f"{self.api_key}"
+        headers["Cache-Control"]= "no-cache, no-store, must-revalidate"
+        headers["Pragma"]= "no-cache"
+        headers["Expires"]= "0"
+
+        if os.getenv("DEVEL", "true")=="true":
+            proxies = None
+        else:
+            proxies = {
+                'http': 'http://squid.ps6.internal:3128/',
+                'https': 'http://squid.ps6.internal:3128/',
+            }
 
         response = self.session.request(
             method,
@@ -34,6 +46,8 @@ class TrueAbilityAPI:
             data=data,
             json=json,
             allow_redirects=allow_redirects,
+            proxies=proxies,
+            # verify=False,
         )
         if retry and response.status_code == 401:
             response = self.make_request(
@@ -43,6 +57,8 @@ class TrueAbilityAPI:
                 data=data,
                 json=json,
                 retry=False,
+                proxies=proxies,
+                # verify=False,
             )
 
         return response
