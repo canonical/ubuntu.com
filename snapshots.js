047d72bbd4bb58af94898b2def37d925d33e1ed0
immortalcodes <21112002mj@gmail.com>
2025-03-25 14:15:25 +0530

    configured percy action and workflow to run on PRs

--- snapshots.js
+++ snapshots.js
@@ -1,9 +1,8 @@
-
-
 module.exports = [
   {
     name: 'Homepage',
     url: 'http://localhost:8001/',
+    "waitForTimeout": 1000,
     execute: {
       beforeSnapshot() {
         document.querySelector(".cookie-policy").style.display = "none";
