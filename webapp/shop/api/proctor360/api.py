7dd879209b9d97842415f19a69627d0b93f10630
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2025-04-14 22:34:07 +0500

    add update student api

--- webapp/shop/api/proctor360/api.py
+++ webapp/shop/api/proctor360/api.py
@@ -17,6 +17,7 @@ class Proctor360API:
         self.app_secret = os.getenv("PROCTOR360_APP_SECRET", "")
         self.session = session
         self.organisation_id = 132
+        self.time_zone_ids = []
 
     def make_request(
         self,
@@ -73,6 +74,23 @@ class Proctor360API:
             )
 
         return response
+
+    def set_time_zone_ids(self):
+        """
+        Set the time zone IDs for the organisation.
+        This method should be called after the class is initialized.
+        """
+        try:
+            uri = "/api/v2/timezone"
+            response = self.make_request("GET", uri).json()
+            self.time_zone_ids = response.get("data", [])
+        except Exception:
+            flask.current_app.extensions["sentry"].captureException(
+                extra={
+                    "request_url": flask.request.url,
+                    "request_headers": flask.request.headers,
+                }
+            )
 
     def __check_keys_exist(self, keys, dictionary):
         """
@@ -155,13 +173,61 @@ class Proctor360API:
     def create_student(self, student: dict):
         try:
             uri = "/api/v2/student"
-            required_keys = ["first_name", "last_name", "email"]
+            required_keys = [
+                "first_name",
+                "last_name",
+                "email",
+                "time_zone_id",
+                "timezone",
+            ]
             optional_keys = ["ext_tenant_id", "ext_student_id"]
             self.__check_keys_exist(required_keys, student)
             data = self._filter_dict_by_keys(
                 required_keys + optional_keys, student
             )
             return self.make_request("POST", uri, json=data).json()
+        except Exception:
+            flask.current_app.extensions["sentry"].captureException(
+                extra={
+                    "request_url": flask.request.url,
+                    "request_headers": flask.request.headers,
+                }
+            )
+            return None
+
+    def update_student(self, student_id: int, student: dict):
+        try:
+            uri = f"/api/v2/student/{student_id}"
+            optional_keys = [
+                "first_name",
+                "last_name",
+                "email",
+                "ext_tenant_id",
+                "ext_student_id",
+                "time_zone_id",
+                "timezone",
+            ]
+            data = self._filter_dict_by_keys(optional_keys, student)
+            return self.make_request("PATCH", uri, json=data).json()
+        except Exception:
+            flask.current_app.extensions["sentry"].captureException(
+                extra={
+                    "request_url": flask.request.url,
+                    "request_headers": flask.request.headers,
+                }
+            )
+            return None
+
+    def upsert_student(self, student: dict):
+        try:
+            time_zone_id = self.get_time_zone_id(student["timezone"])
+            student["time_zone_id"] = time_zone_id
+            data = self.get_student(student["email"])
+            if data.get("data", None) is not None:
+                student_id = data["data"]["student"]["id"]
+                return self.update_student(student_id, student)
+            else:
+                return self.create_student(student)
         except Exception:
             flask.current_app.extensions["sentry"].captureException(
                 extra={
@@ -217,6 +283,13 @@ class Proctor360API:
 
     def create_student_session(self, student_session: dict):
         try:
+            student = {
+                "first_name": student_session["first_name"],
+                "last_name": student_session["last_name"],
+                "email": student_session["student_email"],
+                "timezone": student_session["timezone"],
+            }
+            self.upsert_student(student)
             uri = "/api/v2/student-sessions"
             required_keys = [
                 "first_name",
@@ -243,6 +316,13 @@ class Proctor360API:
 
     def update_student_session(self, session_id: str, student_session: dict):
         try:
+            student = {
+                "first_name": student_session["first_name"],
+                "last_name": student_session["last_name"],
+                "email": student_session["student_email"],
+                "timezone": student_session["timezone"],
+            }
+            self.upsert_student(student)
             uri = f"/api/v2/student-session/{session_id}"
             optional_keys = [
                 "exam_id",
@@ -281,3 +361,15 @@ class Proctor360API:
                 }
             )
             return None
+
+    def get_time_zone_id(self, timezone: str):
+        """
+        Get the time zone ID for a given time zone string.
+
+        :param timezone: The time zone string.
+        :return: The time zone ID if found, otherwise None.
+        """
+        for tz in self.time_zone_ids:
+            if tz["zone_name"] == timezone:
+                return tz["zone_id"]
+        return None
