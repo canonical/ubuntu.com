db4e67069a14e277c146276376ab36c251b7b78e
Peter French <peterfrench94@gmail.com>
2025-05-09 09:53:12 +0200

    chore: Remove unused JS files (#15085)

--- static/js/src/utils/reverseString.js
+++
@@ -1,6 +0,0 @@
-export function reverseString(contentString) {
-  var splitContent = contentString.split("");
-  var reverseArray = splitContent.reverse();
-  var reversedString = reverseArray.join("");
-  return reversedString;
-}
