1bdb09c4b23e20a9744dce5ccba18f075decc450
codeEmpress1 <alimot.akinbode@canonical.com>
2025-05-29 18:04:27 +0300

    fix demo failing

--- webapp/app.py
+++ webapp/app.py
@@ -1265,7 +1265,7 @@ def render_cmmc_blogs():
     sorted_articles = sorted(
         blogs.get_index()["articles"], key=lambda x: x["date"]
     )
-    return flask.render_template("/security/cmmc.html", blogs=sorted_articles)
+    return flask.render_template("/security/cmmc/index.html", blogs=sorted_articles)
 
 
 app.add_url_rule("/security/cmmc", view_func=render_cmmc_blogs)
