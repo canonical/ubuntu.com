2f37cc2b2afebf130c98a3a9786353d57ce84b0b
Peter French <peterfrench94@gmail.com>
2025-06-20 14:30:20 +0200

    feat(api): Expose endpoint for engage page metadata

--- webapp/views.py
+++ webapp/views.py
@@ -531,6 +531,62 @@ def build_engage_pages_sitemap(engage_pages):
     return ep_sitemap
 
 
+def build_engage_pages_metadata(engage_pages):
+    """
+    Retrieve a all engage pages metadata as a single JSON structure.
+    The data is cached and refreshed once per day.
+    """
+    _cache = {"data": None, "last_update_date": None}
+
+    def get_metadata():
+        today = datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+
+        if _cache["last_update_date"] != today or _cache["data"] is None:
+            all_pages = []
+            limit = 100
+            offset = 0
+            current_total = 1
+
+            while offset < current_total:
+                pages_list, total_count, active_count, current_total = (
+                    engage_pages.get_index(limit=limit, offset=offset)
+                )
+
+                for page in pages_list:
+                    if "path" in page and page["path"]:
+                        sanitized_page = {
+                            "topic_name": str(page.get("topic_name", "")),
+                            "path": str(page.get("path", "")),
+                            "form_id": str(page.get("form_id", "")),
+                            "webinar_code": str(page.get("webinar_code", "")),
+                            "resource_url": str(page.get("resource_url", "")),
+                            "author": str(
+                                page.get("author", "")
+                            ),  # Author doesn't exist yet
+                            "active": str(page.get("active", "")),
+                        }
+                        all_pages.append(sanitized_page)
+
+                offset += limit
+
+            all_pages_metadata = {
+                "total_count": total_count,
+                "pages": all_pages,
+            }
+
+            _cache["data"] = all_pages_metadata
+            _cache["last_update_date"] = today
+
+        response = flask.jsonify(_cache["data"])
+        response.headers["Cache-Control"] = "public, max-age=86400"
+
+        return response
+
+    return get_metadata
+
+
 def openstack_install():
     """
     OpenStack install docs
