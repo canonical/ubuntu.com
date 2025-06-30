022d60e6117b24bbf092097f4f6ed78e4127a37d
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2025-07-01 00:12:05 +0530

    remove unnecessary code

--- static/js/src/advantage/linux-patch-management/components/CVETable.test.tsx
+++ static/js/src/advantage/linux-patch-management/components/CVETable.test.tsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import CVETable from "./CVETable";
 import { useFetchCVEData } from "../utils/helpers";
-import exp from "constants";
 
 jest.mock("../utils/helpers", () => ({
   ...jest.requireActual("../utils/helpers"),
