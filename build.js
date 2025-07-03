965ffd058dc5f1772f02f6632862a3b037a42750
Peter French <peterfrench94@gmail.com>
2025-07-03 16:12:37 +0200

    feat: Create custom prism module

--- build.js
+++ build.js
@@ -38,6 +38,7 @@ let entries = {
     "./static/js/src/advantage/credentials/dashboard/app.tsx",
   "canonical-cla": "./static/js/src/canonical-cla/app.tsx",
   "table-of-contents": "./static/js/src/table-of-contents.js",
+  prism: "./static/js/src/prism.js",
 };
 
 const isDev = process && process.env && process.env.NODE_ENV === "development";
---
+++ static/js/src/prism.js
@@ -0,0 +1,7 @@
+import Prism from 'prismjs';
+import 'prismjs/plugins/autoloader/prism-autoloader';
+import 'prismjs/plugins/keep-markup/prism-keep-markup';
+
+Prism.plugins.autoloader.languages_path = '/static/js/dist/prism-components/';
+
+export default Prism;
