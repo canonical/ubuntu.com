ad4dae3f9b88c30870f809b97fd425c409868231
Peter French <peterfrench94@gmail.com>
2025-07-03 16:13:24 +0200

    feat: Include prism in docs and tutorials

--- static/js/src/prism.js
+++ static/js/src/prism.js
@@ -1,7 +1,7 @@
-import Prism from 'prismjs';
-import 'prismjs/plugins/autoloader/prism-autoloader';
-import 'prismjs/plugins/keep-markup/prism-keep-markup';
+import Prism from "prismjs";
+import "prismjs/plugins/autoloader/prism-autoloader";
+import "prismjs/plugins/keep-markup/prism-keep-markup";
 
-Prism.plugins.autoloader.languages_path = '/static/js/dist/prism-components/';
+Prism.plugins.autoloader.languages_path = "/static/js/dist/prism-components/";
 
 export default Prism;
