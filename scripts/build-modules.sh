940088cba76749bfc45e17fa741f0683d04b468a
Peter French <peterfrench94@gmail.com>
2025-07-11 13:21:11 +0200

    feat: Add leaflet.js package and refactor build-module.sh

--- scripts/build-modules.sh
+++ scripts/build-modules.sh
@@ -1,7 +1,23 @@
 
-yarn run build-global-nav 
-yarn run build-cookie-policy 
-yarn run build-latest-news 
-yarn run build-intl-tel-input-utils 
-yarn run build-flickity
-yarn run build-prism
+#!/bin/bash
+
+# Build cookie policy
+cp node_modules/@canonical/cookie-policy/build/js/cookie-policy.js static/js/dist/
+
+# Build flickity
+cp node_modules/flickity/dist/flickity.pkgd.min.js static/js/dist
+
+# Build global nav
+cp node_modules/@canonical/global-nav/dist/global-nav.js static/js/dist/
+
+# Build latest news
+cp node_modules/@canonical/latest-news/dist/latest-news.js static/js/dist
+
+# Build intl tel input utils
+cp node_modules/intl-tel-input/build/js/utils.js static/js/dist
+
+# Build prism components
+mkdir -p static/js/dist/prism-components && cp -r node_modules/prismjs/components/*.min.js static/js/dist/prism-components/
+
+# Build leaflet
+cp node_modules/leaflet/dist/leaflet.js static/js/dist
