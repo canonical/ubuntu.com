1698dbbf0b6ae8fe2add13f08fe552eff1309a25
Benjamin Oni <onibenjo@gmail.com>
2025-08-19 13:07:13 +0100

    feat(certified): update component status display and table styling
    - Add CORE 18/20 support to component status display
    - Simplify third-party driver indicator display
    - Remove redundant third-party driver legend item

--- webapp/certified/views.py
+++ webapp/certified/views.py
@@ -626,7 +626,8 @@ def certified_model_details(canonical_id):
     component_summaries[0]["lts_releases"] = {
         '20.04 LTS': [{'release': '20.04 LTS', 'third_party_driver': True, 'status': 'certified'}],
         '22.04 LTS': [{'release': '22.04 LTS', 'third_party_driver': False, 'status': 'inprogress'}],
-        '18.04 LTS': [{'release': '18.04 LTS', 'third_party_driver': False, 'status': 'certified'}]
+        '18.04 LTS': [{'release': '18.04 LTS', 'third_party_driver': False, 'status': 'certified'}],
+        'CORE 18': [{'release': '18.04 LTS', 'third_party_driver': True, 'status': 'unsupported'}]
     }
 
     return render_template(
