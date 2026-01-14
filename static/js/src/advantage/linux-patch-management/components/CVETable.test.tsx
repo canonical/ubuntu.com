eb90f3597da436539eec1c562ade543e7b9c178c
Abhigyan <abhigyanghosh30@gmail.com>
2026-01-14 19:25:08 +0530

    fix tests

--- static/js/src/advantage/linux-patch-management/components/CVETable.test.tsx
+++ static/js/src/advantage/linux-patch-management/components/CVETable.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { getAllByRole, render } from "@testing-library/react";
 import CVETable from "./CVETable";
 import { useFetchCVEData } from "../utils/helpers";
 
@@ -71,7 +71,7 @@ describe("CVETable", () => {
   });
 
   it("renders CVESelector with correct props", () => {
-    const { getByTestId, getByText, getByRole } = render(<CVETable />);
+    const { getByTestId, getByText, getByRole,getAllByRole } = render(<CVETable />);
     expect(getByTestId("table-status-filter")).toHaveValue("focal");
     const options = getByTestId("table-status-filter").querySelectorAll(
       "option",
@@ -91,11 +91,12 @@ describe("CVETable", () => {
     expect(getByRole("grid", { name: "cve-table" })).toBeInTheDocument();
     expect(getByRole("rowheader", { name: "Package 1" })).toBeInTheDocument();
     expect(getByRole("rowheader", { name: "Package 2" })).toBeInTheDocument();
+    console.log(getAllByRole("row"));
     expect(
-      getByRole("row", { name: "Package 1 1 0 Ubuntu Pro" }),
+      getByRole("row", { name: "Package 1 1 0 LTS" }),
     ).toBeInTheDocument();
     expect(
-      getByRole("row", { name: "Package 2 1 2 Ubuntu Pro" }),
+      getByRole("row", { name: "Package 2 1 2 LTS" }),
     ).toBeInTheDocument();
   });
 });
