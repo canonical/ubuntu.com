e62079da103ddb10fbe3771e5be76119c895cc80
Peter French <peterfrench94@gmail.com>
2026-03-13 09:23:47 +0100

    feat: engage pages

--- static/js/src/resources.js
+++ static/js/src/resources.js
@@ -13,26 +13,32 @@
  * XXX: This is a hack until we have an individual resource component pattern.
  * https://github.com/canonical/vanilla-framework/issues/5771
  */
-const fetchAndRenderResources = (tag = "", resource = "") => {
+const fetchAndRenderResources = (tag = "", resource = "", language = "",  customTemplateID) => {
   tag = tag.replaceAll(" ", "+");
   resource = resource.replaceAll(" ", "+");
+  let template = "";
+  const FALLBACK_META_IMAGE = "https://assets.ubuntu.com/v1/c797f1d7-og_%20canonical.png";
 
-  const template = document.createElement("template");
-  template.innerHTML = `
-    <div class="p-section--shallow">
-      <div class="grid-row">
-        <div class="grid-col-2 grid-col-medium-2">
-          <div class="p-image-container--16-9 is-cover" data-key="meta_image_container">
-            <img class="p-image-container__image" data-key="meta_image" src="" alt="" />
+  if (customTemplateID) {
+    template = document.getElementById(customTemplateID);
+  } else {
+    template = document.createElement("template");
+    template.innerHTML = `
+      <div class="p-section--shallow">
+        <div class="grid-row">
+          <div class="grid-col-2 grid-col-medium-2">
+            <div class="p-image-container--16-9 is-cover" data-key="meta_image_container">
+              <img class="p-image-container__image" data-key="meta_image" src="${FALLBACK_META_IMAGE}" alt="test" />
+            </div>
+          </div>
+          <div class="grid-col-4 grid-col-medium-2">
+            <p><strong><a data-key="path_name"></a></strong></p>
+            <p data-key="subtitle"></p>
           </div>
         </div>
-        <div class="grid-col-4 grid-col-medium-2">
-          <p><strong><a data-key="path_name"></a></strong></p>
-          <p data-key="subtitle"></p>
-        </div>
       </div>
-    </div>
-  `;
+    `;
+  }
 
   fetch(`/engage/resources.json?tag=${tag}&resource=${resource}`)
     .then((r) => r.json())
@@ -42,15 +48,7 @@ const fetchAndRenderResources = (tag = "", resource = "") => {
 
       resources.forEach((resource) => {
         if (!resource.topic_name || !resource.path) return;
-
         const card = template.content.cloneNode(true);
-
-        if (resource.meta_image) {
-          card.querySelector("[data-key='meta_image']").src =
-            resource.meta_image;
-        } else {
-          card.querySelector("[data-key='meta_image_container']").remove();
-        }
 
         const link = card.querySelector("[data-key='path_name']");
         link.href = resource.path;
@@ -58,6 +56,25 @@ const fetchAndRenderResources = (tag = "", resource = "") => {
 
         card.querySelector("[data-key='subtitle']").textContent =
           resource.subtitle || "";
+
+        if (resource.meta_image) {
+          card.querySelector("[data-key='meta_image']").src =
+            resource.meta_image;
+        }
+
+        // Some custom templates accommodate resource type
+        const cardType = card.querySelector("[data-key='resource_type']");
+        if (cardType) {
+          cardType.textContent = resource.type || "";
+        }
+
+        // Add divider after each card except the last one
+        if (list.hasChildNodes()) {
+          const hr = document.createElement("hr");
+          hr.classList.add("p-rule--muted");
+          list.appendChild(hr);
+        }
+
         list.appendChild(card);
       });
 
