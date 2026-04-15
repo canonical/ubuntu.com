b35df50f70713fcf7dcb313ea2355082c4be4860
andesol <andreu.oller@canonical.com>
2026-04-15 19:04:59 +0200

    update release charts

--- static/js/src/chart-data.js
+++ static/js/src/chart-data.js
@@ -138,16 +138,28 @@ export var serverAndDesktopReleases = [
     status: "PRO_LEGACY_SUPPORT",
   },
   {
-    startDate: new Date("2025-04-01T00:00:00"),
-    endDate: new Date("2026-01-05T00:00:00"),
-    taskName: "25.04 (Plucky Puffin)",
-    status: "INTERIM_RELEASE",
-  },
-  {
     startDate: new Date("2025-10-01T00:00:00"),
     endDate: new Date("2026-07-07T00:00:00"),
     taskName: "25.10 (Questing Quokka)",
     status: "INTERIM_RELEASE",
+  },
+  {
+    startDate: new Date("2026-04-01T00:00:00"),
+    endDate: new Date("2028-09-30T00:00:00"),
+    taskName: "26.04 LTS (Resolute Raccoon)",
+    status: "HARDWARE_AND_MAINTENANCE_UPDATES",
+  },
+  {
+    startDate: new Date("2028-09-30T00:00:00"),
+    endDate: new Date("2031-04-01T00:00:00"),
+    taskName: "26.04 LTS (Resolute Raccoon)",
+    status: "MAINTENANCE_UPDATES",
+  },
+  {
+    startDate: new Date("2031-04-01T00:00:00"),
+    endDate: new Date("2036-04-09T00:00:00"),
+    taskName: "26.04 LTS (Resolute Raccoon)",
+    status: "ESM",
   },
 ];
 
@@ -1286,8 +1298,8 @@ export var microStackStatus = {
 };
 
 export var desktopServerReleaseNames = [
+  "26.04 LTS (Resolute Raccoon)",
   "25.10 (Questing Quokka)",
-  "25.04 (Plucky Puffin)",
   "24.04 LTS (Noble Numbat)",
   "22.04 LTS (Jammy Jellyfish)",
   "20.04 LTS (Focal Fossa)",
