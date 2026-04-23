0a8078a8e8aff2f6f7b0e2a8223fb54661d1c13c
Peter French <peterfrench94@gmail.com>
2026-04-23 16:50:29 +0200

    fix: format-prettier

--- static/js/src/chart-data.js
+++ static/js/src/chart-data.js
@@ -175,14 +175,14 @@ export var kernelReleases = [
     endDate: new Date("2031-04-01T00:00:00"),
     taskName: "26.04.0 LTS",
     taskVersion: "7.0 kernel",
-    status: "LTS"
+    status: "LTS",
   },
   {
     startDate: new Date("2031-04-01T00:00:00"),
     endDate: new Date("2036-03-30T00:00:00"),
     taskName: "26.04.0 LTS",
     taskVersion: "7.0 kernel",
-    status: "ESM"
+    status: "ESM",
   },
   {
     startDate: new Date("2036-03-30T00:00:00"),
