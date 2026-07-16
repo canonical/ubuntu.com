168c2fa1fe832fac7e6c94fc7c97993959a424cd
Britney Wang <britney.wang@canonical.com>
2026-07-16 14:43:05 +0800

    qa: remove invalid fields used for testing

--- static/js/src/prepare-form-inputs.js
+++ static/js/src/prepare-form-inputs.js
@@ -60,7 +60,6 @@ export function setupIntlTelInput(countryCode, phoneInput) {
     separateDialCode: true,
     hiddenInput: (phoneInputName) => ({
       phone: phoneInputName,
-      invalid: "field",
     }),
     initialCountry: countryCode.toLowerCase(),
   });
